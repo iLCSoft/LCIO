ea5540d022e9ca7d4f8e1124c3d250fce2adc71c
Frank Gaede <frank.gaede@desy.de>
2003-03-04 18:17:40 +0000

    Starting C++ implementation (new directories/namespaces, e.g. EVENT and IMPL)

---
+++ src/cpp/include/IMPL/LCEventImpl.h
@@ -0,0 +1,109 @@
+#ifndef EVENT_LCEVENTIMPL_H
+#define EVENT_LCEVENTIMPL_H 1
+
+#include <string>
+#include <map>
+#include "EVENT/LCEvent.h"
+#include "EVENT/LCCollection.h"
+
+
+namespace IMPL{
+  
+  typedef std::map<std::string,EVENT::LCCollection*> LCCollectionMap ; 
+  
+  /**Implementation of the main event class. This is used by  
+   * LCIO for reading events from file.
+   * 
+   * @author gaede
+   * @version Mar 4, 2003
+   * @see LCEvent
+   * @see LCCollection
+   */
+class LCEventImpl : public EVENT::LCEvent {
+    
+  public: 
+    LCEventImpl() ;
+    /** Copy contructor, creates a deep copy of the event.
+     */
+    LCEventImpl(const EVENT::LCEvent& evt) ;  // copy c'tor
+    
+    /// Destructor.
+    virtual ~LCEventImpl() ; 
+    
+    /** Return the run number off this event.
+     */
+    virtual int getRunNumber() const ;
+    
+    /** Returns this event's number .
+     */
+    virtual int getEventNumber() const ;
+    
+    /** Returns the name of the detector setup used in the simulation.
+     */
+    virtual const std::string & getDetectorName() const ;
+    
+    /** Returns the time stamp of the event.
+     */
+    virtual long getTimeStamp() const ;
+    
+    /** Returns the names of the collections in the  event.
+     */
+    virtual std::vector<std::string>* getCollectionNames() const ;
+    
+    /** Returns the collection for the given name - null if it doesn't exist.
+     */ 
+    virtual EVENT::LCCollection * getCollection(const std::string & name) const ;
+    
+    /** Adds a collection with the given name. This method should fail if the event 
+     * is read only and return LCIO::ERROR.
+     */ 
+    virtual int addCollection(EVENT::LCCollection * col, const std::string & name) ;
+    
+    /** Removes (and deletes) the collection with the given name. This method should fail if the event 
+     * is read only and return LCIO::ERROR.
+     */ 
+    virtual int removeCollection(const std::string & name) ;
+    
+    //---- set methods -----
+    /** Sets the run number.
+     */
+    void setRunNumber( int rn ) ;
+    
+    /** Sets the event number.
+     */
+    void setEventNumber( int en ) ;
+    
+    /** Sets the detector name.
+     */
+    void setDetectorName(const char* dn ) ;
+    
+    /** Sets the event time stamp.
+     */
+    void setTimeStamp(long ts) ;
+       
+     
+  protected:
+    void setAccessMode( int accessMode ) ;
+    
+    
+    // data members - declared protected to be accessible 
+    // for friends of sub classes ...
+    
+  protected:  
+    int _runNumber ;
+    int _eventNumber ;
+    long _timeStamp ;
+    std::string _detectorName ;
+    
+    // map has to be defined mutable in order to use _map[]  for const methods ...
+    mutable LCCollectionMap _map ;
+    
+  private:
+    int _access ;   // flag for access mode 
+    
+    
+  }; // class
+
+}; // namespace IMPL
+
+#endif /* ifndef EVENT_LCEVENTIMPL_H */
---
+++ src/cpp/src/IMPL/LCEventImpl.cc
@@ -0,0 +1,147 @@
+#include "IMPL/LCEventImpl.h"
+
+#include "EVENT/LCIO.h"
+
+
+using namespace EVENT ;
+
+
+namespace IMPL {
+  
+LCEventImpl::LCEventImpl() :
+  _runNumber(0),
+  _eventNumber(0),
+  _timeStamp(0),
+  _detectorName("unknown"),
+  _access( LCIO::UPDATE ) {
+  
+}
+  
+LCEventImpl::LCEventImpl(const LCEvent& evt) : 
+  _runNumber( evt.getRunNumber() ),
+  _eventNumber( evt.getEventNumber() ),
+  _timeStamp( evt.getTimeStamp() ),
+  _detectorName( evt.getDetectorName().c_str() ),
+  _access( LCIO::UPDATE ) { // copy will be updateable
+  
+  StringVec* strVec = evt.getCollectionNames() ;
+  int nCol = strVec->size() ;
+  
+  for( StringVec::iterator name = strVec->begin() ; name != strVec->end() ; name++){
+    
+    const LCCollection* col = evt.getCollection( *name ) ;
+    col->getTypeName() ;
+    
+    // to be done - need to create new LCCollectionVec and add to the event ...
+  }
+
+}
+
+
+
+LCEventImpl::~LCEventImpl() {
+
+  // here we need to delete all collections in the event
+  typedef LCCollectionMap::const_iterator LCI ;
+  
+  for ( LCI i=_map.begin() ; i != _map.end() ; i++ ){
+    delete i->second ;
+  }
+}
+    
+int LCEventImpl::getRunNumber() const {
+  return _runNumber ;
+}
+
+    
+int LCEventImpl::getEventNumber() const {
+  return _eventNumber ;
+}
+
+    
+const std::string & LCEventImpl::getDetectorName() const {
+  return _detectorName ;
+}
+
+    
+long LCEventImpl::getTimeStamp() const {
+  return _timeStamp ;
+}
+
+    
+std::vector<std::string>* LCEventImpl::getCollectionNames() const {
+
+  // return pointer to static vector 
+  typedef LCCollectionMap::const_iterator LCI ;
+  
+  static std::vector<std::string> names ;
+  names.clear() ;
+
+  for ( LCI i=_map.begin() ; i != _map.end() ; i++ ){
+    names.push_back( i->first  ) ; 
+  }
+  return &names ;
+}
+
+    
+LCCollection * LCEventImpl::getCollection(const std::string & name) const {
+
+  if( _map.find( name ) == _map.end() ) return 0 ;
+  return  _map[ name ] ;
+}
+
+    
+
+int LCEventImpl::addCollection(LCCollection * col, const std::string & name) {
+
+  // add a new collection only, if access mode == update
+  if( _access != LCIO::UPDATE )
+    return LCIO::ERROR ;
+
+  _map[ name ]  = col ;
+  return LCIO::SUCCESS  ;  
+
+}
+
+    
+
+int LCEventImpl::removeCollection(const std::string & name) {
+
+  // remove collection only, if access mode == update
+  if( _access != LCIO::UPDATE )
+    return LCIO::ERROR ;
+  
+  // return error if name wasn't in the event - do we care ?
+  if( _map.erase( name ) == 0 ) return LCIO::ERROR ;
+  
+  return LCIO::SUCCESS ;
+}
+
+    
+void LCEventImpl::setRunNumber( int rn ) {
+  _runNumber = rn ;
+}
+
+
+void LCEventImpl::setEventNumber( int en ) {
+  _eventNumber = en ;
+}
+
+    
+void LCEventImpl::setDetectorName(const char* dn ) {
+  _detectorName = dn ;
+}
+
+    
+void LCEventImpl::setTimeStamp(long ts) {
+  _timeStamp =  ts ;
+}
+
+       
+     
+void LCEventImpl::setAccessMode( int accessMode ) {
+  _access = accessMode ;
+}
+
+    
+};
