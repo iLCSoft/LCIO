76e6008448ef76e9cdba70fe7db4571a7f7a9639
Frank Gaede <frank.gaede@desy.de>
2003-03-10 17:32:07 +0000

    Added Factory class.

---
+++ src/cpp/src/IOIMPL/LCFactory.cc
@@ -0,0 +1,50 @@
+#include "IOIMPL/LCFactory.h"
+
+
+
+#include "IO/LCWriter.h"
+#include "IO/LCReader.h"
+
+#include "SIO/SIOWriter.h"
+#include "SIO/SIOReader.h"
+
+
+
+using namespace IO ;
+using namespace SIO ;
+
+namespace IOIMPL{
+
+  LCFactory* LCFactory::_me = 0 ;
+  
+  
+  LCFactory::LCFactory() {  
+  }
+  
+
+  LCFactory* LCFactory::getInstance() { 
+
+    if( !_me ) _me = new LCFactory ;
+    return _me ;
+  }
+  
+
+  LCFactory::~LCFactory() { 
+    delete _me ;
+  }
+  
+  LCWriter * LCFactory::createLCWriter() { 
+    
+    // the reason for having this class
+    // so far we just create SIO objects
+
+    return new SIOWriter ;
+  }
+  
+  LCReader * LCFactory::createLCReader() { 
+    // so far we just create SIO objects
+    return new SIOReader ;
+  }
+  
+  
+}; // namespace
