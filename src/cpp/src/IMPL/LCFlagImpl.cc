a30cf23c8463634e088474fe29f90a5e2cae44c0
Frank Gaede <frank.gaede@desy.de>
2003-05-07 12:09:29 +0000

    Helper class to handle collection flag word.

---
+++ src/cpp/src/IMPL/LCFlagImpl.cc
@@ -0,0 +1,37 @@
+#include "IMPL/LCFlagImpl.h"
+
+
+using namespace EVENT ;
+
+namespace IMPL {
+  
+  LCFlagImpl::LCFlagImpl() : _flag(0) {}
+
+  LCFlagImpl::LCFlagImpl(int flag) : _flag( flag ) {} 
+
+  
+  bool LCFlagImpl::bitSet(int bit) const { return (_flag & (1<< bit)   ) ? true : false ; } 
+
+  int LCFlagImpl::getFlag() const { return _flag ; } 
+
+
+  void LCFlagImpl::setBit(int bit) { 
+
+    if( 0<=bit && bit<=31 &&  !bitSet( bit) ){
+      
+      _flag = _flag | ( 1 << bit ) ; 
+      
+    }   
+  }
+  
+
+  void LCFlagImpl::unsetBit(int bit){
+
+    if( 0<=bit && bit<=31 &&  bitSet( bit) ){
+      
+      _flag = _flag &  ~( 1 << bit ) ; 
+      
+    }   
+  }
+
+}; // namespace
