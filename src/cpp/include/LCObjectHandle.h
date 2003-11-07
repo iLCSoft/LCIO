8341d76669e3d121b54dc88cdc5948e08b18afce
Frank Gaede <frank.gaede@desy.de>
2003-11-07 16:51:47 +0000

    initital version.

---
+++ src/cpp/include/LCObjectHandle.h
@@ -0,0 +1,40 @@
+#ifndef LCIO_OBJECTHANDLE_H
+#define LCIO_OBJECTHANDLE_H 1
+
+#include "DATA/LCObject.h"
+#include "Exceptions.h"
+
+namespace lcio{
+
+  /**Template handle class for LCObjects 
+   * @author gaede
+   * @version Sep 24, 2003
+   */
+
+  template<class T> class LCObjectHandle{
+    
+  protected: 
+    T* _lcObj ;
+    
+  public: 
+    virtual ~LCObjectHandle() { /*no_op*/; } 
+    
+    LCObjectHandle( T* lcObj) : _lcObj(lcObj) {
+    }
+
+    LCObjectHandle(LCObject* lcObj){
+      
+      _lcObj = dynamic_cast< T* >( lcObj ) ;
+
+      if( _lcObj == 0 )
+	throw Exception("dynamic cast failed for LCObjectHandle() !") ;
+    }
+
+    T* operator->() { return _lcObj ; } 
+
+    T* lcObj() { return _lcObj ; } 
+
+  }; 
+
+} // namespace
+#endif /* ifndef LCIO_OBJECTHANDLE_H */
