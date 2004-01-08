fe7fc33b25d2f21306305f19fe4284500464b66d
Tony Johnson <tonyj@stanford.edu>
2004-01-08 21:46:54 +0000

    Remove spurious import

--- src/java/hep/lcio/implementation/sio/SIOTPCHit.java
+++ src/java/hep/lcio/implementation/sio/SIOTPCHit.java
@@ -4,7 +4,6 @@ import hep.lcd.io.sio.SIOInputStream;
 import hep.lcd.io.sio.SIOOutputStream;
 import hep.lcd.io.sio.SIORef;
 
-
 import hep.lcio.event.LCIO;
 
 import hep.lcio.data.TPCHitData;
@@ -13,13 +12,11 @@ import hep.lcio.implementation.event.ITPCHit;
 
 import java.io.IOException;
 
-import org.apache.xml.utils.RawCharacterHandler;
-
 
 /**
  *
  * @author Tony Johnson
- * @version $Id: SIOTPCHit.java,v 1.2 2003-09-15 21:44:33 tonyj Exp $
+ * @version $Id: SIOTPCHit.java,v 1.3 2004-01-08 21:46:54 tonyj Exp $
  */
 class SIOTPCHit extends ITPCHit
 {
@@ -30,17 +27,18 @@ class SIOTPCHit extends ITPCHit
       time = in.readFloat();
       charge = in.readFloat();
       quality = in.readInt();
-
+      
       if ((flags & (1 << LCIO.TPCBIT_RAW)) != 0)
       {
-	     int size = in.readInt() ;
+         int size = in.readInt() ;
          rawDataArray = new int[ size ] ;
-	     for (int i = 0; i < size; i++) {
-	        rawDataArray[i] = in.readInt() ;		
-		}
+         for (int i = 0; i < size; i++)
+         {
+            rawDataArray[i] = in.readInt() ;
+         }
       }
    }
-
+   
    static void write(TPCHitData hit, SIOOutputStream out, int flags) throws IOException
    {
       if (hit instanceof SIOTPCHit)
@@ -51,30 +49,32 @@ class SIOTPCHit extends ITPCHit
          out.writeFloat(hit.getTime());
          out.writeFloat(hit.getCharge());
          out.writeInt(hit.getQuality());
-
+         
          if ((flags & (1 << LCIO.TPCBIT_RAW)) != 0)
          {
-           out.writeInt( hit.getNRawDataWords() );
-           for (int i = 0; i < hit.getNRawDataWords() ; i++) {
-			 out.writeInt( hit.getRawDataWord(i) ) ;
-		   }            
+            out.writeInt( hit.getNRawDataWords() );
+            for (int i = 0; i < hit.getNRawDataWords() ; i++)
+            {
+               out.writeInt( hit.getRawDataWord(i) ) ;
+            }
          }
       }
    }
-
+   
    private void write(SIOOutputStream out, int flags) throws IOException
    {
       out.writeInt(cellID);
       out.writeFloat(time);
       out.writeFloat(charge);
       out.writeInt(quality);
-
+      
       if ((flags & (1 << LCIO.TPCBIT_RAW)) != 0)
       {
          out.writeInt(rawDataArray.length);
-	     for (int i = 0; i < rawDataArray.length; i++) {
-           out.writeInt( rawDataArray[i]);
-	     }
+         for (int i = 0; i < rawDataArray.length; i++)
+         {
+            out.writeInt( rawDataArray[i]);
+         }
       }
    }
 }
