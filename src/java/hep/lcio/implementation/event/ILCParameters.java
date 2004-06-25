71aafe68ae1b44d7bfb387f549691c601307fb63
Frank Gaede <frank.gaede@desy.de>
2004-06-25 12:56:11 +0000

    java implementation of LCParameters

---
+++ src/java/hep/lcio/implementation/event/ILCParameters.java
@@ -0,0 +1,138 @@
+package hep.lcio.implementation.event;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import hep.lcio.event.LCParameters;
+
+
+/** Implementation of generic named parameters. 
+ * @author gaede
+ * @version $Id: ILCParameters.java,v 1.1 2004-06-25 12:56:11 gaede Exp $
+ */
+public class ILCParameters extends ILCObject implements LCParameters {
+
+
+	protected Map _intMap = new HashMap() ;
+	protected Map _floatMap = new HashMap() ;
+	protected Map _stringMap = new HashMap() ;
+
+	public int getIntVal(String key) {
+		
+		int[] vals = (int[]) _intMap.get( key ) ;
+		if(vals==null||vals.length==0) 
+		   return 0 ;
+	 
+		return vals[0] ;
+	}
+
+	public float getFloatVal(String key) {
+
+		float[] vals = (float[]) _floatMap.get( key ) ;
+		if(vals==null||vals.length==0) 
+		   return 0 ;
+	 
+		return vals[0] ;
+	}
+
+	public String getStringVal(String key) {
+
+		String[] vals = (String[]) _stringMap.get( key ) ;
+		if(vals==null||vals.length==0) 
+		   return "" ;
+	 
+		return vals[0] ;
+	}
+
+	public int[] getIntVals(String key) {
+		return (int[]) _intMap.get(key) ;
+	}
+
+	public float[] getFloatVals(String key) {
+	   return (float[]) _floatMap.get(key) ;
+	}
+
+	public String[] getStringVals(String key) {
+		return (String[]) _stringMap.get(key) ; 
+	}
+
+	public String[] getIntKeys() {
+		Object[] ob = _intMap.keySet().toArray() ;
+		String[] keys = new String[ ob.length ] ;
+		for (int i = 0; i < keys.length; i++) {
+			keys[i] = (String) ob[i] ;
+		}
+		return keys  ;
+	}
+
+	public String[] getFloatKeys() {
+		Object[] ob = _floatMap.keySet().toArray() ;
+		String[] keys = new String[ ob.length ] ;
+		for (int i = 0; i < keys.length; i++) {
+			keys[i] = (String) ob[i] ;
+		}
+		return keys  ;
+	}
+
+	public String[] getStringKeys() {
+		Object[] ob = _stringMap.keySet().toArray() ;
+		String[] keys = new String[ ob.length ] ;
+		for (int i = 0; i < keys.length; i++) {
+			keys[i] = (String) ob[i] ;
+		}
+		return keys  ;
+	}
+
+	public int getNInt(String key) {
+		int[] a = (int[]) _intMap.get(key) ;
+		if( a ==null ) return 0 ;
+		return a.length ;
+	}
+
+	public int getNFloat(String key) {
+		float[] a = (float[]) _floatMap.get(key) ;
+		if( a ==null ) return 0 ;
+		return a.length ;
+	}
+
+	public int getNString(String key) {
+		String[] a = (String[]) _stringMap.get(key) ;
+		if( a ==null ) return 0 ;
+		return a.length ;
+	}
+
+	public void setValue(String key, int value) {
+		checkAccess() ;
+		int[] values = { value } ;
+		_intMap.put(key,values) ;
+	}
+
+	public void setValue(String key, float value) {
+		checkAccess() ;
+		float[] values = { value } ;
+		_floatMap.put(key,values) ;
+	}
+
+	public void setValue(String key, String value) {
+		checkAccess() ;
+		String[] values = { value } ;
+		_stringMap.put(key,values) ;
+	}
+
+	public void setValues(String key, int[] values) {
+		checkAccess() ;
+		_intMap.put(key,values) ;
+	}
+
+	public void setValues(String key, float[] values) {
+		checkAccess() ;
+		_floatMap.put(key,values) ;
+	}
+
+	public void setValues(String key, String[] values) {
+		checkAccess() ;
+		_stringMap.put(key,values) ;
+	}
+
+}
---
+++ src/java/hep/lcio/implementation/sio/SIOLCParameters.java
@@ -0,0 +1,102 @@
+package hep.lcio.implementation.sio;
+
+import hep.lcd.io.sio.SIOInputStream;
+import hep.lcd.io.sio.SIOOutputStream;
+
+import hep.lcio.event.LCParameters;
+
+import hep.lcio.implementation.event.ILCParameters;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Frank Gaede
+ * @version $Id: SIOLCParameters.java,v 1.1 2004-06-25 12:56:11 gaede Exp $
+ */
+class SIOLCParameters extends ILCParameters
+{
+   SIOLCParameters(SIOInputStream in) throws IOException
+   {
+	int nIntParameters =  in.readInt(); 
+	for (int i = 0; i < nIntParameters; i++) {
+	  String key = in.readString();
+	  int nInt = in.readInt() ;
+	  int[] iv = new int[nInt] ;
+	  for (int j = 0; j < nInt; j++) {
+		  iv[j] = in.readInt() ;
+	  }
+	  _intMap.put( key , iv ) ; 
+	}	
+	int nFloatParameters =  in.readInt(); 
+	for (int i = 0; i < nFloatParameters; i++) {
+	  String key = in.readString();
+	  int nFloat = in.readInt() ;
+	  float[] fv = new float[nFloat] ;
+	  for (int j = 0; j < nFloat; j++) {
+		  fv[j] = in.readFloat() ;
+	  }
+	  _intMap.put( key , fv ) ; 
+	}	
+
+	int nStringParameters =  in.readInt(); 
+	for (int i = 0; i < nStringParameters; i++) {
+	  String key = in.readString();
+	  int nString = in.readInt() ;
+	  String[] sv = new String[nString] ;
+	  for (int j = 0; j < nString; j++) {
+		  sv[j] = in.readString() ;
+	  }
+	  _intMap.put( key , sv ) ; 
+	}	
+
+
+   }
+
+   static void write(LCParameters params, SIOOutputStream out) throws IOException
+   {
+
+//	if (params instanceof SIOLCParameters)
+//	   ((SIOLCParameters) params).write(out);
+//	else
+
+	String[] keys = params.getIntKeys() ;
+	int nKeys = keys.length;  
+	out.writeInt( nKeys ) ;
+	for (int i = 0; i < nKeys; i++) {
+	  out.writeString( keys[i] ) ;
+	  int[] v = params.getIntVals( keys[i] ) ;
+	  int nVal = v.length ;
+	  out.writeInt(nVal) ;
+	  for (int j = 0; j < nVal; j++) {
+		 out.writeInt( v[j] ) ; 
+	  }
+	}	
+	keys = params.getFloatKeys() ;
+	nKeys = keys.length;  
+	out.writeInt( nKeys ) ;
+	for (int i = 0; i < nKeys; i++) {
+	  out.writeString( keys[i] ) ;
+	  float[] v = params.getFloatVals( keys[i] ) ;
+	  int nVal = v.length ;
+	  out.writeInt(nVal) ;
+	  for (int j = 0; j < nVal; j++) {
+		 out.writeFloat( v[j] ) ; 
+	  }
+	}	
+	keys = params.getStringKeys() ;
+	nKeys = keys.length;  
+	out.writeInt( nKeys ) ;
+	for (int i = 0; i < nKeys; i++) {
+	  out.writeString( keys[i] ) ;
+	  String[] v = params.getStringVals( keys[i] ) ;
+	  int nVal = v.length ;
+	  out.writeInt(nVal) ;
+	  for (int j = 0; j < nVal; j++) {
+		 out.writeString( v[j] ) ; 
+	  }
+	}	
+   }
+
+}
