68a907e281983f48ef2aab07a19ce472413e72e3
Frank Gaede <frank.gaede@desy.de>
2004-09-22 15:19:12 +0000

    replaced vector::at with vector::operator[] because of gcc2.95

--- src/cpp/src/CPPFORT/lcobv.cc
+++ src/cpp/src/CPPFORT/lcobv.cc
@@ -9,6 +9,7 @@
 #include "LCIOSTLTypes.h"
 
 #include <iostream>
+#include <vector>
 
 using namespace lcio ;
 
@@ -21,13 +22,23 @@ typedef std::map< EVENT::LCObject* , std::pair< EVENT::LCObjectVec , EVENT::Floa
 
  int lcobvgetobjectid( PTRTYPE vector, int index){
    const LCObjectVec* vec    = reinterpret_cast<LCObjectVec*>( vector ) ;
-   LCObject* obj = vec->at(index-1) ;
+
+   //   LCObject* obj = vec->at(index-1) ;
+   if( index < 1 || index > (int) vec->size() )
+     throw Exception("lcobvgetobjectid :  out_of_range " ) ;
+
+   LCObject* obj = (*vec)[ index-1 ] ;
+
    return obj->id() ;
  }
 
- float lcobvgetweight( PTRTYPE vector, int index){
-   const FloatVec* vec    = reinterpret_cast<FloatVec*>( vector ) ;
-   return vec->at(index-1) ;
- }
+float lcobvgetweight( PTRTYPE vector, int index){
+  const FloatVec* vec    = reinterpret_cast<FloatVec*>( vector ) ;
+  //    return vec->at(index-1) ;
+  if( index < 1 || index > (int) vec->size() )
+    throw Exception("lcobvgetweight :  out_of_range " ) ;
+  
+  return (*vec)[ index-1 ] ;
+}
 
 
