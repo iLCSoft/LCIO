e37d7c862ce499d1914fb1f3e54038172d9fe247
Frank Gaede <frank.gaede@desy.de>
2005-02-10 11:31:00 +0000

    helper class for time conversions

---
+++ src/cpp/include/UTIL/LCTime.h
@@ -0,0 +1,109 @@
+// -*- C++ -*-
+#ifndef UTIL_LCTIME_H
+#define UTIL_LCTIME_H 1
+
+#include "LCIOTypes.h"
+
+namespace UTIL {
+  
+  /** Helper class that allows to convert time stamps as defined in 
+   *  LCEvent::getTimeStamp() ( ns since 1.1.1970 00:00:00 UTC) to
+   *  ordinary date and time  and vice versa.<br>
+   *  Dates and times are in UTC/GMT by default.<br>
+   *  Negative time stamps are not supported and mapped to 0LL and 
+   *  1.1.1970 00:00:00 UTC respectively.
+   * 
+   * @author gaede 
+   * @version $Id: LCTime.h,v 1.1 2005-02-10 11:31:00 gaede Exp $
+   */
+  class LCTime {
+
+  public:
+    /** Helper struct that holds the calendar time.
+     */
+    struct CalendarTime{
+      short year  ;
+      unsigned char  month ;
+      unsigned char  day   ;
+      unsigned char  hour ;
+      unsigned char  min ;
+      unsigned char  sec ;
+      int ns ;
+    } ;
+
+  public: 
+    
+    /** Init with current time ( accuracy depends on OS/machine,etc.) in UTC/GMT.
+     */
+    LCTime() ;
+    
+    /** Init from 64 bit time stamp ( ns since 1.1.1970 00:00:00 ).
+     */
+    LCTime( EVENT::long64 time ) ;
+    
+    /** Init from date and time. 
+     */
+    LCTime(int year, int month, int day, int hour=0, int min=0, int s=0 ) ;
+    
+    /** Init from unix time ( s since 1.1.1970 00:00:00 ).
+     */
+    LCTime( int unixTime ) ;
+
+
+    /** Unix time ( s since 1.1.1970 00:00:00 ).
+     */
+    int unixTime() { return ( _t / 1000000000LL ) ; } 
+
+    /** 64bit time stamp as used in LCEvent::getTimestamp().
+     */
+    EVENT::long64 timeStamp() { return _t ; } 
+
+
+    /** True if the year is a leap year in the Gregorian calendar:<br> 
+     *  year is multiple of 4 and not multiple of 100<br>
+     *  or year is multiple of 400.
+     */
+    bool isLeapYear( int year ) { return ( ( !( year % 4 ) &&  ( year%100 ) ) || !(year%400)   ) ; }
+
+    /** The number if days in the given year in the Gregorian calendar. */
+    int daysInYear( int year ) ;
+
+    /** The number if days in the given month and year in the Gregorian calendar. */
+    int daysInMonth( int month , int year ) ;
+
+    /** Date in human readable format, e.g. 10.02.2005  10:54:29.123456789 :<br>
+     *  dd.mm.yyyy  hh:mm:ss._ms_us_ns
+     */
+    std::string getDateString() ;
+
+    /** Calendar time: year,month,day hour,min,sec and ns.
+     */
+    const CalendarTime& calendarTime() { return _d ; } 
+
+
+    /** Add the specified number of ns to the time.
+     */
+    void operator+=(EVENT::long64 t) ;
+
+
+    virtual ~LCTime() { /*no_op*/; } 
+    
+  protected: 
+    
+    EVENT::long64 _t ;  // time stamp in ns
+    CalendarTime  _d ;  // calendar date and time
+
+
+    // internal helper methods
+    void convertToCalTime() ;
+
+    void convertFromCalTime() ;
+
+    static int dpm[13] ;  // days per month
+
+  }; // class
+
+
+}; // namespace UTIL
+
+#endif /* ifndef UTIL_LCTIME_H */
---
+++ src/cpp/src/UTIL/LCTime.cc
@@ -0,0 +1,213 @@
+#include "UTIL/LCTime.h"
+#include "Exceptions.h"
+#include <time.h>
+
+#include <sstream>
+#include <iomanip>
+
+// the canonical year zero
+#define YEAR0 1970 
+
+// seconds per min, hour, day
+#define SPM 60
+#define SPH 3600   // 60*60
+#define SPD 86400  // 60*60*24
+
+#define DPY 365 // days per year - no leap year
+#define NPS 1000000000LL // ns per s - need 64bit to force result of conversion to be 64bit
+
+using namespace EVENT ;
+
+#include <iostream>
+using namespace std ;
+
+namespace UTIL{
+
+  int LCTime::dpm[13]   = { 0,31,28,31,30,31,30,31,31,30,31,30,31 } ;
+
+
+  LCTime::LCTime(){
+
+    time_t now ;
+    time ( &now ) ;
+
+    _t = now * NPS ;
+
+    convertToCalTime() ;
+
+
+//     // selftest :
+//     cout << "--- date : " << getDateString() 
+// 	 << endl 
+// 	 << "--- time: " << _t << endl 
+// 	 << endl ;
+
+//     convertFromCalTime() ;
+//     convertToCalTime() ;
+
+//     cout << "after reconversion  " << endl ;
+//     // selftest :
+//     cout << "--- date : " << getDateString() 
+// 	 << endl 
+// 	 << "--- time: " << _t << endl 
+// 	 << endl ;
+
+  }
+
+  LCTime::LCTime( long64 time) : _t( time )  {
+
+    convertToCalTime() ;
+  }
+
+
+  LCTime::LCTime(int year, int month, int day, int hour, int min, int s ){
+    
+
+    _d.year = year  ;
+    _d.month = month  ;
+    _d.day = day  ;
+    _d.hour = hour  ;
+    _d.min = min  ;
+    _d.sec = s  ;
+    _d.ns = 0 ; 
+    
+    convertFromCalTime() ;
+  }
+  
+  LCTime::LCTime( int unixTime ) {
+
+    _t = unixTime * NPS ;
+
+    convertToCalTime() ;
+  }
+  
+
+
+  void LCTime::convertToCalTime(){
+
+    // initialize date to 1.1.1970
+    _d.year =  YEAR0  ;
+    _d.month = 1 ; 
+    _d.day = 1 ;
+    _d.hour = 0 ;
+    _d.min = 0 ;
+    _d.sec = 0 ;
+    _d.ns = 0 ;
+
+    // don't support negative times 
+    if( _t < 0LL ) {      
+      _t = 0 ;
+      return ;  // map to 1.1.1970 00:00:00
+    }
+
+    // start with unix time, i.e. seconds since 1.1.1970
+    int s = _t / NPS ; 
+    _d.ns = _t % NPS ;
+
+    // total number of days 
+    int nDays = s / SPD ;
+
+    int daysLeft ;
+
+    while( (daysLeft = nDays - daysInYear( _d.year ) )  > 0 ){
+      nDays = daysLeft ;
+      _d.year ++ ;
+    }
+    while( (daysLeft = nDays - daysInMonth( _d.month ,  _d.year ) ) > 0 ){
+      nDays = daysLeft ;
+      _d.month ++ ;
+    }
+    
+    _d.day +=  nDays ;
+
+    s = s % SPD ; // the seconds of the current day
+
+    _d.hour = s / SPH ; // the hours of the current day
+
+    s = s % SPH ; // the seconds of the current hour
+    
+    _d.min  = s / SPM ; // the min of the current hour
+
+    _d.sec = s % SPM  ;  // secs
+
+  }
+  
+  void LCTime::convertFromCalTime(){
+    
+    // don't support dates before 1.1.1970 00:00:00
+    if( _d.year < YEAR0 ) {   // map to 1.1.1970 00:00:00
+      _t = -1  ;  
+      convertToCalTime() ;
+    }
+    
+    // add up all days that have passed
+    unsigned nDays = 0 ; 
+    
+    for( int y = YEAR0 ; y < _d.year ; y ++ ){  
+      nDays += daysInYear( y ) ; 
+    }
+
+    for( int m = 1 ;  m < _d.month ; m ++ ){
+      nDays += daysInMonth( m , _d.year ) ; 
+    }
+
+    nDays += _d.day - 1 ;  // current day hasn't passed yet
+
+    // add up the seconds
+    _t = nDays * SPD + _d.hour  * SPH + _d.min * SPM + _d.sec ;
+ 
+    // now make ns
+    _t *= NPS ;
+
+  }
+
+   
+  int LCTime::daysInYear( int year ) {
+    int d = DPY ;
+    if( isLeapYear( year ) ) d++ ;
+    return d ;
+  }
+  
+  int LCTime::daysInMonth( int month , int year ) {
+    
+    if( month < 1 || month > 12 ) 
+      return 0 ;
+    int d = dpm[ month ] ;
+    
+    if( month == 2 &&  isLeapYear( year ) ) d++ ;
+    return d ;
+  }
+  
+  
+  void LCTime::operator+=(EVENT::long64 t) {
+    _t += t ;
+    convertToCalTime() ;
+  }
+
+
+
+  std::string LCTime::getDateString() {
+
+    std::stringstream out ;
+
+    // dd.mm.yyyy  hh:mm:ss._ms_us_ns
+    out << setfill ('0') 
+	<< std::setw(2) << (int) _d.day 
+	<< std::setw(1) << "." 
+	<< std::setw(2) << (int) _d.month 
+	<< std::setw(1) << "." 
+	<< std::setw(4) << _d.year
+	<< std::setw(1) << "  " 
+ 	<< std::setw(2) << (int) _d.hour 
+	<< std::setw(1) << ":"   
+	<< std::setw(2) << (int) _d.min 
+	<< std::setw(1) << ":"   
+	<< std::setw(2) << (int) _d.sec 
+	<< std::setw(1) << "."   
+	<< std::setw(9) << (int) _d.ns 
+ 	<< std::ends ;  
+    
+    return out.str() ;
+  }
+
+} ;
