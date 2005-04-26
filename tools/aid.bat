40afa5878895d4dc88a9369cbb654d7c260dd74e
No Author <no.author@desy.de>
2005-04-26 11:04:58 +0000

    This commit was manufactured by cvs2svn to create tag 'fg-01-04-01'.

--- tools/aid.bat
+++
@@ -1,1 +0,0 @@
-java %AID_FLAGS% -classpath %LCIO%\tools;%LCIO%\tools\freehep-base.jar;%LCIO%\tools\freehep-aid.jar;%LCIO%\lib\bcel.jar org.freehep.aid.Aid %*
--- tools/ant.bat
+++
@@ -1,3 +0,0 @@
-@SETLOCAL
-@SET ANT_HOME=%LCIO%\tools
-@java -cp "%ANT_HOME%\freehep-buildtools.jar;%ANT_HOME%\ant.jar;%ANT_HOME%\ant-nodeps.jar;%ANT_HOME%\junit.jar;%ANT_HOME%\ant-junit.jar;%ANT_HOME%\jaxp.jar;%ANT_HOME%\parser.jar;%JDK_HOME%\lib\tools.jar;%AIDA%" -Dant.home="%ANT_HOME%" %ANT_OPTS% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
