5e44e04cd98695315507c99f370efa2a0798e8ab
Jeremy McCormick <jeremym@slac.stanford.edu>
2006-03-14 23:55:40 +0000

    JM: Move Python writer test to different file

--- src/python/test.py
+++ src/python/test.py
@@ -34,30 +34,6 @@ print repr(mcp)
 print repr(mcp.getVertex())
 print repr(mcp.getParents())
 
-# LCWriter
-writer=fac.createLCWriter()
-writer.open("writer_test", lcio.LCIO.WRITE_NEW)
-coll = lcio.LCCollectionVec(lcio.cvar.LCIO_SIMCALORIMETERHIT)
-flag = lcio.LCFlagImpl()
-flag.setBit( lcio.LCIO.CHBIT_LONG )
-coll.setFlag( flag.getFlag() )
-evt = lcio.LCEventImpl()
-hit = lcio.SimCalorimeterHitImpl()
-hit.setPosition((1.1, 1.2, 1.3))
-print hit.getPosition()
-coll.addElement(hit)
-
-#print "Collecting garbage ..."
-#gc.collect()
-#print "Done collecting garbage."
-
-evt.addCollection(coll, "test_collection")
-writer.writeEvent(evt)
-writer.close()
-print "Finished with LCWriter test"
-
-coll = None
-
 print "Collecting garbage ..."
 gc.collect()
 print "Done collecting garbage."
