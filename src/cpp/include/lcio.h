bbf426a6b4978faed6981d6dbf93bb17b15bb3bd
Frank Gaede <frank.gaede@desy.de>
2006-08-25 08:47:46 +0000

    updated documentation

--- src/cpp/include/lcio.h
+++ src/cpp/include/lcio.h
@@ -7,7 +7,7 @@
  * documentation with doxygen. 
  * 
  * @author gaede
- * @version $Id: lcio.h,v 1.24.4.1 2006-08-15 15:56:47 engels Exp $ 
+ * @version $Id: lcio.h,v 1.24.4.2 2006-08-25 08:47:46 gaede Exp $ 
  * @see LCEvent
  */
 #include "EVENT/LCIO.h"
@@ -15,7 +15,7 @@
 #include "IMPL/LCIOExceptionHandler.h"
 
 
-/** \mainpage <a href="http://lcio.desy.de">LCIO</a> (v01-08) 
+/** \mainpage <a href="http://lcio.desy.de">LCIO</a> (v01-08-vtx test version for the new vertex class) 
  * A persistency framework for linear collider simulation studies.
  * \section Overview
  * This is the API documentation for the C++ version of LCIO. For general documetation on 
