e5562151c750fa0980ed3af8ece77ac5b4006b40
Jan Engels <jan.engels@desy.de>
2007-07-03 12:56:37 +0000

    more user friendly

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -19,27 +19,42 @@
 SET( ILC_HOME "/afs/desy.de/group/it/ilcsoft/v01-01_beta"
     CACHE PATH "Path to ILC Software" FORCE )
 
-# Path to Java
-SET( JAVA_HOME "${ILC_HOME}/java/1.5.0"
-    CACHE PATH "Path to Java" FORCE )
-
-# Path to CLHEP
-SET( CLHEP_HOME "${ILC_HOME}/CLHEP/2.0.2.2"
-    CACHE PATH "Path to CLHEP" FORCE )
-
 # CMake Modules Path
 SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-00"
     CACHE PATH "Path to CMake Modules" FORCE )
 
 #############################################################################
+# Java
+#############################################################################
+
+# If you don't want to use the java version automatically detected by cmake
+# you can define here what java to use
+
+# Path to Java
+#SET( JAVA_HOME "${ILC_HOME}/java/1.5.0"
+#    CACHE PATH "Path to Java" FORCE )
+
+# If you don't need lcio.jar you can set this to OFF
+#SET( INSTALL_JAR OFF CACHE BOOL "Set to OFF to skip build/install lcio.jar" FORCE )
+
+#############################################################################
+# CLHEP
+#############################################################################
+
+# Enable this for building LCIO with CLHEP
+#SET( BUILD_WITH "CLHEP" CACHE STRING "Build LCIO with these optional packages" FORCE )
+
+# Path to CLHEP
+SET( CLHEP_HOME "${ILC_HOME}/CLHEP/2.0.2.2"
+    CACHE PATH "Path to CLHEP" FORCE )
+
+#############################################################################
 # Project options
 #############################################################################
 
-#SET( BUILD_WITH "CLHEP" CACHE STRING "Build LCIO with these optional packages" FORCE )
 
 #SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
 #SET( INSTALL_DOC OFF CACHE BOOL "Set to OFF to skip build/install Documentation" FORCE )
-#SET( INSTALL_JAR OFF CACHE BOOL "Set to OFF to skip build/install lcio.jar" FORCE )
 #SET( BUILD_LCIO_TESTJOBS ON CACHE BOOL "Set to ON to build LCIO testjobs" FORCE )
 #SET( BUILD_F77_TESTJOBS ON CACHE BOOL "Set to ON to build LCIO F77 testjobs" FORCE )
 
