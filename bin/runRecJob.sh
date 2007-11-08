12fea8580b42a5472c9e4a9ec6d0d82e89b5a289
Frank Gaede <frank.gaede@desy.de>
2007-11-08 17:37:29 +0000

    added new jar files for sio to CLASSPATH

--- bin/runRecJob.sh
+++ bin/runRecJob.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 LOCALCLASSPATH=$LCIO/lib/lcio.jar:$LCIO/tools/sio.jar
+LOCALCLASSPATH=$LOCALCLASSPATH:$LCIO/tools/freehep-sio-2.0.jar:$LCIO/tools/freehep-xdr-2.0.3.jar
 
 # OS specific support for Cygwin 
 cygwin=false;
