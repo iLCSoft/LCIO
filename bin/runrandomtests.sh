79b50d7b3d2db9d954cf6c32231ab55d2fa57904
Jan Engels <jan.engels@desy.de>
2008-06-03 10:13:48 +0000

    fixed bug in path to copyfix

--- bin/runrandomtests.sh
+++ bin/runrandomtests.sh
@@ -3,22 +3,15 @@
 LOCALCLASSPATH=$LCIO/lib/lcio.jar:$LCIO/tools/sio.jar
 LOCALCLASSPATH=$LOCALCLASSPATH:$LCIO/tools/freehep-sio-2.0.jar:$LCIO/tools/freehep-xdr-2.0.3.jar
 
-# OS specific support for Cygwin 
-cygwin=false;
 case "`uname`" in
-  CYGWIN*) cygwin=true ;;
+  CYGWIN*)
+    # For Cygwin, switch paths to Windows format before running java
+    LOCALCLASSPATH=`cygpath --path --windows "$LOCALCLASSPATH"`
+    LCIO=`cygpath --path --windows "$LCIO"`
+  ;;
 esac
-# For Cygwin, switch paths to Windows format before running java
-if $cygwin; then
-  LOCALCLASSPATH=`cygpath --path --windows "$LOCALCLASSPATH"`
-  java -cp "$LOCALCLASSPATH" hep.lcio.test.RandomEventTest
-  java -cp "$LOCALCLASSPATH" hep.lcio.test.CreateRandomEvent rand_java.slcio
-  ./bin/copyfix rand_java.slcio rand_cpp.slcio
-  java -cp "$LOCALCLASSPATH" hep.lcio.test.CompareEvents rand_java.slcio rand_cpp.slcio
-else
-  java -cp $LOCALCLASSPATH hep.lcio.test.RandomEventTest
-  java -cp $LOCALCLASSPATH hep.lcio.test.CreateRandomEvent rand_java.slcio
-  ./bin/copyfix rand_java.slcio rand_cpp.slcio
-  java -cp $LOCALCLASSPATH hep.lcio.test.CompareEvents rand_java.slcio rand_cpp.slcio
-fi
 
+java -cp $LOCALCLASSPATH hep.lcio.test.RandomEventTest
+java -cp $LOCALCLASSPATH hep.lcio.test.CreateRandomEvent rand_java.slcio
+$LCIO/bin/copyfix rand_java.slcio rand_cpp.slcio
+java -cp $LOCALCLASSPATH hep.lcio.test.CompareEvents rand_java.slcio rand_cpp.slcio
