0f7151765f5b8b2c295a3394938d4937bc962294
Frank Gaede <frank.gaede@desy.de>
2009-09-18 09:37:11 +0000

    common RIO constants

---
+++ src/cpp/include/RIO/RIO.h
@@ -0,0 +1,23 @@
+#ifndef RIO_defines_h
+#define RIO_defines_h 1
+
+
+/** Global defines for ROOT I/O
+ */
+
+
+#define RIO_FILE_EXTENSION     ".rlcio"
+
+#define RIO_LCIO_TREENAME         "LCIO"
+#define RIO_LCIO_TREENAME_CYCLE_1 "LCIO;1"   // root way of versioning 
+
+#define RIO_LCEVENT_BRANCHNAME    "LCEvent"
+
+
+#define RIO_RECORD_SIZE    16000
+
+#define RIO_SPLIT_LEVEL        1
+
+#define RIO_DEFAULT_COMPRESS   4  // what is a reasonable default ?
+
+#endif
