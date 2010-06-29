9796006a65872f793e92f16550d777f27b7544d2
Jan Engels <jan.engels@desy.de>
2010-06-29 09:57:11 +0000

    upgraded version to v01-51

--- src/cpp/include/lcio.h
+++ src/cpp/include/lcio.h
@@ -7,7 +7,7 @@
  * documentation with doxygen. 
  * 
  * @author gaede
- * @version $Id: lcio.h,v 1.36 2010-04-30 15:28:24 engels Exp $ 
+ * @version $Id: lcio.h,v 1.37 2010-06-29 09:57:11 engels Exp $ 
  * @see LCEvent
  */
 #include "EVENT/LCIO.h"
@@ -15,7 +15,7 @@
 #include "IMPL/LCIOExceptionHandler.h"
 
 
-/** \mainpage <a href="http://lcio.desy.de">LCIO</a> (v01-13) 
+/** \mainpage <a href="http://lcio.desy.de">LCIO</a> (v01-51) 
  * A persistency framework for linear collider simulation studies.
  * \section Overview
  * This is the API documentation for the C++ version of LCIO. For general documetation on 
