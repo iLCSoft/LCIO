bd8f3e284d3636a32e80b492920a84774ecf0cfb
Frank Gaede <frank.gaede@desy.de>
2011-03-13 12:42:20 +0000

    fixed class LCFourVector (constness etc.) - added simple test

--- src/cpp/include/UTIL/LCFourVector.h
+++ src/cpp/include/UTIL/LCFourVector.h
@@ -30,7 +30,7 @@ namespace UTIL {
   class LCFourVector :  public HepLorentzVector {
 
   protected: 
-    TT* _lcObj ;
+    const TT* _lcObj ;
     
   public: 
     virtual ~LCFourVector() { /*no_op*/; } 
@@ -45,9 +45,9 @@ namespace UTIL {
      *  Can be used to save the dynamic_cast to the explicit type from an LCCollection element,
      *  e.g. LCFourVector<MCParticle>( col->getElement(i) ).
      */
-    LCFourVector(EVENT::LCObject* lcObj){
+    LCFourVector(const EVENT::LCObject* lcObj){
       
-      _lcObj = dynamic_cast< TT* >( lcObj ) ;
+      _lcObj = dynamic_cast< const TT* >( lcObj ) ;
       
       if( _lcObj == 0 )
 	throw EVENT::Exception("Dynamic cast failed for LCFourVector() !") ;
@@ -62,14 +62,14 @@ namespace UTIL {
      *    particle4V->getMomentum()    //  LCIO object   <br>
      *    particle4V.m()               // mass from HepLorentzVector  <br>
      */
-    TT* operator->() { return _lcObj ; } 
+    const TT* operator->() const { return _lcObj ; } 
 
 
     /** Pointer to the LCObject that has been used to create the four vector.
      *  To be used when the object is needed as a function argument or optionally
      *  to call methods of the object, e.g. particle4V.lcObj()->getMomentum() ;
      */
-    TT* lcObj() { return _lcObj ; } 
+    const TT* lcObj() const { return _lcObj ; } 
 
 
     
---
+++ src/cpp/src/TESTS/test_fourvector.cc
@@ -0,0 +1,54 @@
+////////////////////////////////////////
+// example for LCIO tests
+////////////////////////////////////////
+
+#include "tutil.h"
+#include "lcio.h"
+
+#include "EVENT/LCIO.h"
+//#include "IO/LCWriter.h"
+//#include "IMPL/LCEventImpl.h" 
+//#include "IMPL/LCCollectionVec.h"
+#include "IMPL/MCParticleImpl.h"
+#include "IMPL/ReconstructedParticleImpl.h"
+#include "UTIL/LCFourVector.h"
+
+
+#include <iostream>
+
+using namespace std ;
+using namespace lcio ;
+
+// replace mytest with the name of your test
+const static string testname="test_fourvector";
+
+//=============================================================================
+
+int main(int argc, char** argv ){
+    
+    // this should be the first line in your test
+    TEST MYTEST=TEST( testname, std::cout );
+    
+    MCParticleImpl* mcp = new MCParticleImpl ;
+    
+    float p[3] ;
+    p[0] = 1.0 ;
+    p[1] = 2.0 ;
+    p[2] = 3.0 ;
+
+    mcp->setMomentum( p ) ;
+    mcp->setMass( 42. ) ;
+
+    LCFourVector<MCParticle> mcp4v( mcp ) ;
+    
+    MYTEST( mcp4v.px()  , 1.0 ,  "px != 1.0 "   ) ;
+    MYTEST( mcp4v.py()  , 2.0 ,  "py != 2.0 "   ) ;
+    MYTEST( mcp4v.pz()  , 3.0 ,  "pz != 3.0 "   ) ;
+   
+    MYTEST( mcp4v.m()  , 42.0 ,  " e != 42.0 "   ) ;
+    
+    return 0;
+}
+
+//=============================================================================
+
