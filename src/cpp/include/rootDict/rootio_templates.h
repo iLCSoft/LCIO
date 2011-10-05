f0a8a262516dc5878aabbf5565f8467a2f387e35
Frank Gaede <frank.gaede@desy.de>
2011-10-05 14:55:09 +0000

    - added missing template instantiantions for vectors of LCIO classes

--- src/cpp/include/rootDict/rootio_templates.h
+++ src/cpp/include/rootDict/rootio_templates.h
@@ -4,19 +4,35 @@
 
 #include "UTIL/LCTypedVector.h"
 
-#include "IMPL/ReconstructedParticleImpl.h"
+#include "IMPL/AccessChecked.h"
+#include "IMPL/CalorimeterHitImpl.h"
+#include "IMPL/ClusterImpl.h"
+#include "IMPL/LCCollectionVec.h"
+#include "IMPL/LCEventImpl.h"
+#include "IMPL/LCFlagImpl.h"
+#include "IMPL/LCGenericObjectImpl.h"
+#include "IMPL/LCIOExceptionHandler.h"
+#include "IMPL/LCParametersImpl.h"
+#include "IMPL/LCRelationImpl.h"
+#include "IMPL/LCRunHeaderImpl.h"
+#include "IMPL/LCTOOLS.h"
 #include "IMPL/MCParticleImpl.h"
+#include "IMPL/ParticleIDImpl.h"
+#include "IMPL/RawCalorimeterHitImpl.h"
+#include "IMPL/ReconstructedParticleImpl.h"
 #include "IMPL/SimCalorimeterHitImpl.h"
 #include "IMPL/SimTrackerHitImpl.h"
-#include "IMPL/CalorimeterHitImpl.h"
-#include "IMPL/TrackerHitImpl.h"
-#include "IMPL/VertexImpl.h"
+#include "IMPL/TPCHitImpl.h"
 #include "IMPL/TrackImpl.h"
-#include "IMPL/ClusterImpl.h"
-#include "IMPL/LCRelationImpl.h"
-#include "IMPL/LCCollectionVec.h"
-//#include "IMPL/LCRefVec.h"
-//#include "TRefArray.h"
+#include "IMPL/TrackStateImpl.h"
+#include "IMPL/TrackerDataImpl.h"
+#include "IMPL/TrackerHitImpl.h"
+#include "IMPL/TrackerHitPlaneImpl.h"
+#include "IMPL/TrackerHitZCylinderImpl.h"
+#include "IMPL/TrackerPulseImpl.h"
+#include "IMPL/TrackerRawDataImpl.h"
+#include "IMPL/VertexImpl.h"
+
 
 #include <string>
 #include <vector>
@@ -31,72 +47,47 @@
 #pragma link C++ class std::map<std::string,EVENT::LCCollection*>+;
 #pragma link C++ class std::pair<std::string,EVENT::LCCollection*>+;
 
-#pragma link C++ class std::vector<IMPL::ReconstructedParticleImpl*>+;
-#pragma link C++ class std::vector<IMPL::MCParticleImpl*>+;
-#pragma link C++ class std::vector<IMPL::SimTrackerHitImpl*>+;
-#pragma link C++ class std::vector<IMPL::SimCalorimeterHitImpl*>+;
-#pragma link C++ class std::vector<IMPL::TrackImpl*>+;
-#pragma link C++ class std::vector<IMPL::ClusterImpl*>+;
-#pragma link C++ class std::vector<IMPL::LCRelationImpl*>+;
-#pragma link C++ class std::vector<IMPL::VertexImpl*>+;
 
-#pragma link C++ class std::vector<EVENT::LCObject*>+;
-#pragma link C++ class std::vector<EVENT::ReconstructedParticle*>+;
-#pragma link C++ class std::vector<EVENT::MCParticle*>+;
-#pragma link C++ class std::vector<EVENT::SimTrackerHit*>+;
-#pragma link C++ class std::vector<EVENT::SimCalorimeterHit*>+;
-#pragma link C++ class std::vector<EVENT::Track*>+;
+#pragma link C++ class std::vector<EVENT::CalorimeterHit*>+;
 #pragma link C++ class std::vector<EVENT::Cluster*>+;
+#pragma link C++ class std::vector<EVENT::LCGenericObject*>+;
+#pragma link C++ class std::vector<EVENT::LCObject*>+;
 #pragma link C++ class std::vector<EVENT::LCRelation*>+;
+#pragma link C++ class std::vector<EVENT::MCParticle*>+;
+#pragma link C++ class std::vector<EVENT::ParticleID*>+;
+#pragma link C++ class std::vector<EVENT::RawCalorimeterHit*>+;
+#pragma link C++ class std::vector<EVENT::ReconstructedParticle*>+;
+#pragma link C++ class std::vector<EVENT::SimCalorimeterHit*>+;
+#pragma link C++ class std::vector<EVENT::SimTrackerHit*>+;
+#pragma link C++ class std::vector<EVENT::TrackState*>+;
+#pragma link C++ class std::vector<EVENT::TrackerData*>+;
+#pragma link C++ class std::vector<EVENT::TrackerHit*>+;
+#pragma link C++ class std::vector<EVENT::TrackerHitPlane*>+;
+#pragma link C++ class std::vector<EVENT::TrackerHitZCylinder*>+;
+#pragma link C++ class std::vector<EVENT::TrackerPulse*>+;
+#pragma link C++ class std::vector<EVENT::TrackerRawData*>+;
 #pragma link C++ class std::vector<EVENT::Vertex*>+;
 
-// #pragma link C++ class IMPL::LCRefVec<EVENT::LCObject*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::ReconstructedParticle*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::MCParticle*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::SimTrackerHit*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::CalorimeterHit*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::TrackerHit*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::SimCalorimeterHit*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::Track*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::Cluster*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::LCRelation*>+;
-// #pragma link C++ class IMPL::LCRefVec<EVENT::Vertex*>+;
 
-// #pragma link C++ class IMPL::LCRef<EVENT::LCObject*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::ReconstructedParticle*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::MCParticle*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::SimTrackerHit*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::SimCalorimeterHit*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::TrackerHit*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::CalorimeterHit*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::Track*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::Cluster*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::LCRelation*>+;
-// #pragma link C++ class IMPL::LCRef<EVENT::Vertex*>+;
+#pragma link C++ class std::vector<IMPL::CalorimeterHitImpl*>+;
+#pragma link C++ class std::vector<IMPL::ClusterImpl*>+;
+#pragma link C++ class std::vector<IMPL::LCGenericObjectImpl*>+;
 
-// //#pragma link C++ class IMPL::LCCollectionTVec<EVENT::LCObject>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::ReconstructedParticle>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::MCParticle>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::SimTrackerHit>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::SimCalorimeterHit>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::TrackerHit>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::CalorimeterHit>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::Track>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::Cluster>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::LCRelation>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<EVENT::Vertex>+;
-
-
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::ReconstructedParticleImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::MCParticleImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::SimTrackerHitImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::SimCalorimeterHitImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::TrackerHitImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::CalorimeterHitImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::TrackImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::ClusterImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::LCRelationImpl>+;
-// #pragma link C++ class IMPL::LCCollectionTVec<IMPL::VertexImpl>+;
+#pragma link C++ class std::vector<IMPL::LCRelationImpl*>+;
+#pragma link C++ class std::vector<IMPL::MCParticleImpl*>+;
+#pragma link C++ class std::vector<IMPL::ParticleIDImpl*>+;
+#pragma link C++ class std::vector<IMPL::RawCalorimeterHitImpl*>+;
+#pragma link C++ class std::vector<IMPL::ReconstructedParticleImpl*>+;
+#pragma link C++ class std::vector<IMPL::SimCalorimeterHitImpl*>+;
+#pragma link C++ class std::vector<IMPL::SimTrackerHitImpl*>+;
+#pragma link C++ class std::vector<IMPL::TrackStateImpl*>+;
+#pragma link C++ class std::vector<IMPL::TrackerDataImpl*>+;
+#pragma link C++ class std::vector<IMPL::TrackerHitImpl*>+;
+#pragma link C++ class std::vector<IMPL::TrackerHitPlaneImpl*>+;
+#pragma link C++ class std::vector<IMPL::TrackerHitZCylinderImpl*>+;
+#pragma link C++ class std::vector<IMPL::TrackerPulseImpl*>+;
+#pragma link C++ class std::vector<IMPL::TrackerRawDataImpl*>+;
+#pragma link C++ class std::vector<IMPL::VertexImpl*>+;
 
 
 #endif
