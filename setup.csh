4e25a91c02cc66feff1e80beb38637808097246d
Christian Grefe <Christian.Grefe@cern.ch>
2013-05-17 12:12:02 +0000

    Set the python path environment

--- setup.csh
+++ setup.csh
@@ -1,3 +1,5 @@
 #!/bin/sh
 setenv LCIO `pwd`
 setenv PATH $LCIO/bin:$LCIO/tools:$PATH
+setenv PYTHONPATH $LCIO/src/python:$PYTHONPATH
+alias pylcio='python $LCIO/src/python/pylcio.py'
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export LCIO=`pwd`
 export PATH=$LCIO/bin:$LCIO/tools:$PATH
+export PYTHONPATH=$LCIO/src/python:$PYTHONPATH
+alias pylcio='python $LCIO/src/python/pylcio.py'
