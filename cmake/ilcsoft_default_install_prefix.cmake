2314366dff369ba5166fd443f390b9e546144485
Frank Gaede <frank.gaede@desy.de>
2015-10-15 15:48:08 +0000

    - fixed setting of CMAKE_INSTALL_PREFIX to allow    installation in 'usr/local'

--- cmake/ilcsoft_default_install_prefix.cmake
+++ cmake/ilcsoft_default_install_prefix.cmake
@@ -1,9 +1,8 @@
 # set default install prefix to project root directory
 # instead of the cmake default /usr/local
-IF( CMAKE_INSTALL_PREFIX STREQUAL "/usr/local" )
-    SET( CMAKE_INSTALL_PREFIX "${PROJECT_SOURCE_DIR}" )
-ENDIF()
 
-# write this variable to cache
-SET( CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" CACHE PATH "Where to install ${PROJECT_NAME}" FORCE )
+IF(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+ 	SET(CMAKE_INSTALL_PREFIX "${PROJECT_SOURCE_DIR}" CACHE PATH "Where to install ${PROJECT_NAME}" FORCE)
+ENDIF(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+
 
