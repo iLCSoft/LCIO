1b9734c894d274ca037acdc8c1d81909dee990cc
Marko Petric <marko.petric@cern.ch>
2016-12-08 10:29:54 +0100

    Add CI

---
+++ .travis-ci.d/compile_and_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
+source $ILCSOFT/init_ilcsoft.sh
+
+cd /Package
+mkdir build
+cd build
+cmake -GNinja -DUSE_CXX11=ON ..
+ninja
+ctest --output-on-failure
