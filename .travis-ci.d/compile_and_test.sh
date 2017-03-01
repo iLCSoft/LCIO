ff539fb2c8dee507b0dbd2f572659f54e9676c5e
Marko Petric <marko.petric@cern.ch>
2017-03-01 11:35:28 +0100

    Add ands between statements

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -GNinja -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" ..
-ninja
-ninja install
+cmake -GNinja -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
+ninja && \
+ninja install && \
 ctest --output-on-failure
