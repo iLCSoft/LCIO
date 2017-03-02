542fd59cc6ed7debf502e7b7ed2bc0204319d55f
Frank Gaede <frank.gaede@desy.de>
2017-03-02 08:23:25 +0100

    update ilcsoft_default_cxx_flags.cmake from ilcutil
    A.Sailer: improve checking of variables
    removed the -+ signs from variables for the result variable. This variable is passed as a macro to the compilation test
    and produces a warning otherwise. If using Werror this then fails

--- cmake/ilcsoft_default_cxx_flags.cmake
+++ cmake/ilcsoft_default_cxx_flags.cmake
@@ -4,24 +4,27 @@
 
 INCLUDE(CheckCXXCompilerFlag)
 
-SET(COMPILER_FLAGS -Wall -Wextra -Wshadow -Weffc++ -pedantic -Wno-long-long -Wuninitialized -fdiagnostics-color=auto )
+SET(COMPILER_FLAGS -Wall -Wextra -Wshadow -Weffc++ -pedantic -Wno-long-long -Wuninitialized )
 
-IF(("${CMAKE_CXX_COMPILER_ID}" EQUAL "GNU") OR ("${CMAKE_CXX_COMPILER_ID}" EQUAL "Clang"))
-    SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
-ENDIF()
-
-IF("${CMAKE_CXX_COMPILER_ID}" EQUAL "AppleClang")
-    SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-undefined,error")
+IF( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" )
+ LIST( APPEND COMPILER_FLAGS -Wl,-no-undefined )
 ENDIF()
 
 
 MESSAGE( STATUS "FLAGS ${COMPILER_FLAGS}" )
 FOREACH( FLAG ${COMPILER_FLAGS} )
-  CHECK_CXX_COMPILER_FLAG( "${FLAG}" CXX_FLAG_WORKS_${FLAG} )
-  IF( ${CXX_FLAG_WORKS_${FLAG}} )
+
+  ## meed to replace the minus or plus signs from the variables, because it is passed
+  ## as a macro to g++ which causes a warning about no whitespace after macro
+  ## definition
+  STRING(REPLACE "-" "_" FLAG_WORD ${FLAG} )
+  STRING(REPLACE "+" "P" FLAG_WORD ${FLAG_WORD} )
+
+  CHECK_CXX_COMPILER_FLAG( "${FLAG}" CXX_FLAG_WORKS_${FLAG_WORD} )
+  IF( ${CXX_FLAG_WORKS_${FLAG_WORD}} )
     MESSAGE ( STATUS "Adding ${FLAG} to CXX_FLAGS" )
     ### We prepend the flag, so they are overwritten by whatever the user sets in his own configuration
-    SET ( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS} ")
+    SET ( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS}")
   ELSE()
     MESSAGE ( STATUS "NOT Adding ${FLAG} to CXX_FLAGS" )
   ENDIF()
@@ -30,12 +33,17 @@ ENDFOREACH()
 OPTION( USE_CXX11 "Use CXX Standard 2011" True )
 IF( USE_CXX11 )
   SET( FLAG "-std=c++11" )
-  CHECK_CXX_COMPILER_FLAG( ${FLAG} CXX_FLAG_WORKS_${FLAG} )
-  IF( ${CXX_FLAG_WORKS_${FLAG}} )
-    SET( CMAKE_CXX_FLAGS " ${FLAG} ${CMAKE_CXX_FLAGS}")
+  SET( FLAG_WORD "stdCXX11" )
+  CHECK_CXX_COMPILER_FLAG( ${FLAG} CXX_FLAG_WORKS_${FLAG_WORD} )
+  IF( ${CXX_FLAG_WORKS_${FLAG_WORD}} )
+    SET( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS}")
   ELSE()
     MESSAGE( FATAL_ERROR "Cannot add ${FLAG} to CMAKE_CXX_FLAGS, but c++11 was requested, check your compiler" )
   ENDIF()
 ELSE()
   MESSAGE( STATUS "NOT building with CXX11 standard" )
+ENDIF()
+
+IF( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 4.9 )
+  SET( CMAKE_CXX_FLAGS "-fdiagnostics-color=auto ${CMAKE_CXX_FLAGS}" )
 ENDIF()
