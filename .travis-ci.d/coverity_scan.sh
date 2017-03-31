d5872df8af8afdbea7cbd3539a979600ce1c4b9d
Marko Petric <marko.petric@cern.ch>
2017-03-31 23:10:24 +0200

    Modify .travis.yml to integrate with a travis cron to submit automatically coverity scans

---
+++ .travis-ci.d/coverity_scan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
+source $ILCSOFT/init_ilcsoft.sh
+
+cd /Package
+mkdir build
+cd build
+cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON .. && \
+export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
+cov-build --dir cov-int make -j2 && \
+tar czvf myproject.tgz cov-int
