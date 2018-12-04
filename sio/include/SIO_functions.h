8ec8524d06afccce2911ee8144d3f071bd5d1d39
Ete Remi <remi.ete@desy.de>
2018-12-04 15:20:00 +0100

    Moved definitions to SIO_definitions.h. Added pre-processor condition to switch on/off the new macros definitions SIO_DATA, SIO_PNTR and SIO_PTAG.

--- sio/include/SIO_functions.h
+++ sio/include/SIO_functions.h
@@ -14,90 +14,39 @@
 #ifndef SIO_FUNCTIONS_H
 #define SIO_FUNCTIONS_H 1
 
-#define SIO_LEN_SB        1
-#define SIO_LEN_DB        2
-#define SIO_LEN_QB        4
-#define SIO_LEN_OB        8
-
-// ----------------------------------------------------------------------------
-// Deal with declarations for 64 bit integers.  This really amounts to "who is
-// ANSI compiant?"  ANSI defines "long long" as the declaration for 64 bit
-// integers.  M$ hasn't caught up yet and still requires __int64.
-//
-// Branch on flag provided by compiler:
-//
-// OS            CPU           Macro         Provided by   Declaration
-// ------------  ------------  ------------  ------------  -----------
-// AIX           PPC(?)        _AIX          GNU compiler  long long
-// OSF1          Alpha         __alpha__     GNU compiler  long long
-// Linux         x86           __i386__      GNU compiler  long long
-// Linux         Opteron       _LP64         GNU compiler  long long
-// Linux         Itanium       _LP64         GNU compiler  long long
-// SunOS         Sparc         __sparc__     GNU compiler  long long
-// Windows/NT    Alpha         _M_ALPHA      VC  compiler  __int64
-// Windows/NT    x86           _M_IX86       VC  compiler  __int64
-// Windows/NT    MIPS          _M_MRX000     VC  compiler  __int64
-// Windows/NT    PPC           _M_PPC        VC  compiler  __int64
-// ----------------------------------------------------------------------------
-#if defined(_AIX)      ||  defined(__alpha__) || defined(__i386__)  || defined(__sparc__) || defined(__APPLE_CC__) || defined(_LP64)
-
-// fg: gcc complains about long long - what to do about it ?
-// warning: ANSI C++ does not support `long long'
- #define SIO_64BITINT   long long
-// #define SIO_64BITINT   long
-#endif
-
-#if defined(_M_ALPHA)  || defined(_M_IX86)   || defined(_M_PPC)
- #define SIO_64BITINT   __int64
-#endif
-
-// ----------------------------------------------------------------------------
-// Deal with pointer length.  Currently, the only problem is alpha which uses
-// 64 bit pointers.                                                          
-//                                                              
-// OS            CPU           Macro         Provided by   Pointer size
-// ------------  ------------  ------------  ------------  -----------
-// AIX           PPC(?)        _AIX          GNU compiler  4 bytes
-// OSF1          Alpha         __alpha__     GNU compiler  8 bytes
-// Linux         x86           __i386__      GNU compiler  4 bytes
-// Linux         Opteron       _LP64    _    GNU compiler  8 bytes
-// Linux         Itanium       _LP64         GNU compiler  8 bytes
-// SunOS         Sparc         __sparc__     GNU compiler  4 bytes
-// Windows/NT    Alpha         _M_ALPHA      VC  compiler  8 bytes
-// Windows/NT    x86           _M_IX86       VC  compiler  4 bytes
-// Windows/NT    MIPS          _M_MRX000     VC  compiler  ? bytes
-// Windows/NT    PPC           _M_PPC        VC  compiler  4 bytes
-// ----------------------------------------------------------------------------
-//#if defined(__alpha__) || defined(_M_ALPHA) || defined(_LP64)
-// #define SIO_POINTER_DECL   size_t
-//#endif
-
-//#if defined(_AIX)      || defined(__i386__)  || defined(__sparc__) || defined(_M_IX86) || defined(_M_PPC) || defined(__APPLE_CC__)
-// #define SIO_POINTER_DECL   unsigned int
-//#endif
-
-// Old check was problematic because both macros could evaluate to true, and it would
-// also be possible that SIO_POINTER_DECL was not defined at all.  Change as needed.
-// --JM 
-#if defined(__alpha__) || defined(_M_ALPHA) || defined(_LP64)
- #define SIO_POINTER_DECL   size_t
-#else
- #define SIO_POINTER_DECL   unsigned int
-#endif
-
 //
 // Take the drudgery out of error handling.
 //
+#ifdef SIO_TS_IMPL
+
+#define SIO_DATA( rec, pnt, cnt ) { \
+  status = (rec->mode() != SIO_MODE_READ) ? \
+    rec->write_data(pnt, cnt) : \
+    rec->read_data(pnt, cnt); \
+  if( !(status & 1) ) \
+    return status; \
+}
+#define SIO_PNTR( rec, pnt ) { \
+  status = (rec->mode() != SIO_MODE_READ) ? \
+    rec->write_pointer_to( (SIO_POINTER_DECL *)(pnt) ) : \
+    rec->read_pointer_to( (SIO_POINTER_DECL *)(pnt) ); \
+  if( !(status & 1) ) \
+    return status; \
+}
+#define SIO_PTAG( rec, pnt ) { \
+  status = (rec->mode() != SIO_MODE_READ) ? \
+    rec->write_pointed_at( (SIO_POINTER_DECL *)(pnt) ) : \
+    rec->read_pointed_at( (SIO_POINTER_DECL *)(pnt) ); \
+  if( !(status & 1) ) \
+    return status; \
+}
+#else
+
 #define SIO_DATA( rec, pnt, cnt ) status = SIO_functions::data( (rec), (pnt), (cnt) ); if( !(status & 1) ) return status;
-
 #define SIO_PNTR( rec, pnt )   status = SIO_functions::pointer_to( (rec), (SIO_POINTER_DECL *)(pnt) );   if( !(status & 1) )       return status;
-
 #define SIO_PTAG( rec, pnt )   status = SIO_functions::pointed_at( (rec), (SIO_POINTER_DECL *)(pnt) );   if( !(status & 1) )       return status;
 
-//
-// This turns up far too often to be ignored.
-//
-#define UCHR_CAST(pntr)    (reinterpret_cast<unsigned char*>((pntr)))
+#endif
 
 class SIO_stream;
 
