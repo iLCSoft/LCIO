ea8f46c7d28bfaec343e732d3b0c86766c51f9c7
Ete Remi <remi.ete@desy.de>
2018-12-05 18:08:00 +0100

    Moved out all stream operations from SIO_functions to stream class, where it should belong. Keep only the hard part of IO: endianess treatment and miscellaneous functions

--- sio/include/SIO_functions.h
+++ sio/include/SIO_functions.h
@@ -14,73 +14,48 @@
 #ifndef SIO_FUNCTIONS_H
 #define SIO_FUNCTIONS_H 1
 
-//
-// Take the drudgery out of error handling.
-//
-#ifdef SIO_TS_IMPL
-
-#define SIO_DATA( rec, pnt, cnt ) { \
-  status = (rec->mode() != SIO_MODE_READ) ? \
-    rec->write_data(pnt, cnt) : \
-    rec->read_data(pnt, cnt); \
-  if( !(status & 1) ) \
-    return status; \
-}
-#define SIO_PNTR( rec, pnt ) { \
-  status = (rec->mode() != SIO_MODE_READ) ? \
-    rec->write_pointer_to( (SIO_POINTER_DECL *)(pnt) ) : \
-    rec->read_pointer_to( (SIO_POINTER_DECL *)(pnt) ); \
-  if( !(status & 1) ) \
-    return status; \
-}
-#define SIO_PTAG( rec, pnt ) { \
-  status = (rec->mode() != SIO_MODE_READ) ? \
-    rec->write_pointed_at( (SIO_POINTER_DECL *)(pnt) ) : \
-    rec->read_pointed_at( (SIO_POINTER_DECL *)(pnt) ); \
-  if( !(status & 1) ) \
-    return status; \
-}
-#else
-
-#define SIO_DATA( rec, pnt, cnt ) status = SIO_functions::data( (rec), (pnt), (cnt) ); if( !(status & 1) ) return status;
-#define SIO_PNTR( rec, pnt )   status = SIO_functions::pointer_to( (rec), (SIO_POINTER_DECL *)(pnt) );   if( !(status & 1) )       return status;
-#define SIO_PTAG( rec, pnt )   status = SIO_functions::pointed_at( (rec), (SIO_POINTER_DECL *)(pnt) );   if( !(status & 1) )       return status;
-
-#endif
-
-class SIO_stream;
-
-class SIO_functions
-{
-public:
-
-    static unsigned int data( SIO_stream*,          char*,         const int );
-    static unsigned int data( SIO_stream*, unsigned char*,         const int );
-    static unsigned int data( SIO_stream*,          short*,        const int );
-    static unsigned int data( SIO_stream*, unsigned short*,        const int );
-    static unsigned int data( SIO_stream*,          int*,          const int );
-    static unsigned int data( SIO_stream*, unsigned int*,          const int );
-    static unsigned int data( SIO_stream*,          SIO_64BITINT*, const int );
-    static unsigned int data( SIO_stream*, unsigned SIO_64BITINT*, const int );
-    static unsigned int data( SIO_stream*,          float*,        const int );
-    static unsigned int data( SIO_stream*,          double*,       const int );
-
-    static unsigned int pointed_at( SIO_stream*, SIO_POINTER_DECL* );
-    static unsigned int pointer_to( SIO_stream*, SIO_POINTER_DECL* );
-
-    static bool validateName( const char* );
+namespace sio {
+  
+  /**
+   *  @brief  functions class
+   *          Helper class for serialization
+   */
+  class functions {
+  public:
+    /**
+     *  @brief  Tests the validity (according to SIO) of a name
+     *          SIO only accepts names starting with (regular expression) [A-Za-z_] and continuing
+     *          with [A-Za-z0-9_] (which most people will recognize as the definition of a C/C++ variable name).
+     * 
+     *  @param  name the name to validate
+     */
+    static bool validateName( const char* name );
     
-    static void copy( unsigned char*, unsigned char*, 
-                      const int,      const int      );
-
-private:
-
-
-    static unsigned int xfer( SIO_stream*,    const int, 
-                              const int,      unsigned char* );
-
-    friend class SIO_stream;              // Direct access to copy
-    friend class SIO_record;              // Direct access to copy
-}; 
+    /**
+     *  @brief  Primitive copying functions.
+     *  
+     *  This is where the endian-ness problem is resolved.  Note that there are two
+     *  implementations of SIO_functions::copy.  Only (at most) one version gets
+     *  passed to the compiler by the preprocesor.  In the case of a new target CPU
+     *  whose endian-ness cannot determined by the preprocessor directives above,
+     *  -no- version is passed through to the compiler.  The result is a compilation
+     *  failure (i.e. you can't compile the SIO package on a particular CPU if the
+     *  CPU's endian-ness cannot be determined by the SIO preprocessor directives).
+     *  In SIO all datasets have the same endian-ness.  The choice between a little-
+     *  or a big-endian dataset representation should be driven by the type of CPU
+     *  which will do most of the work (swapping the endian-ness on read or
+     *  write is somewhat inefficient).  In a ditributed multi-platform environment
+     *  that's a tough call, so the chice is really somewhat arbitrary.  SIO uses
+     *  big-endian xdr-like data representation.
+     *  
+     *  @param  from buffer or data source to copy
+     *  @param  to buffer or variable destination to copy to
+     *  @param  size the size in memory of the variable to copy
+     *  @param  count the number of variable to copy
+     */
+    static void copy( unsigned char *from, unsigned char *to, const int size, const int count);
+  }; 
+  
+}
 
 #endif
