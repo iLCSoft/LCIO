8daac694aca46a76522a033bb1031ca18277f0ba
Ete Remi <remi.ete@desy.de>
2018-12-06 17:03:06 +0100

    Reduced LCSIO class to strictly needed. Removed all global variables there

--- src/cpp/include/SIO/LCSIO.h
+++ src/cpp/include/SIO/LCSIO.h
@@ -1,11 +1,14 @@
 #ifndef SIO_LCSIO_H
 #define SIO_LCSIO_H 1
 
+// -- lcio headers
 #include "LCIOTypes.h"
 
+// -- sio headers
 #include "SIO_record.h" 
 #include "SIO_definitions.h"
 
+// -- std headers
 #include <string>
 
 // record and block names used in LCIO-SIO
@@ -22,170 +25,71 @@
 #define LCSIO_INDEXRECORDNAME "LCIOIndex"
 #define LCSIO_INDEXBLOCKNAME  "LCIOIndex"
 
-
-class SIO_stream ;
-
 namespace SIO {
 
-  class LCSIO ;
-
-  /** Manager class that holds instances of all known LCIO-SIO records.
-   *  Provides Unpack class for setting subsets of records to be unpacked.
-   *  Every new record type needs to be added here in SIORecords::ID and 
-   *  SIORecords::Unpack.
-   */
-  class SIORecords {
-
-    friend class LCSIO ;
-  public:
-    
-    enum ID{
-      Event=0,
-      Header,
-      Run,
-      Access,
-      Index,
-      NumberOfRecords 
-    } ;
-    //-----------------------------------------------
-    /** Helper class that sets the unpack flag of known SIO records
-     *  as specified by the constructor. After an object of this class goes out of 
-     *  scope the original setting is restored. This is needed to have multiple
-     *  read streams (event overlay).
-     */
-    class Unpack{
-    public:
-      static const unsigned Event  = 0x0001 << SIORecords::Event ;
-      static const unsigned Header = 0x0001 << SIORecords::Header ;
-      static const unsigned Run    = 0x0001 << SIORecords::Run ;
-      static const unsigned Access = 0x0001 << SIORecords::Access ;
-      static const unsigned Index  = 0x0001 << SIORecords::Index ;
-
-      static const unsigned All = 0xFFFFFFFF ; 
-      
-      Unpack( unsigned recordFlag ) ;
-      ~Unpack() ;
-
-    protected:
-      bool _flags[ NumberOfRecords ] ;  
-    };
-    //-----------------------------------------------
-
-
-    SIO_record* operator[](size_t idx) ;
-
-    void setCompress( bool flag=true ) ;
-
-  protected:
-    void add(SIO_record*& rec, const char* name) ;
-
-    SIO_record* _records[ NumberOfRecords ] ;
-
-    SIORecords() ;
-  } ;
-
-
 #define LCSIO_READ( rec, pnt ) status = LCSIO::read( (rec), (pnt)  ); if( !(status & 1) ) return status;
-#define LCSIO_READ_LEN( rec, pnt , len ) status = LCSIO::read( (rec), (pnt) , (len)  ); if( !(status & 1) ) return status;
-
+// #define LCSIO_READ_LEN( rec, pnt , len ) status = LCSIO::read( (rec), (pnt) , (len)  ); if( !(status & 1) ) return status;
 #define LCSIO_WRITE( rec, pnt ) status = LCSIO::write( (rec), (pnt)  ); if( !(status & 1) ) return status;
 
-/** Collection of static helper  functions for reading and writing
- * data with SIO. Could go to the SIO_functions class.
- * Also defines some constant names for lcio-SIO.
+/** 
+ *  @brief  Collection of static helper  functions for reading and writing
+ *          data with SIO. Could go to the SIO_functions class.
+ *          Also defines some constant names for lcio-SIO.
  *
- * @author gaede
- * @version Mar 6, 2003
+ *  @author gaede
+ *  @version Mar 6, 2003
  */
   class LCSIO{
+  public:
+    /// The lcio file extension name
+    static const char* FILE_EXTENSION;
+
+    /**
+     *  @brief  Check for old version of LCIO (> v01-08 ar no longer supported)
+     *          Throws an exception if not supported
+     * 
+     *  @param  version the version to check
+     */
+    static void checkVersion(int versionID );
+
+    /** 
+     *  @brief  Read a string from the stream
+     */
+    static unsigned int read( SIO_stream* stream , std::string &c);
   
-  public :
+    /** 
+     *  @brief  Write an int to the stream.
+     */
+    static unsigned int write( SIO_stream* stream , int i);
+
+    /** 
+     *  @brief  Write an unsigned int to the stream.
+     */
+    static unsigned int write( SIO_stream* stream , unsigned int i);
   
-    static SIORecords& records() ;
-
-//     // define some names of records and blocks
-//     static const char* RUNRECORDNAME ; 
-//     static const char* RUNBLOCKNAME ; 
-//     static const char* EVENTRECORDNAME ; 
-//     static const char* EVENTBLOCKNAME;
-//     static const char* HEADERRECORDNAME ; 
-//     static const char* HEADERBLOCKNAME;
-
-     static const char* FILE_EXTENSION ;
-
-    /** the compression mode for SIO
-     */
-    static const bool COMPRESSION = true ;
-
-
-    /**Checks the version of the file - oldefile (version < v00-08) are no longer supported
-     */
-    static void checkVersion(int versionID ) ;
-
-    /** Read a string from the stream into a dummy buffer. 
-     * Warning the same buffer is used for each call.
-     * So the return value needs to be copied to its final memory destination.
-     * len holds the length of the string (can contain '\0').
-     */
-    static unsigned int read( SIO_stream* stream ,char** c , int* len=0) ;
-
-//     /** This version checks the versionId to be able to read 'old' files with 
-//      * trailing '\00' (version <= 00-02).
-//      * Remove this method after a reasonable transition period...
-//      */
-//     static unsigned int read( SIO_stream* stream ,char** c, int versionID) ;
-  
-    /** Write an int to the stream.
-     */
-    static unsigned int write( SIO_stream* stream , int i) ;
-
-    /** Write an unsigned int to the stream.
-     */
-    static unsigned int write( SIO_stream* stream , unsigned int i) ;
-  
-    /** Write a vector size to the stream (needed for AMD64 architectures)
-     */
 #if defined(_LP64)  || defined(__APPLE_CC__)
-//    static unsigned int write( SIO_stream* stream , SIO_POINTER_DECL i) ;
-    static unsigned int write( SIO_stream* stream , size_t i) ;
+    /**
+     *  @brief  Write a vector size to the stream (needed for AMD64 architectures)
+     */
+    static unsigned int write( SIO_stream* stream , size_t i);
 #endif
 
-    /** Write a 64bit integer to the stream 
+    /** 
+     *  @brief  Write a 64bit integer to the stream 
      */
-    static unsigned int write( SIO_stream* stream , EVENT::long64 i) ;
-
-    /** Write a float  to the stream.
+    static unsigned int write( SIO_stream* stream , EVENT::long64 i);
+    
+    /** 
+     *  @brief  Write a float  to the stream.
      */
-    static unsigned int write( SIO_stream* stream , float f) ;
-  
-    /** Write a string to the stream. Writes 
-     * the length of the string first and adds '\0' to the end.
+    static unsigned int write( SIO_stream* stream , float f);
+    
+    /** 
+     *  @brief  Write a string to the stream. Writes 
+     *          the length of the string first and adds '\0' to the end.
      */
-    static unsigned int write(SIO_stream* stream , const std::string& s) ;
-  
-  
-    /** Creates a valid SIO name (basically equivalent to a valid C++ name)
-     * by replacing every [.], [\] and [/] with underscores and removing all
-     * nonalphanumeric characters and adding an 'A' to the beginning, if necessary.
-     */
-    static std::string getValidSIOName(const std::string& aName ) ;
-    //    static const char* getValidSIOName(const std::string& aName ) ;
-
-
-    /**Seek stream to the given absolute position - if pos<0 from end of file.
-     * @throw IOException
-     */
-    static void seekStream( SIO_stream* stream , EVENT::long64 pos) ;
-
-
-  private:
-
-    static char* dummy  ;
-    static int dummy_size  ;
-    static int uid ;
-    static const int dummy_initial_size = 1024  ;
-
-  } ;
+    static unsigned int write(SIO_stream* stream , const std::string& s);
+  };
 
 } // namespace 
 #endif // ifndef SIO_LCSIO_H
--- src/cpp/src/SIO/LCSIO.cc
+++ src/cpp/src/SIO/LCSIO.cc
@@ -2,7 +2,6 @@
 
 #include "SIO_functions.h"
 #include "SIO_stream.h"
-#include "SIO_recordManager.h"
 #include <cctype>
 #include <cerrno>
 #include "Exceptions.h"
@@ -14,247 +13,50 @@ using namespace EVENT ;
 
 namespace SIO {
 
-  //-------------------------------
-  SIORecords::SIORecords(){
-   
-    add( _records[ Event ]  , LCSIO_EVENTRECORDNAME ) ;
-    add( _records[ Header ] , LCSIO_HEADERRECORDNAME ) ;
-    add( _records[ Run ]    , LCSIO_RUNRECORDNAME ) ;
-    add( _records[ Access ] , LCSIO_ACCESSRECORDNAME ) ;
-    add( _records[ Index ]  , LCSIO_INDEXRECORDNAME ) ;
-
-  }
-  SIO_record* SIORecords::operator[](size_t idx){
-
-    if( idx >= NumberOfRecords ) 
-      throw IO::IOException( " Out of range in SIORecords::operator[](size_t i)" ) ;
-
-    return _records[ idx ] ;
-  }
-
-  void SIORecords::add(SIO_record*& rec , const char* name){
-    
-    rec = SIO_recordManager::add( name ) ; 
-
-    if( rec == 0 ) {
-      std::string err("[LCSIO::SIORecords]: Could not add SIO record: ") ;
-      err+=name ;
-      throw IO::IOException( err ) ;
-    }
-  }
-  
-  void  SIORecords::setCompress( bool flag) {
-
-    for( unsigned i=0; i < NumberOfRecords ; ++i ){
-
-      if( i != Access )
-	_records[i]->setCompress( flag ) ;
-      else
-	_records[i]->setCompress( false ) ; // random access records are always uncompressed ....
-
-    }
-  }
-
-  SIORecords::Unpack::Unpack( unsigned recordFlag ) {
-
-    for( unsigned i=0; i < NumberOfRecords ; ++i ){
-
-      _flags[i] = LCSIO::records()[i]->getUnpack() ;
-    
-      LCSIO::records()[ i ]->setUnpack(   recordFlag & ( 0x0001 << i )  ) ;
-    }
-  }
-  SIORecords::Unpack::~Unpack() {
-
-    for( unsigned i=0; i < NumberOfRecords ; ++i ){
-
-      LCSIO::records()[ i ]->setUnpack( _flags[i] ) ;
-    }
-    
-  }
-
-  //-------------------------------
- 
-
-  int LCSIO::uid = 0 ;
-  int LCSIO::dummy_size = LCSIO::dummy_initial_size ;
-  char* LCSIO::dummy = new char[ LCSIO::dummy_initial_size ]   ;
-  
-//   //FIXME: remove these ....
-//   const char* LCSIO::RUNRECORDNAME = "LCRunHeader" ;
-//   const char* LCSIO::RUNBLOCKNAME = "RunHeader" ;
-//   const char* LCSIO::EVENTRECORDNAME = "LCEvent"  ;
-//   const char* LCSIO::EVENTBLOCKNAME="Event" ;
-//   const char* LCSIO::HEADERRECORDNAME = "LCEventHeader"  ;
-//   const char* LCSIO::HEADERBLOCKNAME="EventHeader" ;
-
   const char* LCSIO::FILE_EXTENSION=".slcio" ;
-
-
-  void LCSIO::seekStream( SIO_stream* stream , long64 pos) {
-
-    if( stream->getState() != SIO_STATE_OPEN ){
-      
-      throw IO::IOException( std::string(" stream not open: ")+ *stream->getName() ) ;
-    }
-    
-    int status ;
-    
-    if( pos < 0 ) 
-      status = stream->seek( pos , SEEK_END ) ; 
-    else
-      status = stream->seek( pos ) ;
-    
-    if( status != SIO_STREAM_SUCCESS ) {
-      std::stringstream s ;  s << "[LCSIO::seekStream] Can't seek stream to " << pos << "  errno : " << errno ;
-      throw IO::IOException( s.str() ) ;
-    }
-
-  }
-
 
   void LCSIO::checkVersion(int versionID){
     if ( SIO_VERSION_MAJOR( versionID ) < 1 && SIO_VERSION_MINOR(versionID) < 8)
       throw IO::IOException(" Old file versions ( < v00-08 ) no longer supported !") ;    
   }
 
-
-  SIORecords& LCSIO::records() {
-    static SIORecords recs ;
-    return recs ;
-  }
-
-  unsigned int LCSIO::read( SIO_stream* stream ,char** c, int* len){
-    
-    int status ;
-    int strLen ;
-    status = SIO_functions::data( stream ,  &strLen  , 1  ) ;
-
+  unsigned int LCSIO::read( SIO_stream* stream , std::string &str ) {
+    // Read string len
+    int strLen;
+    unsigned int status = stream->read_data( &strLen  , 1  ) ;
     if( !( status & 1 ) ) return status ;
-    
-    // make sure our string buffer is large enough 
-    while( strLen + 1 > dummy_size ){
-      dummy_size += dummy_size  ;
-      delete[] dummy ;
-      dummy = new char[ dummy_size  ] ;
-      //    cout << " read : allocating  " << dummy_size  << " bytes ! StrLen : " <<  strLen<< endl ;
-      
-    }
-    
-    SIO_functions::data( stream ,  dummy  , strLen ) ;
-    dummy[ strLen ] = '\0' ; // still needed for char* ...
-    
-    *c = dummy  ;
-    if(len!=0) 
-      *len = strLen ;
-
-    return status ;
+    str.resize( strLen );
+    return stream->read_data( const_cast<char*>(str.c_str()), strLen );
   }
-  
-unsigned int LCSIO::write( SIO_stream* stream , int i){
 
-  int local = i ;
-  return  SIO_functions::data( stream ,  &local , 1  ) ;
- 
-}
-unsigned int LCSIO::write( SIO_stream* stream , unsigned int i){
+  unsigned int LCSIO::write( SIO_stream* stream , int i){
+    return stream->write_data( &i , 1 );
+  }
+  unsigned int LCSIO::write( SIO_stream* stream , unsigned int i){
+    return stream->write_data( &i , 1 );
+  }
 
-  unsigned int local = i ;
-  return  SIO_functions::data( stream ,  &local , 1  ) ;
- 
-}
-
-// needed for LP64 architectures
+  // needed for LP64 architectures
 #if defined(_LP64) || defined(__APPLE_CC__)
-unsigned int LCSIO::write( SIO_stream* stream , size_t i){
-
-  unsigned int local = i ;
-  return  SIO_functions::data( stream ,  &local , 1  ) ;
- 
-}
+  unsigned int LCSIO::write( SIO_stream* stream , size_t i){
+    return stream->write_data( &i , 1 );
+  }
 #endif
 
-unsigned int LCSIO::write( SIO_stream* stream , long64 i){
+  unsigned int LCSIO::write( SIO_stream* stream , EVENT::long64 i){
+    return stream->write_data( &i , 1 );
+  }
 
-  long64 local = i ;
-  return  SIO_functions::data( stream ,  &local , 1  ) ;
- 
-}
+  unsigned int LCSIO::write( SIO_stream* stream , float f){
+    return stream->write_data( &f , 1 );
+  }
 
-unsigned int LCSIO::write( SIO_stream* stream , float f){
-
-  float local = f ;
-  return  SIO_functions::data( stream ,  &local , 1  ) ;
-}
+  unsigned int LCSIO::write(SIO_stream* stream , const std::string& s) {
+    int strLen = s.length() ;
+    int status = stream->write_data( &strLen , 1 );
+    if( !( status & 1 ) ) return status ;
+    return stream->write_data( const_cast<char*>( s.c_str() ) , strLen );
+  }
   
-
-unsigned int LCSIO::write(SIO_stream* stream , const std::string& s){
-
-//   int status ;
-//   int strLen = s.length() ;
-
-//   // make sure our string buffer is large enough 
-//   while( strLen + 1 > dummy_size ){
-//     dummy_size += dummy_size  ;
-//     delete[] dummy ;
-//     dummy = new char[ dummy_size  ] ;
-//   }
-//   strcpy( dummy , s.c_str() ) ;
-  
-//   status = SIO_functions::data( stream ,  & strLen  , 1  ) ;
-//   if( !( status & 1 ) ) return status ;
-  
-//   // fg 20030508 don't write trailing '\0's any more (v-00-03)
-//   status = SIO_functions::data( stream ,  dummy , strLen ) ;
-
-//   return status ;
-
-  // fg: why not use const_cast here - then we don't need to copy to a buffer ....
-  int strLen = s.length() ;
-  int status = SIO_functions::data( stream ,  & strLen  , 1  ) ;
-  if( !( status & 1 ) ) return status ;
-  return  SIO_functions::data( stream , const_cast<char*>( s.c_str() ), strLen );
-}
-  
-  
-
-  
-  //const char* LCSIO::getValidSIOName(const std::string& aName ){
-std::string LCSIO::getValidSIOName(const std::string& aName ) {
-
-  const char* name  = aName.c_str()  ;
-
-  char* newName =  new char[  aName.length() + 1 ];
-  const char * returnStrP = newName ;
-  std::stringstream returnStr ;
-
-  if( *name == '\\' || *name == '/' || *name=='.' )
-    *newName++ = '_' ;
-  // if first char is not alpha or '_' replace it with an 'A'
-  else if ( (*name < 0 ) || ( !isalpha( (int)*name ) && *name != '_' ) )
-    *newName++ = 'A' ;
-  else 
-    *newName++ = *name ;
-  
-
-  for( name += 1; *name != '\0'; name++ ){
-    
-    if( *name == '\\' || *name == '/' || *name=='.' )
-      *newName++ = '_' ;
-
-    else if( (*name>=0) && ( isalnum( (int)*name ) || *name == '_' ) )
-      *newName++ = *name ;
-  } 
-  
-  *newName = '\0' ;
-
-  // JE: append uid to name
-  returnStr << returnStrP << uid++;
-  delete[] returnStrP ;
-
-  return returnStr.str() ;
-}
-
-
 } // namespace 
  
