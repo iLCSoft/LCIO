4c8c0a6332834afc84113732779ad32dfe0e7f41
Ete Remi <remi.ete@desy.de>
2019-01-16 12:12:54 +0100

    Turned SIOHandlerMgr from singleton class to standard class

--- src/cpp/include/SIO/SIOHandlerMgr.h
+++ src/cpp/include/SIO/SIOHandlerMgr.h
@@ -1,52 +1,40 @@
 #ifndef SIO_SIOHANDLERMGR_H
 #define SIO_SIOHANDLERMGR_H 1
 
-#include<map>
-#include<string>
-#include "SIO/SIOObjectHandler.h"
+// -- std headers
+#include <map>
+#include <string>
 
+// -- lcio headers
+#include "SIO/SIOObjectHandler.h"
 
 namespace SIO {
 
   typedef  std::map< std::string, SIOObjectHandler* > SIOHandlerMap ;
   
-/** Singleton that holds instances of all types of SIOObjectHandlers.
+/** Holds instances of all types of SIOObjectHandlers.
  * New types need to have their handler classes registered here. 
  *
  * @author gaede
  * @version $Id: SIOHandlerMgr.h,v 1.5 2005-04-15 08:37:42 gaede Exp $
  */
   class SIOHandlerMgr {
-    
-  private:
-    
-    SIOHandlerMgr() ; // no public c'tor for a singleton
-    
   public:
-    
+    SIOHandlerMgr() ;
     ~SIOHandlerMgr() ;
     
-    /** Returns the instance of this class.
+    /** Returns the instance of the handler for the given type - null if it doesn't exist.
      */
-    static SIOHandlerMgr* instance() ;
-    
-    /**Returns the instance of the handler for the given type - null if it doesn't exist.
-     */
-    SIOObjectHandler* getHandler( const std::string& type ) ;
+    SIOObjectHandler* getHandler( const std::string& type ) const ;
     
     /** Registers an instance of a handler for the given type. 
      *  Fails if type name allready exists.
      */
     bool registerHandler( const std::string& type, SIOObjectHandler* handler ) ;
     
-    
   private:
     
-    SIOHandlerMap _map{} ;
-    
-    static SIOHandlerMgr* _me ;
-    
-    
+    SIOHandlerMap _map{};
   }; // class
   
 } // namespace
