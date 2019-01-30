76d7748c3cec51f465b15e48a110b9c69c503e4e
Ete Remi <remi.ete@desy.de>
2019-01-30 13:56:43 +0100

    Removed singleton pattern from LCIOExceptionHandler. Replace by simple static thread safe functions

--- src/cpp/src/IMPL/LCIOExceptionHandler.cc
+++ src/cpp/src/IMPL/LCIOExceptionHandler.cc
@@ -5,43 +5,22 @@
 
 namespace IMPL {
 
-
-  // create one globale instance of the handler
-  LCIOExceptionHandler* LCIOExceptionHandler::_me = 0 ;
-
-
-/** Method catches any std::exception, prints the message to stdout
- * and then exits the program. 
- */
-void lcio_unexpected(){
+  void LCIOExceptionHandler::setupHandlers() {
+    // thread safe functions ...
+    std::set_unexpected( &LCIOExceptionHandler::handler ) ;
+    std::set_terminate( &LCIOExceptionHandler::handler ) ;
+  }
   
-  try{
-    
-    throw ;
-    
-  }catch( std::exception& e){
-    
-    std::cout << " A runtime error has occured : " 
-	      << e.what() 
-	      << std::endl
-	      << " the program will have to be terminated - sorry." << std::endl ;
-    exit(1) ;
-  }
-}
-
-  LCIOExceptionHandler::LCIOExceptionHandler(){
-      
-    std::set_unexpected( lcio_unexpected ) ;
-    std::set_terminate( lcio_unexpected ) ;
-  }
-    
-
-  LCIOExceptionHandler* LCIOExceptionHandler::createInstance(){
-
-    if( ! _me ){
-      _me = new LCIOExceptionHandler ;
+  void LCIOExceptionHandler::handler() {
+    try {
+      throw ;
     }
-
-    return _me ;
+    catch( std::exception& e) {      
+      std::cout << " A runtime error has occured : " 
+          << e.what() 
+          << std::endl
+          << " the program will have to be terminated - sorry." << std::endl ;
+      exit(1) ;
+    }
   }
 }
