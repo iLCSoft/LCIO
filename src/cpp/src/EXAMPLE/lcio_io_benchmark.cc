6728d58389aff29e6d0df75c9344c05e58192310
Ete Remi <remi.ete@desy.de>
2019-02-13 14:17:52 +0100

    Output only useful variables for benchmarking. Added documentation. Added write benchmarks

--- src/cpp/src/EXAMPLE/lcio_io_benchmark.cc
+++ src/cpp/src/EXAMPLE/lcio_io_benchmark.cc
@@ -8,6 +8,7 @@
 
 #include <cstdlib>
 #include <memory>
+#include <sstream>
 
 #include <TSystem.h>
 #include <TFile.h>
@@ -16,85 +17,96 @@
 using namespace std ;
 using namespace lcio ;
 
+/**
+ *  @brief  BenchmarkListener class
+ *  Simple event/run listener getting memory/proc statistics on callback
+ */
 class BenchmarkListener : public IO::LCEventListener, public IO::LCRunListener {
 public:
   BenchmarkListener(const BenchmarkListener&) = delete ;
   BenchmarkListener& operator=(const BenchmarkListener&) = delete ;
   
+  /**
+   *  @brief  Constructor
+   *  
+   *  @param  fname the ROOT output file with statistics
+   */
   BenchmarkListener(const std::string &fname) {
     m_pTFile = std::make_shared<TFile>( fname.c_str() , "RECREATE" ) ;
     m_pTTree = new TTree( "LCIOBenchmark", "LCIOBenchmark" ) ;
     m_pTTree->SetDirectory( m_pTFile.get() ) ;
-    // cpu benchmarks
-    m_pTTree->Branch( "CpuInfo_fIdle",         &m_cpuInfo.fIdle ) ;
-    m_pTTree->Branch( "CpuInfo_fLoad15m",      &m_cpuInfo.fLoad15m ) ;
-    m_pTTree->Branch( "CpuInfo_fLoad1m",       &m_cpuInfo.fLoad1m ) ;
-    m_pTTree->Branch( "CpuInfo_fLoad5m",       &m_cpuInfo.fLoad5m ) ;
-    m_pTTree->Branch( "CpuInfo_fSys",          &m_cpuInfo.fSys ) ;
-    m_pTTree->Branch( "CpuInfo_fTotal",        &m_cpuInfo.fTotal ) ;
-    m_pTTree->Branch( "CpuInfo_fUser",         &m_cpuInfo.fUser ) ;
-    // memory benchmarks
-    m_pTTree->Branch( "MemInfo_fMemFree",      &m_memInfo.fMemFree ) ;
-    m_pTTree->Branch( "MemInfo_fMemTotal",     &m_memInfo.fMemTotal ) ;
-    m_pTTree->Branch( "MemInfo_fMemUsed",      &m_memInfo.fMemUsed ) ;
-    m_pTTree->Branch( "MemInfo_fSwapFree",     &m_memInfo.fSwapFree ) ;
-    m_pTTree->Branch( "MemInfo_fSwapTotal",    &m_memInfo.fSwapTotal ) ;
-    m_pTTree->Branch( "MemInfo_fSwapUsed",     &m_memInfo.fSwapUsed ) ;
-    // process benchmarks
-    m_pTTree->Branch( "ProcInfo_fCpuSys",      &m_procInfo.fCpuSys ) ;
-    m_pTTree->Branch( "ProcInfo_fCpuUser",     &m_procInfo.fCpuUser ) ;
-    m_pTTree->Branch( "ProcInfo_fMemResident", &m_procInfo.fMemResident, "ProcInfo_fMemResident/Long_t" ) ;
-    m_pTTree->Branch( "ProcInfo_fMemVirtual",  &m_procInfo.fMemVirtual, "ProcInfo_fMemVirtual/Long_t" ) ;
-    
-    // differences on read record
-    m_pTTree->Branch( "ReadTime",      &m_readTime ) ;
-    m_pTTree->Branch( "ReadRSS",      &m_readResidentMemory, "ReadRSS/Long_t" ) ;
-    
+    // global benchmarking
+    m_pTTree->Branch( "ProcRSS",               &m_procRSS, "ProcRSS/Long_t" ) ;
+    // read benchmarks
+    m_pTTree->Branch( "ReadTime",              &m_readTime ) ;
+    m_pTTree->Branch( "ReadRSS",               &m_readResidentMemory, "ReadRSS/Long_t" ) ;
+    // write benchmarks
+    m_pTTree->Branch( "WriteTime",             &m_writeTime ) ;
+    m_pTTree->Branch( "WriteRSS",              &m_writeRSS, "WriteRSS/Long_t" ) ;
+    // the entry id for easy plotting
+    m_pTTree->Branch( "Entry",                 &m_entry ) ;
     // init benchamark vars
-    gSystem->GetCpuInfo(&m_cpuInfo);
-    gSystem->GetMemInfo(&m_memInfo);
-    gSystem->GetProcInfo(&m_procInfo);
-    m_residentMemoryReference = m_procInfo.fMemResident;
-    m_lastClock = clock();
-  }
-  
-  void processEvent(EVENT::LCEvent * /*evt*/) {
-    getVariables();
-    m_pTTree->Fill();
-  }
-
-  void modifyEvent(EVENT::LCEvent * /*evt*/) {
-    /* nop */
-  }
-  
-  void processRunHeader(EVENT::LCRunHeader * /*rh*/) {
-    getVariables();
-    m_pTTree->Fill();
-  }
-
-  void modifyRunHeader(EVENT::LCRunHeader * /*rh*/) {
-    /* nop */
-  }
-  
-  void getVariables() {
-    // Get benchmarks and fill with the tree
-    CpuInfo_t cpuInfo ;
-    MemInfo_t memInfo ;
     ProcInfo_t procInfo ;
-    clock_t currentClock = clock();
-    gSystem->GetCpuInfo(&cpuInfo);
-    gSystem->GetMemInfo(&memInfo);
     gSystem->GetProcInfo(&procInfo);
-    
-    m_readTime = (currentClock - m_lastClock) / static_cast<float>( CLOCKS_PER_SEC );
-    m_readResidentMemory = ( procInfo.fMemResident - m_residentMemoryReference ) ;
-    
-    m_cpuInfo = cpuInfo;
-    m_memInfo = memInfo;
-    m_procInfo = procInfo;
-    m_lastClock = currentClock;
+    m_residentMemoryReference = procInfo.fMemResident;
+    m_firstClock = m_lastClock = clock();
+    // initialize the LCWriter instance
+    m_lcWriter = LCFactory::getInstance()->createLCWriter();
+    m_lcioOutputFileName << "LCIO_benchmarking_" << gSystem->GetPid() << ".slcio";
+    std::cout << "Opening output LCIO file: " << m_lcioOutputFileName.str() << std::endl;
+    m_lcWriter->open( m_lcioOutputFileName.str(), LCIO::WRITE_NEW );
   }
   
+  /**
+   *  @brief  Destructor
+   */
+  ~BenchmarkListener() {
+    std::cout << "Closing and deleting LCIO file " << m_lcioOutputFileName.str() << std::endl;
+    m_lcWriter->close();
+    delete m_lcWriter;
+    std::remove( m_lcioOutputFileName.str().c_str() );
+    std::cout << "Total processing time was " << float(m_lastClock - m_firstClock) / CLOCKS_PER_SEC << " secs" << std::endl;
+  }
+
+  /**
+   *  @brief  Extract and fill benchmark variables
+   *  
+   *  @param  evt the event to process
+   */
+  void processEvent(EVENT::LCEvent *evt) {
+    getVariables(evt);
+    m_pTTree->Fill();
+  }
+  
+  /**
+   *  @brief  Extract benchmark variables
+   *  
+   *  @param  event the event to process
+   */
+  void getVariables(EVENT::LCEvent *event) {
+    // Get benchmarks and fill with the tree
+    // "Read event" variables 
+    ProcInfo_t procInfo, procInfo2 ;
+    clock_t currentClock = clock();
+    m_readTime = float( currentClock - m_lastClock ) / CLOCKS_PER_SEC ;
+    gSystem->GetProcInfo(&procInfo); 
+    m_procRSS = procInfo.fMemResident;
+    m_readResidentMemory = ( procInfo.fMemResident - m_residentMemoryReference ) ;    
+    // "Write event" variables
+    clock_t writeStart = clock() ;
+    m_lcWriter->writeEvent(event) ;
+    clock_t writeEnd = clock() ;
+    gSystem->GetProcInfo(&procInfo2) ;
+    m_writeTime = float( writeEnd - writeStart ) / CLOCKS_PER_SEC ;
+    m_writeRSS = ( procInfo2.fMemResident - procInfo.fMemResident ) ;
+    m_residentMemoryReference = procInfo2.fMemResident ;    
+    m_lastClock = clock();
+    m_entry++;
+  }
+  
+  /**
+   *  @brief  Write benchmarks to ROOT file
+   */
   void write() {
     m_pTFile->cd();
     m_pTTree->Write();
@@ -103,25 +115,40 @@ public:
   }
   
 private:
-  // output root file with benchmarking info
+  void modifyEvent(EVENT::LCEvent * /*evt*/) { /* nop */ }
+  void processRunHeader(EVENT::LCRunHeader * /*rh*/) { /* nop */ }
+  void modifyRunHeader(EVENT::LCRunHeader * /*rh*/) { /* nop */ }
+  
+private:
+  /// Output root file with benchmarking info
   std::shared_ptr<TFile>           m_pTFile {nullptr} ;
-  // output ROOT tree
+  /// Output ROOT tree
   TTree                           *m_pTTree {nullptr} ;
-  // Cpu benchmarks
-  CpuInfo_t                        m_cpuInfo {} ;
-  // Memory benchmarks
-  MemInfo_t                        m_memInfo {} ;
-  // Process benchmarks
-  ProcInfo_t                       m_procInfo {} ;
-  
+  /// LCIO output file writer
+  IO::LCWriter                    *m_lcWriter {nullptr} ;
+  /// The LCIO output file name
+  std::stringstream                m_lcioOutputFileName {} ;
+  /// Event reading time
   Float_t                          m_readTime {0} ;
-  clock_t                          m_lastClock {} ;
-  
+  /// The first clock measurement on instance creation
+  clock_t                          m_firstClock {0} ;
+  /// The last clock measurement on processEvent()
+  clock_t                          m_lastClock {0} ;
+  /// The resident set size on instance creation (reference)
   Long_t                           m_residentMemoryReference {0} ;
+  /// The resident set size after reading an event (minus reference)
   Long_t                           m_readResidentMemory {0} ;
+  /// The current entry id while filling the tree (for easy plotting)
+  Int_t                            m_entry {0} ;
+  /// The event write time
+  Float_t                          m_writeTime {0} ;
+  /// The resisdent set size after writing
+  Long_t                           m_writeRSS {0} ;
+  /// The process resident set size taken in processEvent callback
+  Long_t                           m_procRSS {0} ;
 };
 
-/** Small utility to benchmark lcio reading performances
+/** Small utility to benchmark lcio reading/writing performances
  */
 int main(int argc, char** argv ){
 
