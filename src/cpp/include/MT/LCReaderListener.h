f5990d09f62fb8b256434eb5cb5d703e1bb13856
Ete Remi <remi.ete@desy.de>
2019-05-22 11:46:26 +0200

    Removed modify callbacks

--- src/cpp/include/MT/LCReaderListener.h
+++ src/cpp/include/MT/LCReaderListener.h
@@ -18,25 +18,11 @@ public:
   virtual ~LCReaderListener() {}
 
   /**
-   *  @brief  modify an event 
-   * 
-   *  @param  event the event to modify
-   */
-  virtual void modifyEvent( LCEventPtr event ) = 0 ;
-
-  /**
    *  @brief  process an event 
    * 
    *  @param  event the event to process
    */
   virtual void processEvent( LCEventPtr event ) = 0 ;
-
-  /**
-   *  @brief  modify a run header
-   * 
-   *  @param  hdr the run header to modify
-   */
-  virtual void modifyRunHeader( LCRunHeaderPtr hdr ) = 0 ;
   
   /**
    *  @brief  process a run header
