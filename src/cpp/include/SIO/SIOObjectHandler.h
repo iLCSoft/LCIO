d60642b5a9a8e6f9837e236542e563743c502aa6
Ete Remi <remi.ete@desy.de>
2019-06-13 14:30:17 +0200

    Redefined interface of SIOObjectHandler

--- src/cpp/include/SIO/SIOObjectHandler.h
+++ src/cpp/include/SIO/SIOObjectHandler.h
@@ -3,9 +3,8 @@
 
 #include "EVENT/LCObject.h"
 #include "EVENT/LCCollection.h"
-#include "SIO_definitions.h"
-#include "SIO_block.h"
 
+#include "sio/definitions.h"
 
 namespace SIO{
 
@@ -16,47 +15,50 @@ namespace SIO{
  * @author gaede
  * @version $Id: SIOObjectHandler.h,v 1.10 2008-11-27 16:53:35 engels Exp $
  */
-class SIOObjectHandler {
-  
+class SIOObjectHandler {  
 public:
-
-  virtual ~SIOObjectHandler(){ /* nop */; }
+  /// Default destructor
+  virtual ~SIOObjectHandler() = default ;
+  
+  /// Set the collection flag
+  void setFlag( unsigned int flag ) ;
   
   /** Reads lcio objects from an SIO stream.
    */
-  virtual unsigned int read(SIO_stream* stream, 
-			    EVENT::LCObject** objP) =0 ;
+  virtual void read( sio::read_device& device, EVENT::LCObject** objP, sio::version_type vers ) = 0 ;
   
   /** Writes lcio objects to an SIO stream.
    */
-  virtual unsigned int write(SIO_stream* stream, 
-			     const EVENT::LCObject* obj ) =0 ;
+  virtual void write( sio::write_device& device, const EVENT::LCObject* obj ) = 0 ;
   
-
-  /** Initialize the handler and/or the collection.
-   * Read/write the flag and the parameters.
-   * Overwrite for classes that need specific processing.
-   */
-  virtual unsigned int init( SIO_stream* stream,
-			     SIO_operation op,
-			     EVENT::LCCollection* col,
-			     unsigned int vers ) ; 
-  
-
-  /** Calls read() if the collection is not a subset otherwise only reads the pointers.*/
-  virtual unsigned int readBase(SIO_stream* stream, 
-			    EVENT::LCObject** objP)  ;
-  
-
-  /** Calls write() if the collection is not a subset otherwise only writes the pointers.*/
-  virtual unsigned int writeBase(SIO_stream* stream, 
-			     const EVENT::LCObject* obj ) ;
+  // /** Initialize the handler and/or the collection.
+  //  * Read the flag and the parameters.
+  //  * Overwrite for classes that need specific processing.
+  //  */
+  // virtual void init( sio::read_device& device, EVENT::LCCollection* col, sio::version_type vers ) ; 
+  // 
+  //  /** Initialize the handler and/or the collection.
+  //   * Read/write the flag and the parameters.
+  //   * Overwrite for classes that need specific processing.
+  //   */
+  //  virtual unsigned int init( SIO_stream* stream,
+ 	// 		     SIO_operation op,
+ 	// 		     EVENT::LCCollection* col,
+ 	// 		     unsigned int vers ) ; 
+  // 
+  // 
+  // /** Calls read() if the collection is not a subset otherwise only reads the pointers.*/
+  // virtual void readBase( sio::read_device& device, EVENT::LCObject** objP ) ;
+  // 
+  // 
+  // /** Calls write() if the collection is not a subset otherwise only writes the pointers.*/
+  // virtual void writeBase( sio::write_device& device, const EVENT::LCObject* obj ) ;
 
  protected:
 
-
-  unsigned int _flag{0} ; 
-  unsigned int _vers{0} ;
+  // 
+  unsigned int _flag {0} ; 
+  // unsigned int _vers{0} ;
   
 }; // class
 
