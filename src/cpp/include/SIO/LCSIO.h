bf27128cec5afe8f6e365d517e2dded023f65d60
Ete Remi <remi.ete@desy.de>
2019-06-27 20:57:34 +0200

    Tidy up LCSIO class to keep only useful functions

--- src/cpp/include/SIO/LCSIO.h
+++ src/cpp/include/SIO/LCSIO.h
@@ -1,48 +1,35 @@
 #ifndef SIO_LCSIO_H
 #define SIO_LCSIO_H 1
 
-// -- lcio headers
-#include "LCIOTypes.h"
-
 // -- sio headers
-#include "SIO_record.h" 
-#include "SIO_definitions.h"
-
-// -- std headers
-#include <string>
-
-// record and block names used in LCIO-SIO
-#define LCSIO_RUNRECORDNAME    "LCRunHeader"
-#define LCSIO_RUNBLOCKNAME     "RunHeader"
-#define LCSIO_EVENTRECORDNAME  "LCEvent"
-#define LCSIO_EVENTBLOCKNAME   "Event"
-#define LCSIO_HEADERRECORDNAME "LCEventHeader"
-#define LCSIO_HEADERBLOCKNAME  "EventHeader"
-
-#define LCSIO_ACCESSRECORDNAME "LCIORandomAccess"
-#define LCSIO_ACCESSBLOCKNAME  "LCIORandomAccess"
-#define LCSIO_RANDOMACCESS_SIZE 136 
-#define LCSIO_INDEXRECORDNAME "LCIOIndex"
-#define LCSIO_INDEXBLOCKNAME  "LCIOIndex"
+#include <sio/definitions.h>
 
 namespace SIO {
 
-#define LCSIO_READ( rec, pnt ) status = LCSIO::read( (rec), (pnt)  ); if( !(status & 1) ) return status;
-// #define LCSIO_READ_LEN( rec, pnt , len ) status = LCSIO::read( (rec), (pnt) , (len)  ); if( !(status & 1) ) return status;
-#define LCSIO_WRITE( rec, pnt ) status = LCSIO::write( (rec), (pnt)  ); if( !(status & 1) ) return status;
-
 /** 
- *  @brief  Collection of static helper  functions for reading and writing
- *          data with SIO. Could go to the SIO_functions class.
- *          Also defines some constant names for lcio-SIO.
+ *  @brief  Collection of constants and helper functions
  *
  *  @author gaede
- *  @version Mar 6, 2003
+ *  @author rete
+ *  @version June, 2019
  */
-  class LCSIO{
+  class LCSIO {
   public:
     /// The lcio file extension name
-    static const char* FILE_EXTENSION;
+    static constexpr const char * FileExtension     = ".slcio" ;
+    
+    // record and block names used in LCIO-SIO
+    static constexpr const char * RunRecordName     = "LCRunHeader" ;
+    static constexpr const char * RunBlockName      = "RunHeader" ;
+    static constexpr const char * EventRecordName   = "LCEvent" ;
+    static constexpr const char * EventBlockName    = "Event" ;
+    static constexpr const char * HeaderRecordName  = "LCEventHeader" ;
+    static constexpr const char * HeaderBlockName   = "EventHeader" ;
+    static constexpr const char * AccessRecordName  = "LCIORandomAccess" ;
+    static constexpr const char * AccessBlockName   = "LCIORandomAccess" ;
+    static constexpr unsigned int RandomAccessSize  = 136 ; 
+    static constexpr const char * IndexRecordName   = "LCIOIndex" ;
+    static constexpr const char * IndexBlockName    = "LCIOIndex" ;
 
     /**
      *  @brief  Check for old version of LCIO (> v01-08 ar no longer supported)
@@ -50,45 +37,12 @@ namespace SIO {
      * 
      *  @param  version the version to check
      */
-    static void checkVersion(int versionID );
-
-    /** 
-     *  @brief  Read a string from the stream
-     */
-    static unsigned int read( SIO_stream* stream , std::string &c);
-  
-    /** 
-     *  @brief  Write an int to the stream.
-     */
-    static unsigned int write( SIO_stream* stream , int i);
-
-    /** 
-     *  @brief  Write an unsigned int to the stream.
-     */
-    static unsigned int write( SIO_stream* stream , unsigned int i);
-  
-#if defined(_LP64)  || defined(__APPLE_CC__)
+    static void checkVersion( sio::version_type versionID ) ;
+    
     /**
-     *  @brief  Write a vector size to the stream (needed for AMD64 architectures)
+     *  @brief  Get the LCIO encoded version number for writing in sio blocks 
      */
-    static unsigned int write( SIO_stream* stream , size_t i);
-#endif
-
-    /** 
-     *  @brief  Write a 64bit integer to the stream 
-     */
-    static unsigned int write( SIO_stream* stream , EVENT::long64 i);
-    
-    /** 
-     *  @brief  Write a float  to the stream.
-     */
-    static unsigned int write( SIO_stream* stream , float f);
-    
-    /** 
-     *  @brief  Write a string to the stream. Writes 
-     *          the length of the string first and adds '\0' to the end.
-     */
-    static unsigned int write(SIO_stream* stream , const std::string& s);
+    static sio::version_type blockVersion() ;
   };
 
 } // namespace 
--- src/cpp/src/SIO/LCSIO.cc
+++ src/cpp/src/SIO/LCSIO.cc
@@ -1,62 +1,18 @@
 #include "SIO/LCSIO.h"
-
-#include "SIO_functions.h"
-#include "SIO_stream.h"
-#include <cctype>
-#include <cerrno>
-#include "Exceptions.h"
-
-#include <iostream>
-#include <sstream>
-
-using namespace EVENT ;
+#include "EVENT/LCIO.h"
 
 namespace SIO {
 
-  const char* LCSIO::FILE_EXTENSION=".slcio" ;
-
-  void LCSIO::checkVersion(int versionID){
-    if ( SIO_VERSION_MAJOR( versionID ) < 1 && SIO_VERSION_MINOR(versionID) < 8)
-      throw IO::IOException(" Old file versions ( < v00-08 ) no longer supported !") ;    
-  }
-
-  unsigned int LCSIO::read( SIO_stream* stream , std::string &str ) {
-    // Read string len
-    int strLen;
-    unsigned int status = stream->read_data( &strLen  , 1  ) ;
-    if( !( status & 1 ) ) return status ;
-    str.resize( strLen );
-    return stream->read_data( const_cast<char*>(str.c_str()), strLen );
-  }
-
-  unsigned int LCSIO::write( SIO_stream* stream , int i){
-    return stream->write_data( &i , 1 );
-  }
-  unsigned int LCSIO::write( SIO_stream* stream , unsigned int i){
-    return stream->write_data( &i , 1 );
-  }
-
-  // needed for LP64 architectures
-#if defined(_LP64) || defined(__APPLE_CC__)
-  unsigned int LCSIO::write( SIO_stream* stream , size_t i){
-    return stream->write_data( &i , 1 );
-  }
-#endif
-
-  unsigned int LCSIO::write( SIO_stream* stream , EVENT::long64 i){
-    return stream->write_data( &i , 1 );
-  }
-
-  unsigned int LCSIO::write( SIO_stream* stream , float f){
-    return stream->write_data( &f , 1 );
-  }
-
-  unsigned int LCSIO::write(SIO_stream* stream , const std::string& s) {
-    int strLen = s.length() ;
-    int status = stream->write_data( &strLen , 1 );
-    if( !( status & 1 ) ) return status ;
-    return stream->write_data( const_cast<char*>( s.c_str() ) , strLen );
+  void LCSIO::checkVersion( sio::version_type versionID ){
+    if ( SIO_VERSION_MAJOR( versionID ) < 1 && SIO_VERSION_MINOR( versionID ) < 8 )
+      SIO_THROW( sio::error_code::invalid_argument " Old file versions ( < v00-08 ) no longer supported !") ;    
   }
   
-} // namespace 
+  //----------------------------------------------------------------------------
+  
+  sio::version_type LCSIO::blockVersion() {
+    return SIO_VERSION_ENCODE( EVENT::LCIO::MAJORVERSION, EVENT::LCIO::MINORVERSION ) ;
+  }
+  
+} 
  
