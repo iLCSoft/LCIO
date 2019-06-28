e97214cf62bb4872a445e7e1f7085692472a0d02
Ete Remi <remi.ete@desy.de>
2019-06-28 15:11:21 +0200

    Fixed missing headers and wrong usage of SIO_THROW macro

--- src/cpp/src/SIO/LCSIO.cc
+++ src/cpp/src/SIO/LCSIO.cc
@@ -1,11 +1,15 @@
 #include "SIO/LCSIO.h"
 #include "EVENT/LCIO.h"
 
+// -- sio headers
+#include <sio/version.h>
+#include <sio/exception.h>
+
 namespace SIO {
 
   void LCSIO::checkVersion( sio::version_type versionID ){
     if ( SIO_VERSION_MAJOR( versionID ) < 1 && SIO_VERSION_MINOR( versionID ) < 8 )
-      SIO_THROW( sio::error_code::invalid_argument " Old file versions ( < v00-08 ) no longer supported !") ;    
+      SIO_THROW( sio::error_code::invalid_argument, "Old file versions ( < v00-08 ) no longer supported !") ;    
   }
   
   //----------------------------------------------------------------------------
