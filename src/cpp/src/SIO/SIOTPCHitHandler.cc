dc420d66f22da142cf1768f2d81d62d529840791
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 17:46:48 +0200

    Re-implemented SIOTPCHitHandler with new SIO API

--- src/cpp/src/SIO/SIOTPCHitHandler.cc
+++ src/cpp/src/SIO/SIOTPCHitHandler.cc
@@ -1,92 +1,73 @@
 #include "SIO/SIOTPCHitHandler.h"
 
-#include "SIO/LCSIO.h"
-
+// -- lcio headers
 #include "EVENT/TPCHit.h"
 #include "EVENT/LCIO.h"
 #include "IMPL/LCFlagImpl.h"
 #include "IOIMPL/TPCHitIOImpl.h"
 
-#include "SIO_functions.h"
-#include "SIO_block.h"
-#include "SIO_stream.h"
+// -- sio headers
+#include <sio/io_device.h>
+#include <sio/version.h>
 
+namespace SIO {
 
-using namespace EVENT ;
-using namespace IMPL ;
-using namespace IOIMPL ;
-
-
-namespace SIO{
-    
-  unsigned int SIOTPCHitHandler::read(SIO_stream* stream, 
-				      LCObject** objP){
-    unsigned int status ; 
-	
-    // create a new object :
-    TPCHitIOImpl* hit  = new TPCHitIOImpl ;
-    *objP = hit ;
-	
-    SIO_DATA( stream ,  &(hit->_cellID) , 1  ) ;
-    SIO_DATA( stream ,  &(hit->_time) , 1  ) ;
-    SIO_DATA( stream ,  &(hit->_charge) , 1  ) ;
-    SIO_DATA( stream ,  &(hit->_quality) , 1  ) ;
-
-    if( LCFlagImpl(_flag).bitSet( LCIO::TPCBIT_RAW ) ){
-
-      SIO_DATA( stream ,  &(hit->_rawSize)  , 1 ) ;
-
-      hit->initRawArray( hit->_rawSize ) ; // reserve enough space for raw data
-
-      SIO_DATA( stream ,  hit->_rawArray  , hit->_rawSize  ) ;
-
-    }
-
-    // read a pointer tag for reference to TPC hits from generic hits
-
-    if( _vers > SIO_VERSION_ENCODE( 1, 2) ){    // the logic of the pointer bit has been inverted in v1.3
-      
-      if( ! LCFlagImpl(_flag).bitSet( LCIO::TPCBIT_NO_PTR )  )
-	SIO_PTAG( stream , dynamic_cast<const TPCHit*>(hit) ) ;
-      
-    }else{
-      
-      if( LCFlagImpl(_flag).bitSet( LCIO::TPCBIT_NO_PTR )  )    
-	SIO_PTAG( stream , dynamic_cast<const TPCHit*>(hit) ) ;
-      
-    }
-    
-    return ( SIO_BLOCK_SUCCESS ) ;
+  SIOTPCHitHandler::SIOTPCHitHandler() :
+    SIOObjectHandler( EVENT::LCIO::TPCHIT ) {
+    /* nop */
   }
-    
-    
-  unsigned int SIOTPCHitHandler::write(SIO_stream* stream, 
-				       const LCObject* obj){
-    
-    unsigned int status ; 
 
-    const TPCHit* hit = dynamic_cast<const TPCHit*>(obj)  ;
+  //----------------------------------------------------------------------------
 
-    LCSIO_WRITE( stream, hit->getCellID()  ) ;
-    LCSIO_WRITE( stream, hit->getTime()  ) ;
-    LCSIO_WRITE( stream, hit->getCharge()  ) ;
-    LCSIO_WRITE( stream, hit->getQuality()  ) ;
-
-    if( LCFlagImpl(_flag).bitSet( LCIO::TPCBIT_RAW ) ){
-      
-      LCSIO_WRITE( stream, hit->getNRawDataWords()  ) ;
-
-      for(int i=0;i<hit->getNRawDataWords();i++){
-	LCSIO_WRITE( stream, hit->getRawDataWord(i)  ) ;
+  void SIOTPCHitHandler::read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) {
+    auto hit = dynamic_cast<IOIMPL::TPCHitIOImpl*>( objP ) ;
+    SIO_DATA( device ,  &(hit->_cellID) , 1  ) ;
+    SIO_DATA( device ,  &(hit->_time) , 1  ) ;
+    SIO_DATA( device ,  &(hit->_charge) , 1  ) ;
+    SIO_DATA( device ,  &(hit->_quality) , 1  ) ;
+    IMPL::LCFlagImpl lcflag(_flag) ;
+    if( lcflag.bitSet( EVENT::LCIO::TPCBIT_RAW ) ){
+      SIO_DATA( device ,  &(hit->_rawSize)  , 1 ) ;
+      hit->initRawArray( hit->_rawSize ) ; // reserve enough space for raw data
+      SIO_DATA( device ,  hit->_rawArray  , hit->_rawSize  ) ;
+    }
+    if( vers > SIO_VERSION_ENCODE( 1, 2 ) ) {    // the logic of the pointer bit has been inverted in v1.3
+      if( ! lcflag.bitSet( EVENT::LCIO::TPCBIT_NO_PTR )  ) {
+	     SIO_PTAG( device , dynamic_cast<const EVENT::TPCHit*>(hit) ) ;
+     }
+    }
+    else {
+      if( lcflag.bitSet( EVENT::LCIO::TPCBIT_NO_PTR )  ) {
+	      SIO_PTAG( device , dynamic_cast<const EVENT::TPCHit*>(hit) ) ;
       }
     }
-
-    //  add a pointer tag for reference to TPC hits from generic hits
-    if( ! LCFlagImpl(_flag).bitSet( LCIO::TPCBIT_NO_PTR ) ){
-      SIO_PTAG( stream , hit ) ;
-    }
-    
-    return ( SIO_BLOCK_SUCCESS ) ;
   }
-  
+
+  //----------------------------------------------------------------------------
+
+  void SIOTPCHitHandler::write( sio::write_device& device, const EVENT::LCObject* obj ) {
+    auto hit = dynamic_cast<const EVENT::TPCHit*>(obj)  ;
+    SIO_SDATA( device, hit->getCellID()  ) ;
+    SIO_SDATA( device, hit->getTime()  ) ;
+    SIO_SDATA( device, hit->getCharge()  ) ;
+    SIO_SDATA( device, hit->getQuality()  ) ;
+    IMPL::LCFlagImpl lcflag(_flag) ;
+    if( lcflag.bitSet( EVENT::LCIO::TPCBIT_RAW ) ) {
+      SIO_SDATA( device, hit->getNRawDataWords()  ) ;
+      for(int i=0;i<hit->getNRawDataWords();i++){
+	       SIO_SDATA( device, hit->getRawDataWord(i)  ) ;
+      }
+    }
+    //  add a pointer tag for reference to TPC hits from generic hits
+    if( ! lcflag.bitSet( EVENT::LCIO::TPCBIT_NO_PTR ) ) {
+      SIO_PTAG( device , hit ) ;
+    }
+  }
+
+  //----------------------------------------------------------------------------
+
+  EVENT::LCObject *SIOTPCHitHandler::create() const {
+    return new IOIMPL::TPCHitIOImpl() ;
+  }
+
 } // namespace
