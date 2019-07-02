37e09c6c06405d079e4b3b2c50006171aa95ad9e
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 20:29:03 +0200

    Re-implemented SIOIndexHandler with new SIO API. Removed LCIORandomAccessMgr usage. Use directly a RunEventMap instead

--- src/cpp/include/SIO/SIOIndexHandler.h
+++ src/cpp/include/SIO/SIOIndexHandler.h
@@ -1,45 +1,47 @@
 #ifndef SIO_SIOIndexHandler_H
 #define SIO_SIOIndexHandler_H 1
 
-//#include <string>
+// -- lcio headers
+#include "SIO/RunEventMap.h"
 
-#include "LCIORandomAccessMgr.h"
-#include "SIO_block.h"
+// -- sio headers
+#include "sio/block.h"
+
+// -- std headers
+#include <memory>
 
 namespace SIO {
-    
+
 /** Handler for LCIOrandomAccess and LCIOIndex objects/blocks.
- * 
+ *
  * @author gaede
  * @version $Id: SIOIndexHandler.h,v 1.2 2010-06-22 13:49:54 gaede Exp $
  */
-  class SIOIndexHandler : public SIO_block{
-    
-  protected:
-    SIOIndexHandler() : SIO_block("UNKNOWN") { /* no default c'tor*/  ;} 
-
+  class SIOIndexHandler : public sio::block {
+    typedef RunEvent::long64 long64 ;
   public:
-    
     SIOIndexHandler(const SIOIndexHandler&) = delete;
     SIOIndexHandler& operator=(const SIOIndexHandler&) = delete ;
+    ~SIOIndexHandler() = default ;
 
-    /** C'tor.
-     */
-    SIOIndexHandler(const std::string& name, LCIORandomAccessMgr* raMgr) ; 
+    /// Constructor
+    SIOIndexHandler() ;
 
-//     SIOIndexHandler(const std::string& name, LCIORandomAccess** raP) ;
-    virtual ~SIOIndexHandler() ;
-    
-    // interface from SIO_block
-    virtual unsigned int   xfer( SIO_stream*, SIO_operation, unsigned int ) ;
-    virtual unsigned int   version() const ;
-    
-  private: 
+    /// Set the run / event map to read or write
+    void setRunEventMap( std::shared_ptr<RunEventMap> remap ) ;
 
-    LCIORandomAccessMgr* _raMgr{NULL} ;
+    /// Get the run / event map
+    std::shared_ptr<RunEventMap> runEventMap() const ;
 
+    // from sio::block
+    void read( sio::read_device &device, sio::version_type vers ) override ;
+    void write( sio::write_device &device ) override ;
+
+  private:
+    /// The run / event map to read/write
+    std::shared_ptr<RunEventMap>  _runEventMap {nullptr} ;
   }; // class
-  
+
 } // namespace
 
 #endif /* ifndef SIO_SIORUNHEADERHANDLER_H */
