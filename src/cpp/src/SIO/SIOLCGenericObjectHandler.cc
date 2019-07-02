093e790dfb6defe934f7cf0831917e31fcab34ab
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 14:43:59 +0200

    Re-implemented SIOLCGenericObjectHandler with new SIO API

--- src/cpp/src/SIO/SIOLCGenericObjectHandler.cc
+++ src/cpp/src/SIO/SIOLCGenericObjectHandler.cc
@@ -1,180 +1,129 @@
 #include "SIO/SIOLCGenericObjectHandler.h"
 
-#include "SIO/LCSIO.h"
-
 #include "EVENT/LCIO.h"
-#include "EVENT/MCParticle.h"
 #include "EVENT/LCGenericObject.h"
 #include "IOIMPL/LCGenericObjectIOImpl.h"
 #include "IMPL/LCFlagImpl.h"
 
-#include "SIO_functions.h"
-#include "SIO_block.h"
-#include "SIO_stream.h"
-#include "SIO/SIOLCParameters.h"
+// -- sio headers
+#include <sio/io_device.h>
+#include <sio/version.h>
 
-using namespace EVENT ;
-using namespace IMPL ;
-using namespace IOIMPL ;
+namespace SIO {
 
+  SIOLCGenericObjectHandler::SIOLCGenericObjectHandler() :
+    SIOObjectHandler( EVENT::LCIO::LCGENERICOBJECT ) {
+    /* nop */
+  }
 
-namespace SIO{
-    
-  unsigned int  SIOLCGenericObjectHandler::init( SIO_stream* stream, 
-					SIO_operation op,
-					LCCollection* col, 
-					unsigned int vers ) {
-    unsigned int status ; 
+  //----------------------------------------------------------------------------
 
-    if( op == SIO_OP_READ ) {
-
-
-      SIO_DATA( stream ,  &_flag , 1  ) ;
-      if( vers > SIO_VERSION_ENCODE( 1, 1)   ) 
-	SIOLCParameters::read( stream ,  col->parameters() , vers ) ;
-      
-      col->setFlag( _flag ) ;
-      _vers = vers ;
-      
-      _isFixedSize  =  LCFlagImpl(_flag).bitSet( LCIO::GOBIT_FIXED ) ;
-      
-      if( _isFixedSize ) { // need to read the size variables once
-	
-	SIO_DATA( stream , &_nInt  , 1  ) ;
-	SIO_DATA( stream , &_nFloat  , 1  ) ;
-	SIO_DATA( stream , &_nDouble  , 1  ) ;
-      }
-      
-
-    } else if( op == SIO_OP_WRITE ) {
-      
-
-      _isFixedSize = true ;
-      // need to check whether we have fixed size objects only
-      unsigned int nObj = col->getNumberOfElements() ;
-      for( unsigned int i=0 ; i < nObj ; i++ ){
-	LCGenericObject* obj  =  dynamic_cast< LCGenericObject* >( col->getElementAt( i ) ) ; 
-	if( !  obj->isFixedSize() ){ 
-	  _isFixedSize = false ;
-	  break ;
-	}
-      }
-      LCFlagImpl flag( col->getFlag() ) ;
-      
-      LCGenericObject* gObj  =  0 ;
-      if( col->getNumberOfElements() > 0 ) 
-	  gObj = dynamic_cast< LCGenericObject* >( col->getElementAt( 0 ) ) ; 
-
-      // if the collection doesn't have the TypeName/DataDescription parameters set,
-      //  we use the ones from the first object
-      if(  col->parameters().getStringVal( "TypeName" ).size() ==  0 && gObj != 0 )
-	col->parameters().setValue( "TypeName", gObj->getTypeName() ) ;
-      
-      if( _isFixedSize ) {
-	
-	flag.setBit( LCIO::GOBIT_FIXED ) ;
-	
-	if(  col->parameters().getStringVal( "DataDescription" ).size() ==  0 && gObj != 0 )
-	  col->parameters().setValue( "DataDescription", gObj->getDataDescription() ) ;
-      }
-      
-      
-
-      _flag = flag.getFlag() ;
-      
-      LCSIO_WRITE( stream, _flag  ) ;
-      SIOLCParameters::write( stream , col->getParameters() ) ;
-      
-      if( _isFixedSize ) { // need to write the size variables once
-
-	  if( gObj != 0 ){
-	      _nInt = gObj->getNInt() ;
-	      _nFloat = gObj->getNFloat() ;
-	      _nDouble = gObj->getNDouble() ;
-	  } else {
-	      _nInt = 0 ;
-	      _nFloat = 0 ;
-	      _nDouble = 0 ;
-	  }
-
-	  SIO_DATA( stream , &_nInt  , 1  ) ;
-	  SIO_DATA( stream , &_nFloat  , 1  ) ;
-	  SIO_DATA( stream , &_nDouble  , 1  ) ;
-	  
-	// 	cout << " >>>>>>>  written nInt, nFloat, nDouble : " << _nInt << ", " << _nFloat << ", " << _nDouble  
-	// << " for type " << col->getTypeName() << endl ;
-      }
-      
+  void SIOLCGenericObjectHandler::initReading( sio::read_device &device, EVENT::LCCollection *collection, sio::version_type vers ) {
+    SIOObjectHandler::initReading( device, collection, vers ) ;
+    _isFixedSize = IMPL::LCFlagImpl(_flag).bitSet( EVENT::LCIO::GOBIT_FIXED ) ;
+    if( _isFixedSize ) { // need to read the size variables once
+      SIO_SDATA( device , _nInt ) ;
+      SIO_SDATA( device , _nFloat ) ;
+      SIO_SDATA( device , _nDouble ) ;
     }
-    return ( SIO_BLOCK_SUCCESS ) ;
   }
-  
 
-  unsigned int SIOLCGenericObjectHandler::read(SIO_stream* stream, 
-					       LCObject** objP ){
-    unsigned int status ; 
+  //----------------------------------------------------------------------------
 
-    LCGenericObjectIOImpl* obj  = new LCGenericObjectIOImpl ;	
-    *objP = obj ;
-    
-    obj->_isFixedSize = _isFixedSize ;
-    
-    if( ! _isFixedSize ){ 
-      
-      SIO_DATA( stream ,  &_nInt , 1  ) ;
-      SIO_DATA( stream ,  &_nFloat , 1  ) ;
-      SIO_DATA( stream ,  &_nDouble , 1  ) ;
-    } 
-    
-    
-    obj->_intVec.resize( _nInt )  ; 
-    obj->_floatVec.resize( _nFloat ) ; 
-    obj->_doubleVec.resize( _nDouble ) ;
-    
-    // the 2003 C++ standard guarantes that vector uses  contigous memory ...
-    SIO_DATA( stream , &(obj->_intVec[0])  , _nInt  ) ;      
-    SIO_DATA( stream , &(obj->_floatVec[0])  , _nFloat  ) ;      
-    SIO_DATA( stream , &(obj->_doubleVec[0])  , _nDouble  ) ;      
-    
-    SIO_PTAG( stream , obj  ) ;
-    
-    return ( SIO_BLOCK_SUCCESS ) ;
+  void SIOLCGenericObjectHandler::initWriting( sio::write_device &device, EVENT::LCCollection *collection ) {
+    _isFixedSize = true ;
+    // need to check whether we have fixed size objects only
+    unsigned int nObj = collection->getNumberOfElements() ;
+    for( unsigned int i=0 ; i < nObj ; i++ ) {
+      auto obj = dynamic_cast< EVENT::LCGenericObject* >( collection->getElementAt( i ) ) ;
+      if( !  obj->isFixedSize() ) {
+        _isFixedSize = false ;
+        break ;
+      }
+    }
+    IMPL::LCFlagImpl colflag( collection->getFlag() ) ;
+    EVENT::LCGenericObject* gObj = nullptr ;
+    if( collection->getNumberOfElements() > 0 ) {
+      gObj = dynamic_cast< EVENT::LCGenericObject* >( collection->getElementAt( 0 ) ) ;
+    }
+    // if the collection doesn't have the TypeName/DataDescription parameters set,
+    //  we use the ones from the first object
+    if(  collection->parameters().getStringVal( "TypeName" ).empty() && nullptr != gObj ) {
+      collection->parameters().setValue( "TypeName", gObj->getTypeName() ) ;
+    }
+    if( _isFixedSize ) {
+      colflag.setBit( EVENT::LCIO::GOBIT_FIXED ) ;
+      if( collection->parameters().getStringVal( "DataDescription" ).empty() && nullptr != gObj ) {
+        collection->parameters().setValue( "DataDescription", gObj->getDataDescription() ) ;
+      }
+    }
+    _flag = colflag.getFlag() ;
+    SIOObjectHandler::initWriting( device, collection ) ;
+    if( _isFixedSize ) { // need to write the size variables once
+      if( gObj != 0 ){
+        _nInt = gObj->getNInt() ;
+        _nFloat = gObj->getNFloat() ;
+        _nDouble = gObj->getNDouble() ;
+      }
+      else {
+        _nInt = 0 ;
+        _nFloat = 0 ;
+        _nDouble = 0 ;
+      }
+      SIO_SDATA( device , _nInt ) ;
+      SIO_SDATA( device , _nFloat ) ;
+      SIO_SDATA( device , _nDouble ) ;
+    }
   }
-  
-  
-  unsigned int SIOLCGenericObjectHandler::write(SIO_stream* stream, 
-						const LCObject* obj){
-    
-    unsigned int status ; 
-    
-    const LCGenericObject* gObj = dynamic_cast<const LCGenericObject*>(obj)  ;
-    
-    if( ! _isFixedSize ){ 
 
+  //----------------------------------------------------------------------------
+
+  void SIOLCGenericObjectHandler::read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type /*vers*/ ) {
+    auto gobj = dynamic_cast<IOIMPL::LCGenericObjectIOImpl*>( objP ) ;
+    gobj->_isFixedSize = _isFixedSize ;
+    if( ! _isFixedSize ) {
+      SIO_SDATA( device , _nInt ) ;
+      SIO_SDATA( device , _nFloat ) ;
+      SIO_SDATA( device , _nDouble ) ;
+    }
+    gobj->_intVec.resize( _nInt )  ;
+    gobj->_floatVec.resize( _nFloat ) ;
+    gobj->_doubleVec.resize( _nDouble ) ;
+    SIO_DATA( device , &(gobj->_intVec[0]), _nInt  ) ;
+    SIO_DATA( device , &(gobj->_floatVec[0]), _nFloat  ) ;
+    SIO_DATA( device , &(gobj->_doubleVec[0]), _nDouble  ) ;
+    SIO_PTAG( device , gobj  ) ;
+  }
+
+  //----------------------------------------------------------------------------
+
+  void SIOLCGenericObjectHandler::write( sio::write_device& device, const EVENT::LCObject* obj ) {
+    auto gObj = dynamic_cast<const EVENT::LCGenericObject*>( obj ) ;
+    if( ! _isFixedSize ) {
       _nInt = gObj->getNInt() ;
       _nFloat = gObj->getNFloat() ;
       _nDouble = gObj->getNDouble() ;
-
-      LCSIO_WRITE( stream , _nInt  ) ;
-      LCSIO_WRITE( stream , _nFloat ) ;
-      LCSIO_WRITE( stream ,  _nDouble ) ;
-
-    } 
-    for( int i=0 ; i< _nInt ; i++){
-      LCSIO_WRITE( stream , gObj->getIntVal( i ) ) ;
-    }
-    for( int i=0 ; i< _nFloat ; i++){
-      LCSIO_WRITE( stream , gObj->getFloatVal( i ) ) ;
-    }
-    for( int i=0 ; i< _nDouble ; i++){
-      //      LCSIO_WRITE( stream , gObj->getDoubleVal( i ) ) ;
-      double dVal =  gObj->getDoubleVal( i ) ;
-      SIO_DATA( stream , &dVal  , 1  ) ;
+      SIO_SDATA( device , _nInt  ) ;
+      SIO_SDATA( device , _nFloat ) ;
+      SIO_SDATA( device , _nDouble ) ;
     }
 
-    SIO_PTAG( stream , gObj  ) ;
-
-    return ( SIO_BLOCK_SUCCESS ) ;
+    for( int i=0 ; i< _nInt ; i++) {
+      SIO_SDATA( device , gObj->getIntVal( i ) ) ;
+    }
+    for( int i=0 ; i< _nFloat ; i++) {
+      SIO_SDATA( device , gObj->getFloatVal( i ) ) ;
+    }
+    for( int i=0 ; i< _nDouble ; i++) {
+      SIO_SDATA( device , gObj->getDoubleVal( i ) ) ;
+    }
+    SIO_PTAG( device , gObj ) ;
   }
-  
+
+  //----------------------------------------------------------------------------
+
+  EVENT::LCObject *SIOLCGenericObjectHandler::create() const {
+    return new IOIMPL::LCGenericObjectIOImpl() ;
+  }
+
 } // namespace
