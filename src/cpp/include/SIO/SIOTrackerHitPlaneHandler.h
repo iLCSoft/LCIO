df766a05f7421108945edcea0f8bbeea6e10d6c0
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 19:21:42 +0200

    Re-implemented SIOTrackerHitPlaneHandler with new SIO API

--- src/cpp/include/SIO/SIOTrackerHitPlaneHandler.h
+++ src/cpp/include/SIO/SIOTrackerHitPlaneHandler.h
@@ -3,30 +3,26 @@
 
 #include "SIO/SIOObjectHandler.h"
 
-
 namespace SIO {
-    
-    
+
 /** Implementation of SIOObjectHandler to handle IO of TrackerHitPlane.
  *
  * @author engels
  * @version $Id:$
  */
-
   class SIOTrackerHitPlaneHandler : public SIOObjectHandler {
-	
   public:
-	
-    /** Reads lcio object from an SIO stream.
-     */
-    virtual unsigned int read(SIO_stream* stream, 
-			      EVENT::LCObject** objP)  ;
-	
-    /** Writes lcio object to an SIO stream.
-     */
-    virtual unsigned int write(SIO_stream* stream, 
-			       const EVENT::LCObject* obj) ;
-	
+    /// Constructor
+    SIOTrackerHitPlaneHandler() ;
+
+    /// Reads lcio objects from an SIO stream.
+    void read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) override ;
+
+    /// Writes lcio objects to an SIO stream.
+    void write( sio::write_device& device, const EVENT::LCObject* obj ) override ;
+
+    /// Factory method to create an object of the type of the collection
+    EVENT::LCObject *create() const override ;
   }; // class
 } // namespace
 
