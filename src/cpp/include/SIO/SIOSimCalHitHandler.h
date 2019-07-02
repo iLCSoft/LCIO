d55ec5d6c19c346fd32fc970a7be17cdb7352d62
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 17:27:10 +0200

    Re-implemented SIOSimCalHitHandler with new SIO API

--- src/cpp/include/SIO/SIOSimCalHitHandler.h
+++ src/cpp/include/SIO/SIOSimCalHitHandler.h
@@ -3,29 +3,26 @@
 
 #include "SIO/SIOObjectHandler.h"
 
-
 namespace SIO {
-    
-    
+
   /** Implementation of SIOObjectHandler to handle IO of SimCalorimeterHits.
    *
    * @author gaede
    * @version $Id: SIOSimCalHitHandler.h,v 1.6 2005-04-15 08:37:43 gaede Exp $
    */
   class SIOSimCalHitHandler : public SIOObjectHandler {
-	
   public:
-	
-    /** Reads lcio calorimeter hit objects from an SIO stream.
-     */
-    virtual unsigned int read(SIO_stream* stream, 
-			      EVENT::LCObject** objP)  ;
-	
-    /** Writes lcio objects to an SIO stream.
-     */
-    virtual unsigned int write(SIO_stream* stream, 
-			       const EVENT::LCObject* obj) ;
-	
+    /// Constructor
+    SIOSimCalHitHandler() ;
+
+    /// Reads lcio objects from an SIO stream.
+    void read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) override ;
+
+    /// Writes lcio objects to an SIO stream.
+    void write( sio::write_device& device, const EVENT::LCObject* obj ) override ;
+
+    /// Factory method to create an object of the type of the collection
+    EVENT::LCObject *create() const override ;
   }; // class
 } // namespace
 
