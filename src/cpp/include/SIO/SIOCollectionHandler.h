f760eb3ce902cc2acc77c94649da44a0fa641062
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 11:54:41 +0200

    Missing override keyword

--- src/cpp/include/SIO/SIOCollectionHandler.h
+++ src/cpp/include/SIO/SIOCollectionHandler.h
@@ -12,11 +12,11 @@
 #include <sio/block.h>
 
 namespace SIO {
-  
+
   class SIOObjectHandler ;
-  
+
   /** Handler for LCCollection/LCCollectionIOVec objects for SIO.
-   * 
+   *
    * @author gaede
    * @version $Id: SIOCollectionHandler.h,v 1.8 2005-04-15 08:37:42 gaede Exp $
    */
@@ -25,27 +25,27 @@ namespace SIO {
     SIOCollectionHandler(const SIOCollectionHandler&) = delete;
     SIOCollectionHandler& operator=(const SIOCollectionHandler&) = delete ;
     ~SIOCollectionHandler() = default ;
-        
+
   public:
     /// Constructor with collection (block) name and object handler
     SIOCollectionHandler( const std::string& name, std::shared_ptr<SIOObjectHandler> handler ) ;
 
     /// Get the collection type
     const std::string &type() const ;
-    
+
     /// Set the collection to read/write
     void setCollection( EVENT::LCCollection *col ) ;
 
     // from sio::block
-    void read( sio::read_device &device, sio::version_type vers ) ;
-    void write( sio::write_device &device ) ;
+    void read( sio::read_device &device, sio::version_type vers ) override ;
+    void write( sio::write_device &device ) override ;
 
-  private: 
+  private:
     /// The collection to read/write
     EVENT::LCCollection                *_collection {nullptr} ;
     /// The object handler for reading/writing
     std::shared_ptr<SIOObjectHandler>   _handler {nullptr} ;
   };
-  
+
 } // namespace
 #endif /* ifndef SIO_SIOCOLLECTIONHANDLER_H */
