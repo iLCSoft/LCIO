1952ea7fb078364e1ce7df847e70f1d409e61c77
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 19:14:41 +0200

    Re-implemented SIOTrackerHitZCylinderHandler with new SIO API

--- src/cpp/include/SIO/SIOTrackerHitZCylinderHandler.h
+++ src/cpp/include/SIO/SIOTrackerHitZCylinderHandler.h
@@ -3,30 +3,26 @@
 
 #include "SIO/SIOObjectHandler.h"
 
-
 namespace SIO {
-    
-    
+
 /** Implementation of SIOObjectHandler to handle IO of TrackerHitZCylinder.
  *
  * @author engels
  * @version $Id:$
  */
-
   class SIOTrackerHitZCylinderHandler : public SIOObjectHandler {
-	
   public:
-	
-    /** Reads lcio object from an SIO stream.
-     */
-    virtual unsigned int read(SIO_stream* stream, 
-			      EVENT::LCObject** objP)  ;
-	
-    /** Writes lcio object to an SIO stream.
-     */
-    virtual unsigned int write(SIO_stream* stream, 
-			       const EVENT::LCObject* obj) ;
-	
+    /// Constructor
+    SIOTrackerHitZCylinderHandler() ;
+
+    /// Reads lcio objects from an SIO stream.
+    void read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) override ;
+
+    /// Writes lcio objects to an SIO stream.
+    void write( sio::write_device& device, const EVENT::LCObject* obj ) override ;
+
+    /// Factory method to create an object of the type of the collection
+    EVENT::LCObject *create() const override ;
   }; // class
 } // namespace
 
--- src/cpp/src/SIO/SIOTrackerHitZCylinderHandler.cc
+++ src/cpp/src/SIO/SIOTrackerHitZCylinderHandler.cc
@@ -1,123 +1,81 @@
 #include "SIO/SIOTrackerHitZCylinderHandler.h"
 
-#include "SIO/LCSIO.h"
-
+// -- lcio headers
 #include "EVENT/LCIO.h"
 #include "EVENT/TrackerHitZCylinder.h"
 #include "IOIMPL/TrackerHitZCylinderIOImpl.h"
-
-#include "SIO_functions.h"
-#include "SIO_block.h"
-#include "SIO_stream.h"
-
 #include "IMPL/LCFlagImpl.h"
 
-using namespace EVENT ;
-using namespace IMPL ;
-using namespace IOIMPL ;
+// -- sio headers
+#include <sio/io_device.h>
+#include <sio/version.h>
 
+namespace SIO {
 
-namespace SIO{
+  SIOTrackerHitZCylinderHandler::SIOTrackerHitZCylinderHandler() :
+    SIOObjectHandler( EVENT::LCIO::TRACKERHITZCYLINDER ) {
+    /* nop */
+  }
 
-    unsigned int SIOTrackerHitZCylinderHandler::read(SIO_stream* stream, LCObject** objP){
-        unsigned int status ;
+  //----------------------------------------------------------------------------
 
-        // create a new object :
-        TrackerHitZCylinderIOImpl* hit  = new TrackerHitZCylinderIOImpl ;
-        *objP = hit ;
-
-        LCFlagImpl lcFlag(_flag) ;
-
-        if( _vers > SIO_VERSION_ENCODE( 1, 51) ) {
-            SIO_DATA( stream ,  &(hit->_cellID0) , 1  ) ;
-            if( lcFlag.bitSet( LCIO::RTHZBIT_ID1 ) ){
-                SIO_DATA( stream ,  &(hit->_cellID1) , 1  ) ;
-            }
-        }
-
-        SIO_DATA( stream, &(hit->_type) , 1 ) ;
-        SIO_DATA( stream, hit->_pos     , 3 ) ;
-        SIO_DATA( stream, hit->_center  , 2 ) ;
-        //SIO_DATA( stream, &(hit->_r)    , 1 ) ;
-        SIO_DATA( stream, &(hit->_drphi), 1 ) ;
-        SIO_DATA( stream, &(hit->_dz)   , 1 ) ;
-        SIO_DATA( stream, &(hit->_EDep) , 1 ) ;
-        SIO_DATA( stream, &(hit->_EDepError) , 1 ) ;
-        SIO_DATA( stream, &(hit->_time) , 1 ) ;
-        SIO_DATA( stream, &(hit->_quality) , 1 ) ;
-
-
-        //float cov[TRKHITZCYLNCOVMATRIX] ;
-        //SIO_DATA( stream ,  cov  ,  TRKHITZCYLNCOVMATRIX ) ;
-        //hit->setCovMatrix( cov ) ;
-
-        // rawHits
-        int numberOfRawHits = 1 ; 
-        SIO_DATA( stream ,  &numberOfRawHits , 1  ) ;
-
-        hit->_rawHits.resize( numberOfRawHits ) ;
-
-        for(int i=0;i<numberOfRawHits;i++){
-            SIO_PNTR( stream , &(hit->_rawHits[i] ) ) ;
-        }
-
-        SIO_PTAG( stream , dynamic_cast<const TrackerHitZCylinder*>(hit) ) ;
-
-        return ( SIO_BLOCK_SUCCESS ) ;
-
+  void SIOTrackerHitZCylinderHandler::read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) {
+    auto hit = dynamic_cast<IOIMPL::TrackerHitZCylinderIOImpl*>(objP) ;
+    IMPL::LCFlagImpl lcFlag(_flag) ;
+    if(vers > SIO_VERSION_ENCODE( 1, 51) ) {
+      SIO_DATA( device ,  &(hit->_cellID0) , 1  ) ;
+      if( lcFlag.bitSet( EVENT::LCIO::RTHZBIT_ID1 ) ){
+        SIO_DATA( device ,  &(hit->_cellID1) , 1  ) ;
+      }
     }
-
-
-    unsigned int SIOTrackerHitZCylinderHandler::write(SIO_stream* stream, 
-            const LCObject* obj){
-
-        unsigned int status ; 
-
-        const TrackerHitZCylinder* hit = dynamic_cast<const TrackerHitZCylinder*>(obj)  ;
-
-        LCFlagImpl lcFlag(_flag) ;
-
-        LCSIO_WRITE( stream, hit->getCellID0()  ) ;
-
-        if( lcFlag.bitSet( LCIO::RTHZBIT_ID1 ) ){
-            LCSIO_WRITE( stream, hit->getCellID1()  ) ;
-        }
-
-        LCSIO_WRITE( stream ,  hit->getType() ) ;
-
-        // as SIO doesn't provide a write function with const arguments
-        // we have to cast away the constness 
-        double* pos = const_cast<double*> ( hit->getPosition() ) ; 
-        SIO_DATA( stream,  pos , 3 ) ;
-
-        float* center = const_cast<float*> ( hit->getCenter() ) ; 
-        SIO_DATA( stream,  center , 2 ) ;
-
-        //LCSIO_WRITE( stream, hit->getR()  ) ;
-        LCSIO_WRITE( stream, hit->getdRPhi()  ) ;
-        LCSIO_WRITE( stream, hit->getdZ()  ) ;
-        LCSIO_WRITE( stream, hit->getEDep()  ) ;
-        LCSIO_WRITE( stream, hit->getEDepError()  ) ;
-        LCSIO_WRITE( stream, hit->getTime()  ) ;
-        LCSIO_WRITE( stream, hit->getQuality()  ) ;
-
-        
-        //const FloatVec& cov = hit->getCovMatrix() ;
-        //for(unsigned int i=0;i<cov.size();i++){
-        //    LCSIO_WRITE( stream, cov[i]  ) ;
-        //}
-
-        const EVENT::LCObjectVec& rawHits = hit->getRawHits() ;
-
-        LCSIO_WRITE( stream, rawHits.size()  ) ;
-        for(unsigned int i=0; i < rawHits.size() ; i++){
-            SIO_PNTR( stream , &(rawHits[i]) ) ;
-        }
-
-        SIO_PTAG( stream , hit ) ;
-
-        return ( SIO_BLOCK_SUCCESS ) ;
-
+    SIO_DATA( device, &(hit->_type) , 1 ) ;
+    SIO_DATA( device, hit->_pos     , 3 ) ;
+    SIO_DATA( device, hit->_center  , 2 ) ;
+    SIO_DATA( device, &(hit->_drphi), 1 ) ;
+    SIO_DATA( device, &(hit->_dz)   , 1 ) ;
+    SIO_DATA( device, &(hit->_EDep) , 1 ) ;
+    SIO_DATA( device, &(hit->_EDepError) , 1 ) ;
+    SIO_DATA( device, &(hit->_time) , 1 ) ;
+    SIO_DATA( device, &(hit->_quality) , 1 ) ;
+    int numberOfRawHits = 1 ;
+    SIO_DATA( device ,  &numberOfRawHits , 1  ) ;
+    hit->_rawHits.resize( numberOfRawHits ) ;
+    for( int i=0 ; i<numberOfRawHits ; i++ ) {
+      SIO_PNTR( device , &(hit->_rawHits[i] ) ) ;
     }
+    SIO_PTAG( device , dynamic_cast<const EVENT::TrackerHitZCylinder*>(hit) ) ;
+  }
+
+  //----------------------------------------------------------------------------
+
+  void SIOTrackerHitZCylinderHandler::write( sio::write_device& device, const EVENT::LCObject* obj ) {
+    auto hit = dynamic_cast<const EVENT::TrackerHitZCylinder*>(obj)  ;
+    IMPL::LCFlagImpl lcFlag(_flag) ;
+    SIO_SDATA( device, hit->getCellID0()  ) ;
+    if( lcFlag.bitSet( EVENT::LCIO::RTHZBIT_ID1 ) ) {
+      SIO_SDATA( device, hit->getCellID1()  ) ;
+    }
+    SIO_SDATA( device ,  hit->getType() ) ;
+    SIO_DATA( device,  hit->getPosition() , 3 ) ;
+    SIO_DATA( device,  hit->getCenter() , 2 ) ;
+    SIO_SDATA( device, hit->getdRPhi()  ) ;
+    SIO_SDATA( device, hit->getdZ()  ) ;
+    SIO_SDATA( device, hit->getEDep()  ) ;
+    SIO_SDATA( device, hit->getEDepError()  ) ;
+    SIO_SDATA( device, hit->getTime()  ) ;
+    SIO_SDATA( device, hit->getQuality()  ) ;
+    const EVENT::LCObjectVec& rawHits = hit->getRawHits() ;
+    SIO_SDATA( device, rawHits.size()  ) ;
+    for(unsigned int i=0; i < rawHits.size() ; i++){
+      SIO_PNTR( device , &(rawHits[i]) ) ;
+    }
+    SIO_PTAG( device , hit ) ;
+  }
+
+  //----------------------------------------------------------------------------
+
+  EVENT::LCObject *SIOTrackerHitZCylinderHandler::create() const {
+    return new IOIMPL::TrackerHitZCylinderIOImpl() ;
+  }
 
 } // namespace
