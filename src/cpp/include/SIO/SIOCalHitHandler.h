743bb17f93e10c572415c3433ee1936737990719
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 14:44:15 +0200

    Cosmetic changes

--- src/cpp/include/SIO/SIOCalHitHandler.h
+++ src/cpp/include/SIO/SIOCalHitHandler.h
@@ -3,10 +3,8 @@
 
 #include "SIO/SIOObjectHandler.h"
 
-
 namespace SIO {
-    
-    
+
   /** Implementation of SIOObjectHandler to handle IO of CalorimeterHits.
    *
    * @author gaede
@@ -16,13 +14,13 @@ namespace SIO {
   public:
     /// Constructor
     SIOCalHitHandler() ;
-    
+
     /// Reads lcio objects from an SIO stream.
     void read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) ;
-    
+
     /// Writes lcio objects to an SIO stream.
     void write( sio::write_device& device, const EVENT::LCObject* obj ) ;
-    
+
     /// Factory method to create an object of the type of the collection
     EVENT::LCObject *create() const ;
   }; // class
