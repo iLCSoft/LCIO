29096c4e441cfde88587b00163090ae4802019e0
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 19:31:14 +0200

    Re-implemented SIOTrackerPulseHandler with new SIO API

--- src/cpp/include/SIO/SIOTrackerPulseHandler.h
+++ src/cpp/include/SIO/SIOTrackerPulseHandler.h
@@ -3,30 +3,26 @@
 
 #include "SIO/SIOObjectHandler.h"
 
-
 namespace SIO {
-    
-    
+
 /** Implementation of SIOObjectHandler to handle IO of TrackerPulses.
  *
  * @author gaede
  * @version $Id: SIOTrackerPulseHandler.h,v 1.1 2005-05-31 07:43:25 gaede Exp $
  */
-
   class SIOTrackerPulseHandler : public SIOObjectHandler {
-	
   public:
-	
-    /** Reads lcio TrackerPulse objects from an SIO stream.
-     */
-    virtual unsigned int read(SIO_stream* stream, 
-			      EVENT::LCObject** objP)  ;
-	
-    /** Writes TrackerPulse hit objects to an SIO stream.
-     */
-    virtual unsigned int write(SIO_stream* stream, 
-			       const EVENT::LCObject* obj) ;
-	
+    /// Constructor
+    SIOTrackerPulseHandler() ;
+
+    /// Reads lcio objects from an SIO stream.
+    void read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) override ;
+
+    /// Writes lcio objects to an SIO stream.
+    void write( sio::write_device& device, const EVENT::LCObject* obj ) override ;
+
+    /// Factory method to create an object of the type of the collection
+    EVENT::LCObject *create() const override ;
   }; // class
 } // namespace
 
