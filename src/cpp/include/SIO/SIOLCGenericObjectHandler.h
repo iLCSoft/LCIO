3bd2e3b588b733e0fdcb2e63be1d8a08a630db2b
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 14:43:59 +0200

    Re-implemented SIOLCGenericObjectHandler with new SIO API

--- src/cpp/include/SIO/SIOLCGenericObjectHandler.h
+++ src/cpp/include/SIO/SIOLCGenericObjectHandler.h
@@ -3,11 +3,7 @@
 
 #include "SIO/SIOObjectHandler.h"
 
-#include "EVENT/LCEvent.h"
-
 namespace SIO {
-    
-    
 
   /** Implementation of SIOObjectHandler to handle IO of LCGenericObjects.
    *
@@ -15,31 +11,32 @@ namespace SIO {
    * @version $Id: SIOLCGenericObjectHandler.h,v 1.3 2005-04-15 08:37:42 gaede Exp $
    */
   class SIOLCGenericObjectHandler : public SIOObjectHandler {
-
   public:
-	
-    /** Reads lcio LCGenericObject objects from an SIO stream.
-     */
-    virtual unsigned int read(SIO_stream* stream, 
-			      EVENT::LCObject** objP)  ;
-	
-    /** Writes lcio LCGenericObject objects to an SIO stream.
-     */
-	
-    virtual unsigned int write(SIO_stream* stream, 
-			       const EVENT::LCObject* obj) ;
+    /// Constructor
+    SIOLCGenericObjectHandler() ;
 
-  virtual unsigned int  init( SIO_stream* stream, SIO_operation op,  
-			      EVENT::LCCollection* col  , unsigned int vers ) ; 
+    /// Init collection reading
+    void initReading( sio::read_device &device, EVENT::LCCollection *collection, sio::version_type vers ) override ;
 
+    /// Init collection reading
+    void initWriting( sio::write_device &device, EVENT::LCCollection *collection ) override ;
 
-  protected:
-    int _nInt{0} ;
-    int _nFloat{0} ;
-    int _nDouble{0} ;
-    bool _isFixedSize{false} ;
+    /// Reads lcio objects from an SIO stream.
+    void read( sio::read_device& device, EVENT::LCObject* objP, sio::version_type vers ) override ;
+
+    /// Writes lcio objects to an SIO stream.
+    void write( sio::write_device& device, const EVENT::LCObject* obj ) override ;
+
+    /// Factory method to create an object of the type of the collection
+    EVENT::LCObject *create() const override ;
+
+  private:
+    int         _nInt {0} ;
+    int         _nFloat {0} ;
+    int         _nDouble {0} ;
+    bool        _isFixedSize {false} ;
 
   }; // class
-    
+
 } // namespace
 #endif /* ifndef SIO_SIOLCGENERICOBJECTHANDLER_H */
