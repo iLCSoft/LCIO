84e6d2dd5e70b05d75b72fa587c8761dff521d5d
Remi Ete <eteremi@flc03.desy.de>
2019-07-02 20:29:03 +0200

    Re-implemented SIOIndexHandler with new SIO API. Removed LCIORandomAccessMgr usage. Use directly a RunEventMap instead

--- src/cpp/src/SIO/SIOIndexHandler.cc
+++ src/cpp/src/SIO/SIOIndexHandler.cc
@@ -1,205 +1,121 @@
 #include "SIO/SIOIndexHandler.h"
 
-//#include "SIO/LCIORandomAccessMgr.h"
+// -- lcio headers
 #include "SIO/LCSIO.h"
-#include "SIO_functions.h"
-#include "SIO_stream.h"
-
 #include "EVENT/LCIO.h"
 
-#include <iostream>
-#include <limits.h>
+// -- sio headers
+#include <sio/io_device.h>
+#include <sio/version.h>
+
+// -- std headers
+#include <limits>
 
 namespace SIO  {
 
-
-  SIOIndexHandler::SIOIndexHandler(const std::string& bname, LCIORandomAccessMgr* raMgr) : 
-    SIO_block( bname.c_str() ),
-    _raMgr( raMgr ) {
-    
+  SIOIndexHandler::SIOIndexHandler() :
+    sio::block( LCSIO::IndexBlockName, LCSIO::blockVersion() ),
+    _runEventMap( std::make_shared<RunEventMap>() ) {
+    /* nop */
   }
-  
-  //----------------------------------------------------------------------------
- 
-  SIOIndexHandler::~SIOIndexHandler(){ }
-  
+
   //----------------------------------------------------------------------------
 
-  unsigned int SIOIndexHandler::xfer( SIO_stream* stream, SIO_operation op, 
-					  unsigned int versionID){
-  
-    LCSIO::checkVersion(versionID) ;
-    
-    unsigned int status ; // needed by the SIO_DATA macro
-  
+  void SIOIndexHandler::setRunEventMap( std::shared_ptr<RunEventMap> remap ) {
+    _runEventMap = remap ;
+  }
 
-    if( op == SIO_OP_READ ){ 
+  //----------------------------------------------------------------------------
 
-      int control ;
-      int runMin ;
-      long64 baseOffset ;
-      int size ;
+  std::shared_ptr<RunEventMap> SIOIndexHandler::runEventMap() const {
+    return _runEventMap ;
+  }
 
-      SIO_DATA( stream ,  &control, 1  ) ;
-     
-      bool oneRun     = control & 1 ;
-      bool longOffset = control & 2 ;
+  //----------------------------------------------------------------------------
 
-      //FIXME: do we need this ?
-      if( control & 4 ){  // parameters
-
-	std::cerr << " WARNING: SIOIndexHandler: parameters not implemented .... " << std::endl ;
-
-	return SIO_BLOCK_SKIP ; // FIXME: test
-      }
-
-      SIO_DATA( stream ,  &runMin, 1  ) ;
-      SIO_DATA( stream ,  &baseOffset, 1  ) ;
-      SIO_DATA( stream ,  &size, 1  ) ;
-
-
-//       int nEvents = 0;
-//       int nRunHeaders = 0;
-
-      int runNum ;
-      int evtNum ;
-      int runOffset ;
-      long64 pos ;
-
-      int dummy_int; 
-      long64 dummy_long ;
-
-      for (int i = 0; i < size; i++) {
-
-	runNum = runMin ;
-
-	if( !oneRun ) {
-	  SIO_DATA( stream ,  &runOffset, 1  ) ;
-	  runNum += runOffset ;
-	}
-	SIO_DATA( stream ,  &evtNum , 1  ) ;
-
-// 	if (evtNum >= 0) 
-// 	  nEvents++;
-// 	else 
-// 	  nRunHeaders++;
-
-	if( longOffset ){
-
-	  SIO_DATA( stream ,  &dummy_long , 1  ) ;
-	  pos = dummy_long ;
-
-	} else {
-
-	  SIO_DATA( stream ,  &dummy_int , 1  ) ;
-	  pos = dummy_int ;
-	}
-
-	pos += baseOffset ;
-
-	//	if( evtNum < 0 ) 
-	  //	  std::cout << " +++++++++++ adding run " << runNum << " evt: " << evtNum << " at " << pos <<std::endl ; 
-
-	_raMgr->_runEvtMap.add( RunEvent( runNum , evtNum ) ,  pos ) ;
-      }
-
-//       _raMgr->addLCIORandomAccess( ra ) ;
-//       std::cout << " ... LCIORandomAccess read from stream : " << *ra << std::endl ;
-
-      //****************************************************************************************************
-
-    }  else if( op == SIO_OP_WRITE ){ 
-
-
-//       std::cout << " -- SIOindexHandler - write called - runeventmap :" 
-// 		<< _raMgr->_runEvtMap << std::endl ;
-
-      unsigned control = 0 ;
-
-      RunEvent minEntry  = _raMgr->_runEvtMap.minRunEvent() ;
-      RunEvent maxEntry  = _raMgr->_runEvtMap.maxRunEvent() ;
-      
-      bool oneRun = minEntry.RunNum == maxEntry.RunNum ;
-      
-      if( oneRun )
-	control |= 1 ;
-
-      long64 posMin = _raMgr->_runEvtMap.getPosition( minEntry ) ;
-      long64 posMax = _raMgr->_runEvtMap.getPosition( maxEntry ) ;
-      
-      bool longOffset =   ( posMax - posMin ) > INT_MAX ;
-      
-      if( longOffset )
-	control |= 2 ;
-
-      //+++++ if we would add parameters to the LCIO index record
-      //+++++ control |= 4 ;
-
-      SIO_DATA( stream ,  &control, 1  ) ;
-
-      int runMin = minEntry.RunNum ;
-      SIO_DATA( stream ,  &runMin, 1  ) ;
-
-
-      long64 baseOffset = posMin ;
-      SIO_DATA( stream ,  &baseOffset, 1  ) ;
-
-
-      int size =  _raMgr->_runEvtMap.size() ;
-
-//       std::cout << " -- SIOindexHandler - write : " 
-// 		<< " control : " << control 
-// 		<< " baseoffset : " << baseOffset 
-// 		<< " size: " << size 
-// 		<< " min: " << minEntry 
-// 		<< " max: " << maxEntry
-// 		<< std::endl ;
-
-      
-      SIO_DATA( stream ,  &size, 1  ) ;
-      
-      for( auto it =  _raMgr->_runEvtMap.begin() ; it !=  _raMgr->_runEvtMap.end() ; ++it ){
-	
-	const RunEvent& re = it->first ;
-	
-	if( !oneRun ) {
-
-	  int runOffset =  re.RunNum - runMin ;
-
-	  SIO_DATA( stream ,  &runOffset, 1  ) ;
-	}
-	
-	int evtNum = re.EvtNum ;
-	SIO_DATA( stream ,  &evtNum , 1  ) ;
-	
-	// 	if (evtNum >= 0) 
-	// 	  nEvents++;
-	// 	else 
-	// 	  nRunHeaders++;
-	
-	
-	if( longOffset ){
-	  
-	  long64 dummyL = it->second - baseOffset ;
-	  SIO_DATA( stream ,  &dummyL , 1  ) ;
-	  
-	} else {
-	  
-	  int dummyI = it->second - baseOffset ;
-	  SIO_DATA( stream ,  &dummyI , 1  ) ;
-	}
-	
-      }
-
+  void SIOIndexHandler::read( sio::read_device &device, sio::version_type vers ) {
+    LCSIO::checkVersion( vers ) ;
+    int control ;
+    SIO_DATA( device ,  &control, 1  ) ;
+    bool oneRun     = control & 1 ;
+    bool longOffset = control & 2 ;
+    //FIXME: do we need this ?
+    if( control & 4 ) {  // parameters
+      SIO_THROW( sio::error_code::io_failure, "SIOIndexHandler: parameters not implemented ...." ) ;
     }
-  
-    return ( SIO_BLOCK_SUCCESS ) ;
+    int runMin ;
+    SIO_DATA( device ,  &runMin, 1  ) ;
+    long64 baseOffset ;
+    SIO_DATA( device ,  &baseOffset, 1  ) ;
+    int size ;
+    SIO_DATA( device ,  &size, 1  ) ;
+
+    int runNum ;
+    int evtNum ;
+    int runOffset ;
+    long64 pos ;
+    int dummy_int;
+    long64 dummy_long ;
+
+    for (int i = 0; i < size; i++) {
+    	runNum = runMin ;
+    	if( !oneRun ) {
+    	  SIO_DATA( device ,  &runOffset, 1  ) ;
+    	  runNum += runOffset ;
+    	}
+    	SIO_DATA( device ,  &evtNum , 1  ) ;
+    	if( longOffset ) {
+    	  SIO_DATA( device ,  &dummy_long , 1  ) ;
+    	  pos = dummy_long ;
+    	}
+      else {
+    	  SIO_DATA( device ,  &dummy_int , 1  ) ;
+    	  pos = dummy_int ;
+    	}
+    	pos += baseOffset ;
+    	_runEventMap->add( RunEvent( runNum , evtNum ) ,  pos ) ;
+    }
   }
-  
+
   //----------------------------------------------------------------------------
-  
-  unsigned int SIOIndexHandler::version() const {
-    return SIO_VERSION_ENCODE( EVENT::LCIO::MAJORVERSION, EVENT::LCIO::MINORVERSION ) ;
+
+  void SIOIndexHandler::write( sio::write_device &device ) {
+    unsigned control = 0 ;
+    auto minEntry  = _runEventMap->minRunEvent() ;
+    auto maxEntry  = _runEventMap->maxRunEvent() ;
+    bool oneRun = minEntry.RunNum == maxEntry.RunNum ;
+    if( oneRun ) {
+    	control |= 1 ;
+    }
+    auto posMin = _runEventMap->getPosition( minEntry ) ;
+    auto posMax = _runEventMap->getPosition( maxEntry ) ;
+    bool longOffset =   ( posMax - posMin ) > std::numeric_limits<int>::max() ;
+    if( longOffset ) {
+    	control |= 2 ;
+    }
+    SIO_DATA( device ,  &control, 1  ) ;
+    int runMin = minEntry.RunNum ;
+    SIO_DATA( device ,  &runMin, 1  ) ;
+    long64 baseOffset = posMin ;
+    SIO_DATA( device ,  &baseOffset, 1  ) ;
+    int size = _runEventMap->size() ;
+    SIO_DATA( device ,  &size, 1  ) ;
+    for( auto it = _runEventMap->begin() ; it != _runEventMap->end() ; ++it ) {
+    	if( !oneRun ) {
+    	  int runOffset =  it->first.RunNum - runMin ;
+    	  SIO_DATA( device ,  &runOffset, 1  ) ;
+    	}
+    	int evtNum = it->first.EvtNum ;
+    	SIO_DATA( device ,  &evtNum , 1  ) ;
+    	if( longOffset ) {
+    	  long64 dummyL = it->second - baseOffset ;
+    	  SIO_DATA( device ,  &dummyL , 1  ) ;
+    	}
+      else {
+    	  int dummyI = it->second - baseOffset ;
+    	  SIO_DATA( device ,  &dummyI , 1  ) ;
+    	}
+    }
   }
-  
+
 }
