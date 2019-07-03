54bd39739187a8a00241c812499564e8e23a58f7
Remi Ete <eteremi@flc03.desy.de>
2019-07-03 15:17:31 +0200

    Re-implemented SIOHandlerMgr using shared_ptr instead of raw pointers

--- src/cpp/include/SIO/SIOHandlerMgr.h
+++ src/cpp/include/SIO/SIOHandlerMgr.h
@@ -4,38 +4,38 @@
 // -- std headers
 #include <map>
 #include <string>
+#include <memory>
 
 // -- lcio headers
 #include "SIO/SIOObjectHandler.h"
 
 namespace SIO {
 
-  typedef  std::map< std::string, SIOObjectHandler* > SIOHandlerMap ;
-  
 /** Holds instances of all types of SIOObjectHandlers.
- * New types need to have their handler classes registered here. 
+ * New types need to have their handler classes registered here.
  *
  * @author gaede
  * @version $Id: SIOHandlerMgr.h,v 1.5 2005-04-15 08:37:42 gaede Exp $
  */
   class SIOHandlerMgr {
+    typedef  std::map< std::string, std::shared_ptr<SIOObjectHandler> > SIOHandlerMap ;
   public:
     SIOHandlerMgr() ;
-    ~SIOHandlerMgr() ;
-    
+    ~SIOHandlerMgr() = default ;
+
     /** Returns the instance of the handler for the given type - null if it doesn't exist.
      */
-    SIOObjectHandler* getHandler( const std::string& type ) const ;
-    
-    /** Registers an instance of a handler for the given type. 
+    std::shared_ptr<SIOObjectHandler> getHandler( const std::string& type ) const ;
+
+    /** Registers an instance of a handler for the given type.
      *  Fails if type name allready exists.
      */
-    bool registerHandler( const std::string& type, SIOObjectHandler* handler ) ;
-    
+    void registerHandler( const std::string& type, std::unique_ptr<SIOObjectHandler> handler ) ;
+
   private:
-    
-    SIOHandlerMap _map{};
+    /// The handler map
+    SIOHandlerMap         _map {};
   }; // class
-  
+
 } // namespace
 #endif /* ifndef SIO_SIOHANDLERMGR_H */
