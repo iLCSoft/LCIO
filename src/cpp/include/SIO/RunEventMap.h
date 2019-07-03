cbf1b02846941715c419d79e16d21f8f3afe2304
Remi Ete <eteremi@flc03.desy.de>
2019-07-03 18:06:13 +0200

    Missing const for getters

--- src/cpp/include/SIO/RunEventMap.h
+++ src/cpp/include/SIO/RunEventMap.h
@@ -5,21 +5,21 @@
 #include <map>
 
 namespace SIO {
-  
-  class RunEventMap ; 
-  
+
+  class RunEventMap ;
+
   /** Helper struct that stores run and event positions in the file. The operator<() defines a lexicographical ordering
    *  in RunNum and EvtNum where all run records (EvtNum=-1) are orderd first.
    */
   struct RunEvent {
     typedef long long long64 ;
-    
+
     RunEvent() = default ;
     ~RunEvent() = default ;
     RunEvent(int run, int evt): RunNum( run ), EvtNum( evt ) {}
     RunEvent(long64 runEvt): RunNum( (runEvt >> 32 ) & 0xffffffff  ), EvtNum( runEvt &  0xffffffff ) {}
     operator long64() const {  return  ( ( long64(RunNum) << 32)  |  ( long64(EvtNum) & 0xffffffff )  ) ; }
-    
+
     int RunNum {0} ;
     int EvtNum {0} ;
   };
@@ -30,14 +30,14 @@ namespace SIO {
 
 
 /** Map that holds positions of Run and Event records.
- * 
+ *
  * @author gaede
  * @version $Id: RunEventMap.h,v 1.3 2011-03-03 16:00:12 gaede Exp $
  */
   class RunEventMap {
     typedef long long long64 ;
     friend std::ostream & operator<<(std::ostream& os, const RunEventMap& rm ) ;
-   
+
   public:
     // traits
     typedef std::map< RunEvent, long64> map_type ;
@@ -46,13 +46,13 @@ namespace SIO {
     static constexpr const int npos = -1 ;
 
   public:
-    RunEventMap() = default ; 
+    RunEventMap() = default ;
     ~RunEventMap() = default ;
-    
+
     /** Add a new entry to the event map - if the RunEvent already exists the new position will be stored.
      */
     void add(const RunEvent& re, long64 pos ) ;
-    
+
     /** Return the position of the specified Event record or Run record respectively (if EventNum == -1 ).
      *  Returns RunEventMap::NPos if no record found.
      */
@@ -60,18 +60,18 @@ namespace SIO {
       return getPosition( long64( re ) ) ;
     }
 
-    /** Same as getPosition(const RunEvent& re ). 
+    /** Same as getPosition(const RunEvent& re ).
      */
-    long64 getPosition( long64 re ) ; 
-   
+    long64 getPosition( long64 re ) ;
+
 
     /* Return the minimal run event object - (-1,-1) if empty.
     */
-    RunEvent minRunEvent() ;
-    
+    RunEvent minRunEvent() const ;
+
     /* Return the maximum run event object - (-1,-1) if empty.
     */
-    RunEvent maxRunEvent() ;
+    RunEvent maxRunEvent() const ;
 
 
     int getNumberOfEventRecords() const { return _nEvt ; }
@@ -79,7 +79,7 @@ namespace SIO {
     int getNumberOfRunRecords() const  { return _nRun ; }
 
     // map funcitons
-    size_t size() { return _map.size()  ; } 
+    size_t size() { return _map.size()  ; }
 
     iterator begin()  { return _map.begin() ; }
     iterator end()  { return _map.end() ; }
@@ -88,11 +88,11 @@ namespace SIO {
     bool empty() const { return _map.empty() ; }
 
     template <class InputIterator>
-    void insert(InputIterator b, InputIterator e) { _map.insert( b , e  ) ; } 
+    void insert(InputIterator b, InputIterator e) { _map.insert( b , e  ) ; }
 
     void clear() {  _map.clear() ; _nRun=0 ; _nEvt =0 ; }
 
-  protected: 
+  protected:
     map_type _map{} ;
     int _nRun{0} ;
     int _nEvt{0} ;
