d7ef9e470d5092acb2b34fb2ee58030213445b31
Remi Ete <eteremi@flc03.desy.de>
2019-07-03 13:25:48 +0200

    Re-implemented SIORandomAccessHandler with new SIO API. Removed reference of LCIORandomAccessMgr. Use only a LCIORandomAccess object

--- src/cpp/src/SIO/SIORandomAccessHandler.cc
+++ src/cpp/src/SIO/SIORandomAccessHandler.cc
@@ -1,98 +1,58 @@
 #include "SIO/SIORandomAccessHandler.h"
 
+// -- lcio headers
 #include "SIO/LCIORandomAccess.h"
 #include "SIO/LCSIO.h"
-#include "SIO_functions.h"
-#include "SIO_stream.h"
 
-#include "EVENT/LCIO.h"
-
-#include <iostream>
-
+// -- sio headers
+#include <sio/io_device.h>
+#include <sio/version.h>
 
 namespace SIO  {
 
-
-  SIORandomAccessHandler::SIORandomAccessHandler(const std::string& bname, LCIORandomAccessMgr* raMgr) : 
-    SIO_block( bname.c_str() ),
-    _raMgr( raMgr ) {
-    
+  SIORandomAccessHandler::SIORandomAccessHandler() :
+    sio::block( LCSIO::AccessBlockName, LCSIO::blockVersion() ),
+    _randomAccess( std::make_shared<LCIORandomAccess>() ) {
+    /* nop */
   }
- 
 
-  SIORandomAccessHandler::~SIORandomAccessHandler(){ }
+  //----------------------------------------------------------------------------
 
-  
-  unsigned int SIORandomAccessHandler::xfer( SIO_stream* stream, SIO_operation op, 
-					  unsigned int versionID){
-  
-    LCSIO::checkVersion(versionID) ;
-    
-    unsigned int status ; // needed by the SIO_DATA macro
-  
-
-    if( op == SIO_OP_READ ){ 
-
-      LCIORandomAccess* ra = new LCIORandomAccess ;
-
-      SIO_DATA( stream ,  &(ra->_minRunEvt.RunNum) , 1  ) ;
-      SIO_DATA( stream ,  &(ra->_minRunEvt.EvtNum) , 1  ) ;
-      
-      SIO_DATA( stream ,  &( ra->_maxRunEvt.RunNum) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_maxRunEvt.EvtNum) , 1  ) ;
-      
-      SIO_DATA( stream ,  &( ra->_nRunHeaders) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_nEvents) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_recordsAreInOrder) , 1  ) ;
-      
-      SIO_DATA( stream ,  &( ra->_indexLocation) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_prevLocation) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_nextLocation) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_firstRecordLocation) , 1  ) ;
-
- 
-      // read record size - ignored for now - need to read if record size changes in the future ...
-      int recSize ;
-      SIO_DATA( stream ,  &recSize , 1  ) ;
-
-      
-      _raMgr->addLCIORandomAccess( ra ) ;
-
-    }  else if( op == SIO_OP_WRITE ){ 
-    
-
-      LCIORandomAccess* ra = const_cast<LCIORandomAccess*> ( _raMgr->lastLCIORandomAccess() ) ;
-      // these SIO functions really should also exist for const arguments ....
-
-      SIO_DATA( stream ,  &(ra->_minRunEvt.RunNum) , 1  ) ;
-      SIO_DATA( stream ,  &(ra->_minRunEvt.EvtNum) , 1  ) ;
-      
-      SIO_DATA( stream ,  &( ra->_maxRunEvt.RunNum) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_maxRunEvt.EvtNum) , 1  ) ;
-      
-      SIO_DATA( stream ,  &( ra->_nRunHeaders) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_nEvents) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_recordsAreInOrder) , 1  ) ;
-      
-      SIO_DATA( stream ,  &( ra->_indexLocation) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_prevLocation) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_nextLocation) , 1  ) ;
-      SIO_DATA( stream ,  &( ra->_firstRecordLocation) , 1  ) ;
-      
-
-      // write the current size of the LCIORandomAccess record as last word - incl. check at bits 31-16: 0xabcd
-      int recSize = (   0xabcd0000 | LCSIO_RANDOMACCESS_SIZE ) ;
-      SIO_DATA( stream ,  &recSize , 1  ) ;
-     
-    }
-    
-    
-    return ( SIO_BLOCK_SUCCESS ) ;
+  void SIORandomAccessHandler::read( sio::read_device &device, sio::version_type vers ) {
+    LCSIO::checkVersion( vers ) ;
+    SIO_SDATA( device ,  _randomAccess->_minRunEvt.RunNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_minRunEvt.EvtNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_maxRunEvt.RunNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_maxRunEvt.EvtNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_nRunHeaders ) ;
+    SIO_SDATA( device ,  _randomAccess->_nEvents ) ;
+    SIO_SDATA( device ,  _randomAccess->_recordsAreInOrder ) ;
+    SIO_SDATA( device ,  _randomAccess->_indexLocation ) ;
+    SIO_SDATA( device ,  _randomAccess->_prevLocation ) ;
+    SIO_SDATA( device ,  _randomAccess->_nextLocation ) ;
+    SIO_SDATA( device ,  _randomAccess->_firstRecordLocation ) ;
+    // read record size - ignored for now - need to read if record size changes in the future ...
+    int recSize ;
+    SIO_SDATA( device, recSize ) ;
   }
-  
-  unsigned int   SIORandomAccessHandler::version() const {
-    
-    return SIO_VERSION_ENCODE( EVENT::LCIO::MAJORVERSION, EVENT::LCIO::MINORVERSION ) ;
+
+  //----------------------------------------------------------------------------
+
+  void SIORandomAccessHandler::write( sio::write_device &device ) {
+    SIO_SDATA( device ,  _randomAccess->_minRunEvt.RunNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_minRunEvt.EvtNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_maxRunEvt.RunNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_maxRunEvt.EvtNum ) ;
+    SIO_SDATA( device ,  _randomAccess->_nRunHeaders ) ;
+    SIO_SDATA( device ,  _randomAccess->_nEvents ) ;
+    SIO_SDATA( device ,  _randomAccess->_recordsAreInOrder ) ;
+    SIO_SDATA( device ,  _randomAccess->_indexLocation ) ;
+    SIO_SDATA( device ,  _randomAccess->_prevLocation ) ;
+    SIO_SDATA( device ,  _randomAccess->_nextLocation ) ;
+    SIO_SDATA( device ,  _randomAccess->_firstRecordLocation ) ;
+    // write the current size of the LCIORandomAccess record as last word - incl. check at bits 31-16: 0xabcd
+    int recSize = ( 0xabcd0000 | LCSIO::RandomAccessSize ) ;
+    SIO_SDATA( device, recSize ) ;
   }
-  
+
 }
