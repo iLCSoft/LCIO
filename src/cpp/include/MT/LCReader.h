c753e917dc37aa3005922dd92f0594c5a373432e
Remi Ete <eteremi@flc03.desy.de>
2019-07-08 13:46:49 +0200

    Re-implemented MT::LCReader class with SIO API. Need to be tested

--- src/cpp/include/MT/LCReader.h
+++ src/cpp/include/MT/LCReader.h
@@ -11,12 +11,13 @@
 #include "LCIOSTLTypes.h"
 #include "LCIOTypes.h"
 #include "EVENT/LCIO.h"
+#include "SIO/SIOHandlerMgr.h"
 
 // -- sio headers
-#include "SIO_definitions.h"
+#include "sio/definitions.h"
+#include "sio/buffer.h"
 
 namespace SIO {
-  class LCIORecords ;
   class LCIORandomAccessMgr ;
 }
 
@@ -40,7 +41,7 @@ public:
 
 public:
   ~LCReader() = default;
-  
+
   /**
    *  @brief  Constructor
    *
@@ -59,38 +60,38 @@ public:
    */
   void open( const std::vector<std::string> & filenames ) ;
 
-  /** Reads the next run header from the file. 
+  /** Reads the next run header from the file.
    *  Returns NULL if 'EOF' read.
    *  Note that this method is not thread safe !
    */
   LCRunHeaderPtr readNextRunHeader( int accessMode = EVENT::LCIO::READ_ONLY ) ;
 
-  /** Reads the next event from the file. 
-   *  Returns NULL if 'EOF' read. 
+  /** Reads the next event from the file.
+   *  Returns NULL if 'EOF' read.
    *  Note that this method is not thread safe !
    */
   LCEventPtr readNextEvent( int accessMode = EVENT::LCIO::READ_ONLY ) ;
 
   /** Return the number of events in the file - the file has to be open. In
-   *  case several input files are specified in the open() method - 
-   *  the number of events in the file that is currently open is returned. 
+   *  case several input files are specified in the open() method -
+   *  the number of events in the file that is currently open is returned.
    */
   int getNumberOfEvents() ;
 
   /** Return the number of runs (run headers) in the file - the file has to be open. In
-   *  case several input files are specified in the open() method - 
-   *  the number of runs (run headers) in the file that is currently open is returned. 
+   *  case several input files are specified in the open() method -
+   *  the number of runs (run headers) in the file that is currently open is returned.
    */
   int getNumberOfRuns() ;
 
   /** Return the run numbers of the runs (run headers) in the file - the file has to be open. In
-   *  case several input files are specified in the open() method - 
-   *  the run numbers of the runs (run headers) in the file that is currently open is returned. 
+   *  case several input files are specified in the open() method -
+   *  the run numbers of the runs (run headers) in the file that is currently open is returned.
    */
   void getRuns( EVENT::IntVec & runs ) ;
 
   /** Return the run and event numbers of the events in the file - the file has to be open. In
-   *  case several input files are specified in the open() method - 
+   *  case several input files are specified in the open() method -
    *  the  run and event numbers of the events in the file that is currently open are returned.
    *  The size of the vector events will be twice the number of events, where i-th run number
    *  will be in events[2*i] and the i-th event number in  events[2*i+].
@@ -99,7 +100,7 @@ public:
 
   /** Limit the collection names that are going to be read to the subset given in the vector -
    *  all other collection will be ignored. This might improve the reading performance
-   *  considerably in cases where only a small subset of the collections in the event is needed. 
+   *  considerably in cases where only a small subset of the collections in the event is needed.
    */
   void setReadCollectionNames( const std::vector<std::string> & colnames ) ;
 
@@ -121,62 +122,66 @@ public:
    */
   void close() ;
 
-  /** Reads the input stream and notifies 
-   * listeners according to the object type 
-   * found in the stream. 
+  /** Reads the input stream and notifies
+   * listeners according to the object type
+   * found in the stream.
    */
   void readStream( const LCReaderListenerList & listeners ) ;
 
-  /** Reads maxRecord from the input stream and notifies 
+  /** Reads maxRecord from the input stream and notifies
    * listeners according to the object type found in the stream.
    * An exception is thrown if less than maxRecord where read from the file.
    */
   void readStream( const LCReaderListenerList & listeners , int maxRecord ) ;
-  
-  /** Reads the input stream and notifies 
-   * listeners according to the object type 
-   * found in the stream. 
+
+  /** Reads the input stream and notifies
+   * listeners according to the object type
+   * found in the stream.
    */
   void readNextRecord( const LCReaderListenerList & listeners ) ;
-  
-  /** Reads the input stream and notifies the 
-   * listener according to the object type 
-   * found in the stream. 
+
+  /** Reads the input stream and notifies the
+   * listener according to the object type
+   * found in the stream.
    */
   void readStream( LCReaderListener *listener ) ;
 
-  /** Reads maxRecord from the input stream and notifies 
+  /** Reads maxRecord from the input stream and notifies
    * listener according to the object type found in the stream.
    * An exception is thrown if less than maxRecord where read from the file.
    */
   void readStream( LCReaderListener *listener , int maxRecord ) ;
-  
-  /** Reads the input stream and notifies 
-   * listener according to the object type 
-   * found in the stream. 
+
+  /** Reads the input stream and notifies
+   * listener according to the object type
+   * found in the stream.
    */
   void readNextRecord( LCReaderListener *listener ) ;
-    
+
 private:
-  void readRecord( const sio::record_map &records , sio::record_read_result &readResult ) ;
+  // void readRecord( const sio::record_map &records , sio::record_read_result &readResult ) ;
   void postProcessEvent( EVENT::LCEvent *evt ) ;
   void getEventMap() ;
-    
+
 private:
+  /// The input file stream
+  sio::ifstream                               _stream {} ;
+  /// The raw buffer for extracting bytes from the stream
+  sio::buffer                                 _rawBuffer {32*sio::kbyte} ;
+  /// The raw buffer for uncompression
+  sio::buffer                                 _compBuffer {64*sio::kbyte} ;
+  /// The collection block handler manager for events
+  SIO::SIOHandlerMgr                          _eventHandlerMgr {} ;
   /// Whether to read the event map using the random access manager
-  bool _readEventMap{false} ;
-  /// The SIO stream for reading
-  sio::stream_ptr _stream{nullptr} ;
-  /// The LCIO records holder
-  std::shared_ptr<SIO::LCIORecords> _lcioRecords {nullptr} ;
+  bool                                        _readEventMap {false} ;
   /// The list of files to open and read
-  std::vector<std::string> _myFilenames{} ;
-  /// A restricted list of collections to read only 
-  std::vector<std::string> _readCollectionNames{} ;
+  std::vector<std::string>                    _myFilenames {} ;
+  /// A restricted list of collections to read only
+  std::vector<std::string>                    _readCollectionNames {} ;
   /// The current file list index when opening multiple files
-  unsigned int _currentFileIndex{0} ;
-  /// The random access manager for event/run random access in the file 
-  std::shared_ptr<SIO::LCIORandomAccessMgr> _raMgr {nullptr} ;
+  unsigned int                                _currentFileIndex {0} ;
+  /// The random access manager for event/run random access in the file
+  std::shared_ptr<SIO::LCIORandomAccessMgr>   _raMgr {nullptr} ;
 }; // class
 
 } // namespace MT
