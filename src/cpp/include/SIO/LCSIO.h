f11bd1e066f32fa071d2b5492993ac496862875b
Remi Ete <eteremi@flc03.desy.de>
2019-07-08 18:44:46 +0200

    Use int instead of long64 for RandomAccessSize to avoid overflow in bit shifting

--- src/cpp/include/SIO/LCSIO.h
+++ src/cpp/include/SIO/LCSIO.h
@@ -6,7 +6,7 @@
 
 namespace SIO {
 
-/** 
+/**
  *  @brief  Collection of constants and helper functions
  *
  *  @author gaede
@@ -14,10 +14,11 @@ namespace SIO {
  *  @version June, 2019
  */
   class LCSIO {
+    typedef long long long64 ;
   public:
     /// The lcio file extension name
     static constexpr const char * FileExtension     = ".slcio" ;
-    
+
     // record and block names used in LCIO-SIO
     static constexpr const char * RunRecordName     = "LCRunHeader" ;
     static constexpr const char * RunBlockName      = "RunHeader" ;
@@ -27,23 +28,23 @@ namespace SIO {
     static constexpr const char * HeaderBlockName   = "EventHeader" ;
     static constexpr const char * AccessRecordName  = "LCIORandomAccess" ;
     static constexpr const char * AccessBlockName   = "LCIORandomAccess" ;
-    static constexpr unsigned int RandomAccessSize  = 136 ; 
+    static constexpr int          RandomAccessSize  = 136 ;
     static constexpr const char * IndexRecordName   = "LCIOIndex" ;
     static constexpr const char * IndexBlockName    = "LCIOIndex" ;
 
     /**
      *  @brief  Check for old version of LCIO (> v01-08 ar no longer supported)
      *          Throws an exception if not supported
-     * 
+     *
      *  @param  version the version to check
      */
     static void checkVersion( sio::version_type versionID ) ;
-    
+
     /**
-     *  @brief  Get the LCIO encoded version number for writing in sio blocks 
+     *  @brief  Get the LCIO encoded version number for writing in sio blocks
      */
     static sio::version_type blockVersion() ;
   };
 
-} // namespace 
+} // namespace
 #endif // ifndef SIO_LCSIO_H
