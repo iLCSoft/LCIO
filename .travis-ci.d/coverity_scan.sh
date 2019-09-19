488f67b29ab70f664825e970385c06a7239db063
Marko Petric <marko.petric@cern.ch>
2019-09-19 11:40:20 +0200

    Update CI to LCG_96b

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
-source $ILCSOFT/init_ilcsoft.sh
+source /Package/.travis-ci.d/init_x86_64.sh
 
 cd /Package
 mkdir build
 cd build
-cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON .. && \
+cmake -DCMAKE_CXX_STANDARD=${STANDARD} -DBUILD_ROOTDICT=ON .. && \
 export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
 cov-build --dir cov-int make -j2 && \
 tar czvf myproject.tgz cov-int
---
+++ .travis-ci.d/init_x86_64.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Determine which OS you are using
+if [ "$(uname)" == "Linux" ]; then
+    if [ "$( cat /etc/*-release | grep Scientific )" ]; then
+        OS=slc6
+    elif [ "$( cat /etc/*-release | grep CentOS )" ]; then
+        OS=centos7
+    fi
+else
+    echo "UNKNOWN OS"
+    exit 1
+fi
+
+#Determine is you have CVMFS installed
+if [ ! -d "/cvmfs" ]; then
+    echo "No CVMFS detected, please install it."
+    exit 1
+fi
+
+if [ ! -d "/cvmfs/sft.cern.ch" ]; then
+    echo "No sft CVMFS repository detected, please add it."
+    exit 1
+fi
+
+
+#Determine which compiler to use
+if [ -z ${COMPILER_TYPE} ]; then
+    COMPILER_TYPE="gcc"
+fi
+if [ -z ${COMPILER_VERSION} ]; then
+    if [ ${COMPILER_TYPE} == "gcc" ]; then
+        COMPILER_VERSION="gcc8"
+    fi
+    if [[ ${COMPILER_TYPE} == "clang" ]]; then
+        COMPILER_VERSION="clang8"
+    fi
+fi
+
+
+#Choose build type
+if [ -z ${BUILD_TYPE} ]; then
+    BUILD_TYPE=opt
+fi
+
+
+# General variables
+if [ -z ${LCG_RELEASE} ]; then
+    LCG_RELEASE="LCG_96b"
+fi
+
+LCGREPO=/cvmfs/sft.cern.ch/lcg/releases/${LCG_RELEASE}
+BUILD_FLAVOUR=x86_64-${OS}-${COMPILER_VERSION}-${BUILD_TYPE}
+
+echo /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${BUILD_FLAVOUR}/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${BUILD_FLAVOUR}/setup.sh
