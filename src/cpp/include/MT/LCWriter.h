508355562b5ef4d61fc1ca16df23890c2ebe50e8
Remi Ete <remi.ete@gmail.com>
2019-09-19 15:51:39 +0200

    Finished first implementation of the thread safe writer MT::LCWriter. To be tested

--- src/cpp/include/MT/LCWriter.h
+++ src/cpp/include/MT/LCWriter.h
@@ -6,12 +6,14 @@
 #include <vector>
 #include <fstream>
 #include <memory>
+#include <mutex>
+#include <atomic>
 
 // -- lcio headers
 #include "MT/Types.h"
 #include "EVENT/LCIO.h"
 
-// need to make forward declaration to 
+// need to make forward declaration to
 // avoid lcio dependencies to include directly sio headers
 
 namespace sio {
@@ -27,7 +29,14 @@ namespace MT {
   /**
    *  @brief  LCWriter class.
    *          Implement a writer for events and run headers using SIO.
-   *          The implementation of the write methods are meant to be thread safe
+   *          Thread safety specifications:
+   *          - Writing two events from different threads is safe
+   *          - Writing an event and a run header from different threads is safe
+   *          - Opening/closing a file and writing might throw an exception depending on the scheduling
+   *          A mutex ensures that no data race will occur but won't protect
+   *          against race conditions
+   *
+   *  @author R. Ete, DESY
    */
   class LCWriter {
   public:
@@ -40,20 +49,20 @@ namespace MT {
     /// Destructor
     ~LCWriter() = default ;
 
-    /** 
-     *  @brief  Opens a file for writing. If file with given name exists, 
+    /**
+     *  @brief  Opens a file for writing. If file with given name exists,
      *          an exception is thrown. Use append or new mode instead.
      */
     void open( const std::string &filename ) ;
 
-    /** 
+    /**
      *  @brief  Opens a file for writing.
      *          Possible write modes are: LCIO::WRITE_NEW
-     *          (existing files are replaced) and LCIO::WRITE_APPEND. 
+     *          (existing files are replaced) and LCIO::WRITE_APPEND.
      */
     void open( const std::string &filename, int writeMode ) ;
 
-    /** 
+    /**
      *  @brief  Set the compression level - needs to be called before open() otherwise
      *          call will have no effect. If not called the Writer will use default compression.<br>
      *          Valid compression levels are:
@@ -62,41 +71,41 @@ namespace MT {
      *          - level >  0 : compression level (typically 1 (fastest) - 9 (best compression))
      *          No data race expected (atomic variable in use) but a race condition can occur if
      *          the compression level is set while write a record
-     *          
+     *
      *  @param level compression level
      */
     void setCompressionLevel( int level ) ;
 
-    /** 
+    /**
      *  @brief  Writes the given run header to file.
      */
     void writeRunHeader( EVENT::LCRunHeader *hdr ) ;
 
-    /** 
+    /**
      *  @brief  Writes the given event to file.
      */
     void writeEvent( EVENT::LCEvent *evt ) ;
 
-    /** 
+    /**
      *  @brief  Closes the output file/stream.
      */
     void close() ;
-    
+
   private:
     static std::string getSIOFileName( const std::string& filename ) ;
-    
+
   private:
     /// The output file stream
-    std::ofstream                        _stream {} ;
-    /// The raw buffer for writing bytes to the stream
-    std::shared_ptr<sio::buffer>         _rawBuffer {nullptr} ;
-    /// The raw buffer for compression
-    std::shared_ptr<sio::buffer>         _compBuffer {nullptr} ;
+    std::ofstream                               _stream {} ;
     /// The compression level
-    std::atomic<int>                     _compressionLevel {0} ;
+    std::atomic<unsigned int>                   _maxBufferSize {2*1204*1024} ;
+    /// The compression level
+    std::atomic<int>                            _compressionLevel {0} ;
     /// The random access manager for event/run random access in the file
     std::shared_ptr<SIO::LCIORandomAccessMgr>   _raMgr {nullptr} ;
-  }; // class
+    /// Synchronization mutex
+    std::mutex                                  _mutex {} ;
+  };
 
 }
 
