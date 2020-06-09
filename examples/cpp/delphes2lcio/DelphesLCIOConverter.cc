5c8fc6828326cbbd0f47f2384085e326ff98dcfd
Frank Gaede <frank.gaede@desy.de>
2020-06-09 08:04:57 +0200

    add event parameters

--- examples/cpp/delphes2lcio/DelphesLCIOConverter.cc
+++ examples/cpp/delphes2lcio/DelphesLCIOConverter.cc
@@ -38,7 +38,7 @@ DelphesLCIOConverter::DelphesLCIOConverter(const char* fileName ) {
   
   if( ! fn.empty()){
     fWriter = lcio::LCFactory::getInstance()->createLCWriter() ;
-    fWriter->open( fn ) ;
+    fWriter->open( fn , lcio::LCIO::WRITE_NEW ) ;
   }
 
 }
@@ -63,58 +63,86 @@ void DelphesLCIOConverter::writeEvent(TTree* tree){
 }
 //-----------------------------------------------------------------
 
-void DelphesLCIOConverter::convertTree2LCIO( TTree *tree , lcio::LCEvent* evt){
+void DelphesLCIOConverter::convertTree2LCIO( TTree *tree , lcio::LCEventImpl* evt){
 
 
-  int nEntry =  tree->GetEntriesFast() ;
-  
-  auto* mcps = new LCCollectionVec( LCIO::MCPARTICLE )  ;
+  //int nEntry =  tree->GetEntriesFast() ;
 
   size_t n = tree->GetListOfBranches()->GetEntries();
   for( size_t i = 0; i < n; ++ i ) {
     TBranch *br = dynamic_cast<TBranch*>(tree->GetListOfBranches()->At(i));
-//    cout << " *************** branch \"" << br->GetName() << " --- " << br->GetClassName() << std::endl;
-
-    
-    if( !strcmp( br->GetName(), "Particle" ) ){
-      TClonesArray* col = *(TClonesArray**) br->GetAddress()  ;
-
-      int ngp = col->GetEntriesFast();
-      
-      for(int j = 0; j < ngp ; ++j){
-       	GenParticle* p = (GenParticle*) col->At(j);
-
-	auto* mcp = new MCParticleImpl ;
-	mcps->addElement( mcp ) ;
-
-	mcp->setPDG(p->PID) ;
-	double m[3] = { p->Px , p->Py , p->Pz }  ;
-	mcp->setMomentum( m ) ;
-	mcp->setGeneratorStatus( p->Status );
-	mcp->setCharge( p->Charge );
-	mcp->setMass( p->Mass );
-	mcp->setTime( p->T ) ;
-	double v[3] = { p->X , p->Y , p->Z }  ;
-	mcp->setVertex( v ) ;
-      }
-      for(int j = 0; j < ngp ; ++j){
-	GenParticle* p = (GenParticle*) col->At(j);
-	auto* mcp = (MCParticleImpl*) mcps->getElementAt(j) ;
-
-	if( p->M1 > -1 )
-	  mcp->addParent( (MCParticle*) mcps->getElementAt( p->M1 ) ) ;
-	if( p->M2 > -1 )
-	  mcp->addParent( (MCParticle*) mcps->getElementAt( p->M2 ) ) ;
-      }
-    }
-
+    std::cout << " *************** branch \"" << br->GetName() << " --- " << br->GetClassName() << std::endl;
   }
 
-  evt->addCollection( mcps, "MCParticle" ) ;
+
+  //=====================================================================
+  TBranch *evB = tree->GetBranch("Event");
+
+  if( evB != nullptr ){
+    TClonesArray* col = *(TClonesArray**) evB->GetAddress()  ;
+    LHEFEvent* e = (LHEFEvent*) col->At(0);
+
+    // Float_t ReadTime; // read time
+    // Float_t ProcTime; // processing time
+    // Int_t Trigger; // trigger word
+    // Int_t ProcessID; // subprocess code for the event | hepup.IDPRUP
+    // Float_t Weight; // weight for the event | hepup.XWGTUP
+    // Float_t CrossSection; // cross-section (read from init, implemented only for Wizard evgen)
+    // Float_t ScalePDF; // scale in GeV used in the calculation of the PDFs in the event | hepup.SCALUP
+    // Float_t AlphaQED; // value of the QED coupling used in the event | hepup.AQEDUP
+    // Float_t AlphaQCD; // value of the QCD coupling used in the event | hepup.AQCDUP
+
+    evt->setRunNumber( e->ProcessID ) ;  // is this useful ?
+    evt->setEventNumber( e->Number ) ;
+    evt->setWeight( e->Weight ) ;
+    evt->setTimeStamp( e->ReadTime ) ;
+  }
+
+  //=====================================================================
+
+  TBranch *gpB = tree->GetBranch("Particle");
+
+  if( gpB != nullptr ){
+
+    auto* mcps = new LCCollectionVec( LCIO::MCPARTICLE )  ;
+    evt->addCollection( mcps, "MCParticle" ) ;
+
+    TClonesArray* col = *(TClonesArray**) gpB->GetAddress()  ;
+
+    int ngp = col->GetEntriesFast();
+
+    for(int j = 0; j < ngp ; ++j){
+      GenParticle* p = (GenParticle*) col->At(j);
+
+      auto* mcp = new MCParticleImpl ;
+      mcps->addElement( mcp ) ;
+      
+      mcp->setPDG(p->PID) ;
+      double m[3] = { p->Px , p->Py , p->Pz }  ;
+      mcp->setMomentum( m ) ;
+      mcp->setGeneratorStatus( p->Status );
+      mcp->setCharge( p->Charge );
+      mcp->setMass( p->Mass );
+      mcp->setTime( p->T ) ;
+      double v[3] = { p->X , p->Y , p->Z }  ;
+      mcp->setVertex( v ) ;
+    }
+    for(int j = 0; j < ngp ; ++j){
+      GenParticle* p = (GenParticle*) col->At(j);
+      auto* mcp = (MCParticleImpl*) mcps->getElementAt(j) ;
+
+      if( p->M1 > -1 )
+	mcp->addParent( (MCParticle*) mcps->getElementAt( p->M1 ) ) ;
+      if( p->M2 > -1 )
+	mcp->addParent( (MCParticle*) mcps->getElementAt( p->M2 ) ) ;
+    }
+  }
+  //=====================================================================
+
+
 
 
   LCTOOLS::dumpEvent( evt ) ;
-
 
 }
 
