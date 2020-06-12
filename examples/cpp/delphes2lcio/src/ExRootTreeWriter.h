e868e5f32d3235302dca91644e9c7dcaf1afb82e
Frank Gaede <frank.gaede@desy.de>
2020-06-12 12:25:28 +0200

    add modified ExRootTreeWriter.h

---
+++ examples/cpp/delphes2lcio/src/ExRootTreeWriter.h
@@ -0,0 +1,55 @@
+#ifndef ExRootTreeWriter_h
+#define ExRootTreeWriter_h
+
+/** \class ExRootTreeWriter
+ *
+ *  Class handling output ROOT tree
+ *
+ *  \author P. Demin - UCL, Louvain-la-Neuve
+ *  F.Gaede, DESY
+ *  added accessor functions setTree() and getTree for delphes2lcio
+ *
+ */
+
+#include "TNamed.h"
+
+#include <set>
+
+class TFile;
+class TTree;
+class TClass;
+class ExRootTreeBranch;
+
+class ExRootTreeWriter: public TNamed
+{
+public:
+  ExRootTreeWriter(TFile *file = 0, const char *treeName = "Analysis");
+  ~ExRootTreeWriter();
+
+  void SetTreeFile(TFile *file) { fFile = file; }
+  void SetTreeName(const char *name) { fTreeName = name; }
+
+  ExRootTreeBranch *NewBranch(const char *name, TClass *cl);
+
+
+  void  setTree(TTree* t) { fTree = t ; }
+  TTree* getTree() { return fTree ; }
+
+  void Clear();
+  void Fill();
+  void Write();
+
+private:
+  TTree *NewTree();
+
+  TFile *fFile; //!
+  TTree *fTree; //!
+
+  TString fTreeName; //!
+
+  std::set<ExRootTreeBranch *> fBranches; //!
+
+  ClassDef(ExRootTreeWriter, 1)
+};
+
+#endif /* ExRootTreeWriter */
