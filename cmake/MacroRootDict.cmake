8d4ef740cde50e8f40c63829cd2bb0ae30e796de
flagarde <lagarde@sjtu.edu.cn>
2020-07-17 11:28:41 +0800

    Use default ROOTConfig

--- cmake/MacroRootDict.cmake
+++ cmake/MacroRootDict.cmake
@@ -124,7 +124,7 @@ MACRO( GEN_ROOT_DICT_SOURCE _dict_src_filename )
     ADD_CUSTOM_COMMAND(
         OUTPUT  ${_dict_src_file} ${_dict_hdr_file}
         COMMAND mkdir -p ${ROOT_DICT_OUTPUT_DIR}
-        COMMAND ${ROOT_CINT_WRAPPER} -f "${_dict_src_file}" -c ${ROOT_DICT_CINT_DEFINITIONS} ${_dict_includes} ${ROOT_DICT_INPUT_HEADERS}
+        COMMAND ${ROOT_rootcint_CMD} -f "${_dict_src_file}" ${ROOT_DICT_CINT_DEFINITIONS} ${_dict_includes} ${ROOT_DICT_INPUT_HEADERS}
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         DEPENDS ${ROOT_DICT_INPUT_HEADERS}
         COMMENT "generating: ${_dict_src_file} ${_dict_hdr_file}"
