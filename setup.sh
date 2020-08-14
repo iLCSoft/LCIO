8d0cec1fe4c0a921425e92ea92e6e9ff52305964
Frank Gaede <frank.gaede@desy.de>
2020-08-14 13:54:11 +0200

    set LD_LIBRARY_PATH in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+#
+# initialize the current LCIO version by sourcing this in the top level directory
+#
+
 export LCIO=`pwd`
 export PATH=$LCIO/bin:$LCIO/tools:$PATH
+export LD_LIBRARY_PATH=$LCIO/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$LCIO/src/python:$PYTHONPATH
 alias pylcio='python $LCIO/src/python/pylcio.py'
