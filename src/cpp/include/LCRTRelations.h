b14d749abcd3cc656ccec08807162e601a5cc597
Omar Moreno <email@omarmoreno.net>
2022-11-17 13:54:23 -0800

    Make destructor virtual.

--- src/cpp/include/LCRTRelations.h
+++ src/cpp/include/LCRTRelations.h
@@ -476,7 +476,7 @@ namespace lcrtrel{
 
 
 
-    ~LCRTRelations() {
+    virtual ~LCRTRelations() {
       for( PtrMap::iterator it = _map.begin() ; it != _map.end() ; ++it ){
 	cleaners()[ it->first ] ( it->second ) ;  // call the delete function
       }
