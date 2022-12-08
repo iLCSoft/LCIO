47495a70f3b36a0fd24fdcd3e3d60dd9eee955e3
Frank Gaede <frank.gaede@desy.de>
2022-12-08 17:15:44 +0100

    add utility class CheckCollections

--- src/cpp/src/EXAMPLE/check_missing_cols.cc
+++ src/cpp/src/EXAMPLE/check_missing_cols.cc
@@ -2,8 +2,9 @@
 
 #include "MT/LCReader.h"
 #include "EVENT/LCCollection.h"
+#include "UTIL/CheckCollections.h"
 
-#include <map>
+#include <unordered_map>
 
 static std::vector<std::string> FILEN ; 
 
@@ -38,80 +39,14 @@ int main(int argc, char** argv ){
     
     lcReader.close() ;
   }  
+
+  UTIL::CheckCollections colCheck ;
   
+  colCheck.checkFiles( FILEN ) ;
+
+  colCheck.print( std::cout ) ;
   
-  // open list of files
-  lcReader.open( FILEN ) ;
 
-  std::unique_ptr<EVENT::LCEvent> evt ;
-
-  unsigned nEvents = 0 ;
-  
-  
-  std::map<std::string,unsigned> cntMap ;
-  std::map<std::string,std::string> typeMap ;
-
-  //----------- the event loop -----------
-  while( (evt = lcReader.readNextEventHeader()) != 0 ) {
-    
-    auto colNames = evt->getCollectionNames() ;
-
-    for(unsigned i=0,n= colNames->size() ; i<n ; ++i){
-      
-      auto name = colNames->at(i) ;
-      
-      unsigned cnt = cntMap[ name ] ++ ;
-
-      if( cnt == 0 ){ // enter type only for first occurence
-	auto col = evt->getCollection( name ) ;
-	typeMap.insert( std::make_pair( name,  col->getTypeName() ) ) ;
-      }
-    }
-
-    nEvents ++ ;
-  } 
-
-  // -------- end of event loop -----------
-  
-  std::cout << " ================================================================ " << std::endl ;
-  std::cout << std::endl <<  "  " <<  nEvents << " events read " << std::endl  ;
-  std::cout << "     collections that are not in all events : " << std::endl ;
-  std::cout << " ================================================================ " << std::endl ;
-
-
-  unsigned cntSize = cntMap.size() ;
-  unsigned typeSize = typeMap.size() ;
-
-  if( cntSize  != typeSize ) {
-    std::cout << " ERROR : different sizes in maps : count: " << cntSize << " types: "
-	      << typeSize << std::endl ;
-    exit(1) ;
-  }
-
-  auto it_c = cntMap.begin();
-  auto it_t = typeMap.begin();
-  for( ; it_c != cntMap.end() ; it_c++, it_t++ ){
-
-    if( it_c->second != nEvents )
-      std::cout << "     " <<  it_c->first << " : " << it_t->second << "  - " << it_c->second << std::endl ;
-  }
-
-  std::cout << " ================================================================ " << std::endl ;
-  std::cout << "     collections that are in all events : " << std::endl ;
-  std::cout << " ================================================================ " << std::endl ;
-
-
-  it_c = cntMap.begin();
-  it_t = typeMap.begin();
-  for( ; it_c != cntMap.end() ; it_c++, it_t++ ){
-
-    if( it_c->second == nEvents )
-      std::cout << "     " <<  it_c->first << " : " << it_t->second << "  - " << it_c->second << std::endl ;
-  }
-  std::cout << " ================================================================ " << std::endl ;
-
-
-  lcReader.close() ;
   return 0 ;
 }
 
