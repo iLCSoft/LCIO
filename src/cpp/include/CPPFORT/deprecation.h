4113e93e70ec00fbc86752fddb1b977cb85a0417
Thomas Madlener <thomas.madlener@desy.de>
2023-02-10 10:17:32 +0100

    Remove more leftover files from C-API and F77 bindings

--- src/cpp/include/CPPFORT/deprecation.h
+++
@@ -1,7 +0,0 @@
-#ifndef LCIO_CPPFORT_DEPRECATION_MACRO_H
-#define LCIO_CPPFORT_DEPRECATION_MACRO_H
-
-#define LCIO_DEPRECATED_CAPI                              \
-    [[deprecated("C-API of LCIO is deprecated and slated for removal in one of the next releases")]]
-
-#endif
