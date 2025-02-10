3f087aca4e1d7c22fb4f461e5ae02e5b50babf11
Thomas Madlener <thomas.madlener@desy.de>
2025-02-10 16:25:25 +0100

    Cleanup anajob.py slightly

--- examples/python/anajob.py
+++ examples/python/anajob.py
@@ -6,68 +6,86 @@ Re-implemented using python3 functions
 @author: Nazar Bartosik, INFN Torino
 """
 
+from pyLCIO import IOIMPL
+from ROOT import vector
+
+
 def formatVecValues(vec):
     """Helper method to transform the content of a vector into a string"""
-
-    if len( vec ) == 0:
-        return ' [empty]'
-    return ', '.join([str(v) for v in vec])
+    if len(vec) == 0:
+        return " [empty]"
+    return f"[{', '.join([str(v) for v in vec])}]"
 
 
 def printParameters(parameters):
     """Helper method to print the content of an LCParameters object"""
-    from ROOT import vector
-
-    keyVec = vector( 'string' )()
-    for intKey in parameters.getIntKeys( keyVec ):
-        intVec = vector( 'int' )()
-        parameters.getIntVals( intKey, intVec )
-        print('- %s [int]: %s' % (intKey, formatVecValues(intVec)))
+    keyVec = vector("string")()
+    for intKey in parameters.getIntKeys(keyVec):
+        intVec = vector("int")()
+        parameters.getIntVals(intKey, intVec)
+        print(f"- {intKey} [int]: formatVecValues(intVec)")
     keyVec.clear()
-    for floatKey in parameters.getFloatKeys( keyVec ):
-        floatVec = vector( 'float' )()
-        parameters.getFloatVals( floatKey, floatVec )
-        print('- %s [float]: %s' % (floatKey, formatVecValues(floatVec)))
+    for floatKey in parameters.getFloatKeys(keyVec):
+        floatVec = vector("float")()
+        parameters.getFloatVals(floatKey, floatVec)
+        print(f"- {floatKey} [float]: formatVecValues(floatVec)")
     keyVec.clear()
-    for stringKey in parameters.getStringKeys( keyVec ):
-        stringVec = vector( 'string' )()
-        parameters.getStringVals( stringKey, stringVec )
-        print('- %s [string]: %s' % (stringKey, formatVecValues(stringVec)))
+    for doubleKey in parameters.getDoubleKeys(keyVec):
+        doubleVec = vector("double")()
+        parameters.getDoubleVals(doubleKey, doubleVec)
+        print(f"- {doubleKey} [double]: formatVecValues(doubleVec)")
+    keyVec.clear()
+    for stringKey in parameters.getStringKeys(keyVec):
+        stringVec = vector("string")()
+        parameters.getStringVals(stringKey, stringVec)
+        print(f"- {stringKey} [string]: formatVecValues(stringVec)")
 
 
 def dumpEvent(event):
     """Helper method to print information on all collections in an LCEvent"""
-
-    print(f'RUN: {event.getRunNumber():<9d}  EVENT: {event.getEventNumber():<10d}  DETECTOR: {event.getDetectorName()}')
-    print('---------------------------------------------------------------------------')
-    print('{0:35}{1:27}{2:13}'.format('COLLECTION NAME', 'COLLECTION TYPE', '# OF ELEMENTS'))
-    print('===========================================================================')
+    print(
+        f"RUN: {event.getRunNumber():<9d}  EVENT: {event.getEventNumber():<10d}  DETECTOR: {event.getDetectorName()}"
+    )
+    print("---------------------------------------------------------------------------")
+    print(
+        "{0:35}{1:27}{2:13}".format(
+            "COLLECTION NAME", "COLLECTION TYPE", "# OF ELEMENTS"
+        )
+    )
+    print("===========================================================================")
 
     for collectionName, collection in event:
-        print('{0:35}{1:27}{2:>13}'.format(str(collectionName), collection.getTypeName(), collection.getNumberOfElements()))
+        print(
+            f"{str(collectionName):35}{collection.getTypeName():27}{collection.getNumberOfElements():>13}"
+        )
 
-    print('---------------------------------------------------------------------------\n\n')
+    print(
+        "---------------------------------------------------------------------------\n\n"
+    )
 
 
 def dumpRunHeader(run):
     """Helper method to print information on a LCRunHeader"""
 
-    print('Run : %d - %s:  %s' % (run.getRunNumber(), run.getDetectorName(), run.getDescription()))
-    printParameters( run.parameters() )
-    print('')
+    print(
+        f"Run : {run.getRunNumber()} - {run.getDetectorName()}:  {run.getDescription()}"
+    )
+    printParameters(run.parameters())
+    print("")
 
 
-def analyze_file(file_name, print_headers=False, max_events=None,
-                 run_nr=None, event_nr=None):
+def analyze_file(
+    file_name, print_headers=False, max_events=None, run_nr=None, event_nr=None
+):
     """Loop over all events of the given LCIO file and list the stored collections"""
-
-    from pyLCIO import IOIMPL, UTIL
 
     reader = IOIMPL.LCFactory.getInstance().createLCReader()
     reader.open(file_name)
 
-    print(f'Reading from file: {file_name}')
-    print(f'# of runs: {reader.getNumberOfRuns()}   # of events: {reader.getNumberOfEvents()}\n')
+    print(f"Reading from file: {file_name}")
+    print(
+        f"# of runs: {reader.getNumberOfRuns()}   # of events: {reader.getNumberOfEvents()}\n"
+    )
 
     if print_headers:
         runHeader = reader.readNextRunHeader()
@@ -78,7 +96,7 @@ def analyze_file(file_name, print_headers=False, max_events=None,
 
     n_events_read = 0
     # Print the contents of each event
-    reader.open( file_name )
+    reader.open(file_name)
     for event in reader:
         if max_events is not None and n_events_read >= max_events:
             break
@@ -95,29 +113,61 @@ def analyze_file(file_name, print_headers=False, max_events=None,
                 continue
         # UTIL.LCTOOLS.dumpEvent( event )
         # Using the custom dump function with more compact formatting
-        dumpEvent( event )
+        dumpEvent(event)
     reader.close()
     return n_events_read
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Prints summary of the LCIO file contents')
-    parser.add_argument('files', metavar='FILE', help='Input file(s)', nargs='+')
-    parser.add_argument('-e', '--event', metavar='NUMBER', help='Event number to print', type=int, default=None)
-    parser.add_argument('-r', '--run', metavar='NUMBER', help='Run number to print', type=int, default=None)
-    parser.add_argument('-m', '--max_events', metavar='N', help='Maximum number of events to print', type=int, default=None)
-    parser.add_argument('-v', '--verbose', help='Print run headers', action='store_true')
+    parser = argparse.ArgumentParser(
+        description="Prints summary of the LCIO file contents"
+    )
+    parser.add_argument("files", metavar="FILE", help="Input file(s)", nargs="+")
+    parser.add_argument(
+        "-e",
+        "--event",
+        metavar="NUMBER",
+        help="Event number to print",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "-r",
+        "--run",
+        metavar="NUMBER",
+        help="Run number to print",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "-m",
+        "--max_events",
+        metavar="N",
+        help="Maximum number of events to print",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="Print run headers", action="store_true"
+    )
     args = parser.parse_args()
 
-    print(f'Analyzing {len(args.files)} file(s)')
+    print(f"Analyzing {len(args.files)} file(s)")
 
     n_events_read = 0
     for file_name in args.files:
-        max_events = None if args.max_events is None else args.max_events - n_events_read
-        n_events_read += analyze_file(file_name, print_headers=args.verbose, max_events=max_events,
-                                      run_nr=args.run, event_nr=args.event)
+        max_events = (
+            None if args.max_events is None else args.max_events - n_events_read
+        )
+        n_events_read += analyze_file(
+            file_name,
+            print_headers=args.verbose,
+            max_events=max_events,
+            run_nr=args.run,
+            event_nr=args.event,
+        )
         if args.max_events is not None and n_events_read >= args.max_events:
             break
-    print(f'\nFinished reading {n_events_read} event(s)')
+    print(f"\nFinished reading {n_events_read} event(s)")
