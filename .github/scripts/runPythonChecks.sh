26c8e2faabd9052be622b370446bfa6747a2f7b8
Thomas Madlener <thomas.madlener@desy.de>
2025-08-21 11:52:14 +0200

    Remove no longer necessary checks for compatibility

--- .github/scripts/runPythonChecks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /cvmfs/sft.cern.ch/lcg/views/${VIEW}/setup.sh
-cd /Package
-
-echo "RUNNING PYLINT PY3K CHECK"
-find . -name "*.py" -and -not -name 'sixlcio.py' -exec pylint --rcfile=.github/scripts/lcio.pylint.py3k.rc --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
-
